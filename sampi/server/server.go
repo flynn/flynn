5603600a4937eacedbceec18e75159798cb4cad2
Jonathan Rudenberg <jonathan@titanous.com>
2013-10-07 12:07:25 -0400

    host/sampi: Register with service discovery

--- sampi/server/server.go
+++ sampi/server/server.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"net"
 	"net/http"
+	"strings"
 
+	"github.com/flynn/go-discover/discover"
 	"github.com/flynn/rpcplus"
 )
 
+var listenAddr = flag.String("listen", ":1112", "listen address")
+
 func main() {
+	flag.Parse()
 	rpcplus.HandleHTTP()
-	http.ListenAndServe(":1112", nil)
+	l, err := net.Listen("tcp", *listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d, err := discover.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if hostPort := strings.SplitN(*listenAddr, ":", 2); hostPort[0] != "" {
+		err = d.RegisterWithHost("flynn-sampi", hostPort[0], hostPort[1], nil)
+	} else {
+		err = d.Register("flynn-sampi", hostPort[1], nil)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Serve(l, nil)
 }
