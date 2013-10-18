ea36d12d500f2c38955f6db526f49c210dc38bd8
Jeff Lindsay <progrium@gmail.com>
2013-10-17 23:09:33 -0700

    blobstore: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Shelf
+HTTP File Service
+
+Simple HTTP interface to a filestore ... right now the filesystem.
---
+++ shelf.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+var port = flag.String("p", "8888", "Port to listen on")
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage:	%v -p <port> <storage-path>\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
+func errorResponse(w http.ResponseWriter, e error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(e.Error()))
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(64)
+	}
+
+	storagepath := flag.Arg(0)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		filepath := storagepath + r.RequestURI
+		switch r.Method {
+		case "GET":
+			file, err := os.Open(filepath)
+			if err != nil {
+				errorResponse(w, err)
+				return
+			}
+			defer file.Close()
+			_, err = io.Copy(w, file)
+			if err != nil {
+				errorResponse(w, err)
+				return
+			}
+		case "PUT":
+			file, err := os.Create(filepath)
+			if err != nil {
+				errorResponse(w, err)
+				return
+			}
+			defer file.Close()
+			_, err = io.Copy(file, r.Body)
+			if err != nil {
+				errorResponse(w, err)
+				return
+			}
+		case "DELETE":
+			err := os.RemoveAll(filepath)
+			if err != nil {
+				errorResponse(w, err)
+				return
+			}
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
