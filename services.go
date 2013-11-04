66488a99da31192013fe9d55c513412cd2b7de7a
Jeff Lindsay <progrium@gmail.com>
2013-11-04 13:30:18 -0800

    sdutil: update to go-discover api. also -1 option for services subcommand

--- services.go
+++ services.go
@@ -8,6 +8,7 @@ import (
 
 type services struct {
 	clientCmd
+	onlyOne *bool
 }
 
 func (cmd *services) Name() string {
@@ -15,6 +16,7 @@ func (cmd *services) Name() string {
 }
 
 func (cmd *services) DefineFlags(fs *flag.FlagSet) {
+	cmd.onlyOne = fs.Bool("1", false, "only show one service")
 }
 
 func (cmd *services) Run(fs *flag.FlagSet) {
@@ -22,6 +24,13 @@ func (cmd *services) Run(fs *flag.FlagSet) {
 	set, err := cmd.client.Services(fs.Arg(0))
 	if err != nil {
 		log.Fatal(err)
+	}
+	if *cmd.onlyOne {
+		addrs := set.OnlineAddrs()
+		if len(addrs) > 0 {
+			fmt.Println(addrs[0])
+		}
+		return
 	}
 	for _, addr := range set.OnlineAddrs() {
 		fmt.Println(addr)
