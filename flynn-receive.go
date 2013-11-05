df77293a76912471abf63d348ef4b7d15c967722
Jeff Lindsay <progrium@gmail.com>
2013-11-05 13:53:33 -0800

    demo: maybe it all works

--- flynn-receive.go
+++ flynn-receive.go
@@ -1,37 +1,56 @@
 package main
 
 import (
-	"bytes"
-	"crypto/rand"
-	"encoding/base64"
+	"os/exec"
+	"strings"
 	"encoding/gob"
 	"fmt"
 	"bufio"
 	"io"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"os/exec"
-	"strings"
-	"syscall"
-
+	
 	"github.com/flynn/go-discover/discover"
 	"github.com/flynn/lorne/types"
-	"github.com/flynn/sampi/client"
-	"github.com/flynn/sampi/types"
 	"github.com/titanous/go-dockerclient"
+	"github.com/flynn/sampi/types"
+	sc "github.com/flynn/sampi/client"
+	lc "github.com/flynn/lorne/client"
 )
+
+// WARNING: assumes one host at the moment (firstHost will always be the same)
+
+
+var sd *discover.Client
+var sched *sc.Client
+var host *lc.Client
+var hostid string
+
+func init() {
+	var err error
+	sd, err = discover.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sched, err = sc.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hostid := findHost()
+	host, err = lc.New(hostid)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	root := "/var/lib/demo/apps"
 	hostname := shell("curl -s icanhazip.com")
 
-	client, err := discover.NewClient()
-	if err != nil {
-		panic(err)
-	}
-	set, _ := client.Services("shelf")
+	set, _ := sd.Services("shelf")
 	addrs := set.OnlineAddrs()
 	if len(addrs) < 1 {
 		panic("Shelf is not discoverable")
@@ -43,7 +62,7 @@ func main() {
 
 	fmt.Printf("-----> Building %s on %s ...\n", app, hostname)
 
-	scheduleAndAttach(docker.Config{
+	scheduleAndAttach(app + ".build", docker.Config{
 		Image:        "flynn/slugbuilder",
 		Cmd:          []string{"http://" + shelfHost + "/" + app + ".tgz"},
 		Tty:          false,
@@ -54,112 +73,71 @@ func main() {
 		StdinOnce:    true,
 	})
 
-	/*fmt.Printf("-----> Deploying %s ...\n", app)
-		if _, err := os.Stat(root + "/" + app + "/CONTAINER"); err == nil {
-	    	oldid := readFile(root + "/" + app + "/CONTAINER")
-	    	shell("docker kill " + oldid)
-		}
+	fmt.Printf("-----> Deploying %s ...\n", app)
+	
+	jobid := app + ".web"
 
-		id := shell("docker run -d -p 5000 -e PORT=5000 -e SLUG_URL=http://"+shelfHost+"/"+app+".tgz flynn/slugrunner start web")
-		writeFile(root + "/" + app + "/CONTAINER", id)
-		port := shell("docker port "+id+" 5000 | sed 's/0.0.0.0://'")
-		writeFile(root + "/" + app + "/PORT", port)
-		writeFile(root + "/" + app + "/URL", "http://"+hostname+":"+port)
+	stopIfExists(jobid)
+	scheduleWithTcpPort(jobid, docker.Config{
+		Image:        "flynn/slugrunner",
+		Cmd:          []string{"start web"},
+		Tty:          false,
+		AttachStdin:  false,
+		AttachStdout: false,
+		AttachStderr: false,
+		OpenStdin:    false,
+		StdinOnce:    false,
+		Env: []string{
+			"SLUG_URL=http://" + shelfHost + "/" + app + ".tgz",
+		},
+	})
 
-		fmt.Printf("=====> Application deployed:\n")
-		fmt.Printf("       %s\n", readFile(root + "/" + app + "/URL"))*/
+	fmt.Printf("=====> Application deployed!\n")
+	//fmt.Printf("       %s\n", getUrl(jobid))
 	fmt.Println("")
 
 }
 
-func readFile(filename string) string {
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	return string(data)
-}
-
-func writeFile(filename, data string) {
-	err := ioutil.WriteFile(filename, []byte(data), 0644)
-	if err != nil {
-		panic(err)
-	}
-}
-
 func shell(cmdline string) string {
-	out, err := exec.Command("bash", "-c", cmdline).Output()
-	if err != nil {
-		panic(err)
-	}
-	return strings.Trim(string(out), " \n")
+        out, err := exec.Command("bash", "-c", cmdline).Output()
+        if err != nil {
+                panic(err)
+        }
+        return strings.Trim(string(out), " \n")
 }
 
-func attachCmd(cmd *exec.Cmd, stdout, stderr io.Writer, stdin io.Reader) chan error {
-	errCh := make(chan error)
-
-	stdinIn, err := cmd.StdinPipe()
+func stopIfExists(jobid string) {
+	job, err := host.GetJob(jobid)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	stdoutOut, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	stderrOut, err := cmd.StderrPipe()
-	if err != nil {
-		panic(err)
-	}
-
-	go func() {
-		_, e := io.Copy(stdinIn, stdin)
-		errCh <- e
-	}()
-	go func() {
-		_, e := io.Copy(stdout, stdoutOut)
-		errCh <- e
-	}()
-	go func() {
-		_, e := io.Copy(stderr, stderrOut)
-		errCh <- e
-	}()
-
-	return errCh
-}
-
-func exitStatusCh(cmd *exec.Cmd) chan uint {
-	exitCh := make(chan uint)
-	go func() {
-		err := cmd.Wait()
-		if err != nil {
-			if exiterr, ok := err.(*exec.ExitError); ok {
-				// There is no plattform independent way to retrieve
-				// the exit code, but the following will work on Unix
-				if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-					exitCh <- uint(status.ExitStatus())
-				}
-			} else {
-				panic(err)
-			}
-			return
+	if job != nil {
+		if err := host.StopJob(jobid); err != nil {
+			log.Fatal(err)
 		}
-		exitCh <- uint(0)
-	}()
-	return exitCh
+	}
+
 }
 
-func scheduleAndAttach(config docker.Config) {
-	disc, err := discover.NewClient()
-	if err != nil {
+func scheduleWithTcpPort(jobid string, config docker.Config) {
+
+	schedReq := &sampi.ScheduleReq{
+		Incremental: true,
+		HostJobs:    map[string][]*sampi.Job{hostid: {{ID: jobid, Config: &config, TCPPorts: 1}}},
+	}
+	if _, err := sched.Schedule(schedReq); err != nil {
 		log.Fatal(err)
 	}
+}
 
-	scheduler, err := client.New()
-	if err != nil {
+/*func getUrl(jobid string) string {
+	if job, err := host.GetJob(jobid); err != nil {
 		log.Fatal(err)
 	}
+}*/
 
-	state, err := scheduler.State()
+func findHost() string {
+	state, err := sched.State()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -172,10 +150,13 @@ func scheduleAndAttach(config docker.Config) {
 	if firstHost == "" {
 		log.Fatal("no hosts")
 	}
+	return firstHost
+}
 
-	id := randomID()
+func scheduleAndAttach(jobid string, config docker.Config) {
+	firstHost := hostid
 
-	services, err := disc.Services("flynn-lorne-attach." + firstHost)
+	services, err := sd.Services("flynn-lorne-attach." + firstHost)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -184,7 +165,7 @@ func scheduleAndAttach(config docker.Config) {
 		log.Fatal(err)
 	}
 	err = gob.NewEncoder(conn).Encode(&lorne.AttachReq{
-		JobID: id,
+		JobID: jobid,
 		Flags: lorne.AttachFlagStdout | lorne.AttachFlagStderr | lorne.AttachFlagStdin | lorne.AttachFlagStream,
 	})
 	if err != nil {
@@ -201,9 +182,9 @@ func scheduleAndAttach(config docker.Config) {
 
 	schedReq := &sampi.ScheduleReq{
 		Incremental: true,
-		HostJobs:    map[string][]*sampi.Job{firstHost: {{ID: id, Config: &config}}},
+		HostJobs:    map[string][]*sampi.Job{firstHost: {{ID: jobid, Config: &config}}},
 	}
-	if _, err := scheduler.Schedule(schedReq); err != nil {
+	if _, err := sched.Schedule(schedReq); err != nil {
 		log.Fatal(err)
 	}
 
@@ -219,19 +200,5 @@ func scheduleAndAttach(config docker.Config) {
 	for scanner.Scan() {
 		fmt.Fprintln(os.Stdout, scanner.Text()[8:])
 	}
-	/*if _, err := io.Copy(os.Stdout, conn); err != nil {
-		log.Fatal(err)
-	}*/
 	conn.Close()
-}
-
-func randomID() string {
-	b := make([]byte, 16)
-	enc := make([]byte, 24)
-	_, err := io.ReadFull(rand.Reader, b)
-	if err != nil {
-		panic(err) // This shouldn't ever happen, right?
-	}
-	base64.URLEncoding.Encode(enc, b)
-	return string(bytes.TrimRight(enc, "="))
 }
