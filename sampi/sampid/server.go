1588a12169228703959b3dd46b7a81abb19fa8ca
Jonathan Rudenberg <jonathan@titanous.com>
2013-12-01 15:21:58 -0500

    host/sampi: Use grohl for logging

--- sampi/sampid/server.go
+++ sampi/sampid/server.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/flynn/go-discover/discover"
 	rpc "github.com/flynn/rpcplus/comborpc"
+	"github.com/technoweenie/grohl"
 )
 
 var listenAddr = flag.String("listen", ":1112", "listen address")
 
 func main() {
 	flag.Parse()
+	g := grohl.NewContext(grohl.Data{"app": "sampi"})
+	g.Log(grohl.Data{"at": "start"})
 	rpc.HandleHTTP()
 	l, err := net.Listen("tcp", *listenAddr)
 	if err != nil {
-		log.Fatal(err)
+		g.Log(grohl.Data{"at": "listen", "status": "error", "err": err})
+		os.Exit(1)
 	}
 
 	d, err := discover.NewClient()
 	if err != nil {
-		log.Fatal(err)
+		g.Log(grohl.Data{"at": "discover_connect", "status": "error", "err": err})
+		os.Exit(1)
 	}
 	if hostPort := strings.SplitN(*listenAddr, ":", 2); hostPort[0] != "" {
 		err = d.RegisterWithHost("flynn-sampi", hostPort[0], hostPort[1], nil)
@@ -31,7 +36,8 @@ func main() {
 		err = d.Register("flynn-sampi", hostPort[1], nil)
 	}
 	if err != nil {
-		log.Fatal(err)
+		g.Log(grohl.Data{"at": "discover_registration", "status": "error", "err": err})
+		os.Exit(1)
 	}
 
 	http.Serve(l, nil)
