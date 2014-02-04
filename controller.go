cafaae8c717554e13ff5c768c960a0c43c0bb9ab
Jonathan Rudenberg <jonathan@titanous.com>
2014-02-04 16:06:16 -0500

    controller: Add a quick CRUD helper (currently only CR)
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- controller.go
+++ controller.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/codegangsta/martini"
-	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/codegangsta/martini-contrib/render"
 )
 
@@ -22,72 +21,17 @@ func appHandler() http.Handler {
 	m.Use(martini.Logger())
 	m.Use(martini.Recovery())
 	m.Use(render.Renderer())
-	m.Map(NewAppRepo())
-	m.Map(NewArtifactRepo())
 	m.Action(r.Handle)
 
-	r.Post("/apps", binding.Bind(App{}), createApp)
-	r.Get("/apps/:app_id", getAppMiddleware, getApp)
+	appRepo := NewAppRepo()
+	artifactRepo := NewArtifactRepo()
+	m.Map(appRepo)
+	m.Map(artifactRepo)
 
-	r.Post("/artifacts", binding.Bind(Artifact{}), createArtifact)
-	r.Get("/artifacts/:artifact_id", getArtifactMiddleware, getArtifact)
+	crud("apps", App{}, appRepo, r)
+	crud("artifacts", Artifact{}, artifactRepo, r)
 
 	return m
-}
-
-type Repository interface {
-	Add(interface{}) error
-	Get(string) (interface{}, error)
-}
-
-// POST /apps
-func createApp(app App, repo *AppRepo, r render.Render) {
-	if err := repo.Add(&app); err != nil {
-		// TODO: handle error
-	}
-	r.JSON(200, &app)
-}
-
-func getAppMiddleware(c martini.Context, repo *AppRepo, params martini.Params, w http.ResponseWriter) {
-	app, err := repo.Get(params["app_id"])
-	if err != nil {
-		if err == ErrNotFound {
-			w.WriteHeader(404)
-			return
-		}
-		// TODO: 500/log error
-	}
-	c.Map(app)
-}
-
-// GET /apps/:app_id
-func getApp(app *App, r render.Render) {
-	r.JSON(200, app)
-}
-
-// POST /artifacts
-func createArtifact(artifact Artifact, repo *ArtifactRepo, r render.Render) {
-	if err := repo.Add(&artifact); err != nil {
-		// TODO: handle error
-	}
-	r.JSON(200, &artifact)
-}
-
-func getArtifactMiddleware(c martini.Context, repo *ArtifactRepo, params martini.Params, w http.ResponseWriter) {
-	artifact, err := repo.Get(params["artifact_id"])
-	if err != nil {
-		if err == ErrNotFound {
-			w.WriteHeader(404)
-			return
-		}
-		// TODO: 500/log error
-	}
-	c.Map(artifact)
-}
-
-// GET /artifacts/:artifact_id
-func getArtifact(artifact *Artifact, r render.Render) {
-	r.JSON(200, artifact)
 }
 
 func createRelease() {
---
+++ crud.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"reflect"
+
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/render"
+)
+
+type Repository interface {
+	Add(interface{}) error
+	Get(string) (interface{}, error)
+}
+
+func crud(resource string, example interface{}, repo Repository, r martini.Router) {
+	resourceType := reflect.TypeOf(example)
+
+	r.Post("/"+resource, func(req *http.Request, r render.Render) {
+		thing := reflect.New(resourceType).Interface()
+		err := json.NewDecoder(req.Body).Decode(thing)
+		if err != nil {
+			// 400?
+			return
+		}
+
+		err = repo.Add(thing)
+		if err != nil {
+			// 500
+			// log error
+			return
+		}
+		r.JSON(200, thing)
+	})
+
+	r.Get("/"+resource+"/:id", func(params martini.Params, r render.Render, w http.ResponseWriter) {
+		thing, err := repo.Get(params["id"])
+		if err != nil {
+			if err == ErrNotFound {
+				w.WriteHeader(404)
+				return
+			}
+			// TODO: 500/log error
+		}
+		r.JSON(200, thing)
+	})
+}
