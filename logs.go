06aa1724e7151a644322a8e065347f4cb2246b75
Stephen Belanger <admin@stephenbelanger.com>
2014-03-13 23:04:22 -0700

    cli: More descriptive error when job id is left blank on logs command.
    Signed-off-by: Stephen Belanger <admin@stephenbelanger.com>

--- logs.go
+++ logs.go
@@ -12,5 +12,9 @@ var cmdLogs = &Command{
 }
 
 func runLogs(cmd *Command, args []string) {
+	if len(args) != 1 {
+		panic("You must supply a job id")
+	}
+
 	must(Get(os.Stdout, "/apps/"+mustApp()+"/jobs/"+args[0]+"/logs"))
 }
