3431a40f3298be9317b7663d19a1f97ec5b8ef06
Jonathan Rudenberg <jonathan@titanous.com>
2014-03-25 20:05:09 -0400

    bootstrap: Add bootstrap runner executable
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

---
+++ bootstrapper/bootstrapper.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/flynn/flynn-bootstrap"
+)
+
+func manifest() ([]byte, error) {
+	if len(os.Args) == 1 {
+		return ioutil.ReadAll(os.Stdin)
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ioutil.ReadAll(f)
+}
+
+func main() {
+	manifest, err := manifest()
+	if err != nil {
+		log.Fatalln("Error reading manifest:", err)
+	}
+
+	if err := bootstrap.Run(manifest); err != nil {
+		log.Fatal(err)
+	}
+}
