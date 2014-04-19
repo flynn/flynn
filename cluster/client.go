3c52928cafb67972e5d92073a7b49867210a826d
Jonathan Rudenberg <jonathan@titanous.com>
2014-04-19 10:36:25 -0400

    pkg/cluster: Allow flynn-host to specify a local leader
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- cluster/client.go
+++ cluster/client.go
@@ -26,7 +26,18 @@ func NewClient() (*Client, error) {
 	return NewClientWithDial(nil, nil)
 }
 
-func NewClientWithDial(dial rpcplus.DialFunc, services func(name string) (discoverd.ServiceSet, error)) (*Client, error) {
+type ServiceSetFunc func(name string) (discoverd.ServiceSet, error)
+
+func NewClientWithDial(dial rpcplus.DialFunc, services ServiceSetFunc) (*Client, error) {
+	client, err := newClient(services)
+	if err != nil {
+		return nil, err
+	}
+	client.dial = dial
+	return client, client.start()
+}
+
+func newClient(services ServiceSetFunc) (*Client, error) {
 	if services == nil {
 		services = discoverd.NewServiceSet
 	}
@@ -34,10 +45,43 @@ func NewClientWithDial(dial rpcplus.DialFunc, services func(name string) (discov
 	if err != nil {
 		return nil, err
 	}
-	client := &Client{dial: dial, service: ss}
+	return &Client{service: ss}, nil
+}
+
+type LocalClient interface {
+	ListHosts() (map[string]host.Host, error)
+	AddJobs(*host.AddJobsReq) (*host.AddJobsRes, error)
+	RegisterHost(*host.Host, chan *host.Job) *error
+	RemoveJobs([]string) error
+}
+
+func NewClientWithSelf(id string, self LocalClient) (*Client, error) {
+	client, err := newClient(nil)
+	if err != nil {
+		return nil, err
+	}
+	client.selfID = id
+	client.self = self
+	return client, client.start()
+}
+
+type Client struct {
+	service  discoverd.ServiceSet
+	leaderID string
+
+	dial rpcplus.DialFunc
+	c    RPCClient
+	mtx  sync.RWMutex
+	err  error
+
+	selfID string
+	self   LocalClient
+}
+
+func (c *Client) start() error {
 	firstErr := make(chan error)
-	go client.followLeader(firstErr)
-	return client, <-firstErr
+	go c.followLeader(firstErr)
+	return <-firstErr
 }
 
 func (c *Client) followLeader(firstErr chan<- error) {
@@ -53,12 +97,15 @@ func (c *Client) followLeader(firstErr chan<- error) {
 		c.mtx.Lock()
 		if c.c != nil {
 			c.c.Close()
+			c.c = nil
 		}
 		c.leaderID = update.Attrs["id"]
-		c.err = Attempts.Run(func() (err error) {
-			c.c, err = rpcplus.DialHTTPPath("tcp", update.Addr, rpcplus.DefaultRPCPath, c.dial)
-			return
-		})
+		if c.leaderID != c.selfID {
+			c.err = Attempts.Run(func() (err error) {
+				c.c, err = rpcplus.DialHTTPPath("tcp", update.Addr, rpcplus.DefaultRPCPath, c.dial)
+				return
+			})
+		}
 		c.mtx.Unlock()
 		if firstErr != nil {
 			firstErr <- c.err
@@ -72,14 +119,13 @@ func (c *Client) followLeader(firstErr chan<- error) {
 	// TODO: reconnect to discoverd here
 }
 
-type Client struct {
-	service  discoverd.ServiceSet
-	leaderID string
-
-	dial rpcplus.DialFunc
-	c    RPCClient
-	mtx  sync.RWMutex
-	err  error
+func (c *Client) local() LocalClient {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
+	if c.leaderID == c.selfID {
+		return c.self
+	}
+	return nil
 }
 
 func (c *Client) Close() error {
@@ -98,31 +144,27 @@ func (c *Client) LeaderID() string {
 }
 
 func (c *Client) ListHosts() (map[string]host.Host, error) {
-	c.mtx.RLock()
-	if err := c.err; err != nil {
-		c.mtx.RUnlock()
+	if c := c.local(); c != nil {
+		return c.ListHosts()
+	}
+	client, err := c.RPCClient()
+	if err != nil {
 		return nil, err
 	}
-	client := c.c
-	c.mtx.RUnlock()
-
 	var state map[string]host.Host
-	err := client.Call("Cluster.ListHosts", struct{}{}, &state)
-	return state, err
+	return state, client.Call("Cluster.ListHosts", struct{}{}, &state)
 }
 
 func (c *Client) AddJobs(req *host.AddJobsReq) (*host.AddJobsRes, error) {
-	c.mtx.RLock()
-	if err := c.err; err != nil {
-		c.mtx.RUnlock()
+	if c := c.local(); c != nil {
+		return c.AddJobs(req)
+	}
+	client, err := c.RPCClient()
+	if err != nil {
 		return nil, err
 	}
-	client := c.c
-	c.mtx.RUnlock()
-
 	var res host.AddJobsRes
-	err := client.Call("Cluster.AddJobs", req, &res)
-	return &res, err
+	return &res, client.Call("Cluster.AddJobs", req, &res)
 }
 
 func (c *Client) DialHost(id string) (Host, error) {
@@ -133,6 +175,33 @@ func (c *Client) DialHost(id string) (Host, error) {
 	}
 	rc, err := rpcplus.DialHTTPPath("tcp", services[0].Addr, rpcplus.DefaultRPCPath, c.dial)
 	return newHostClient(c.service, rc, c.dial), err
+}
+
+// Register is used by flynn-host to register itself with the leader and get
+// a stream of new jobs. It is not used by clients.
+func (c *Client) RegisterHost(host *host.Host, jobs chan *host.Job) *error {
+	if c := c.local(); c != nil {
+		return c.RegisterHost(host, jobs)
+	}
+	client, err := c.RPCClient()
+	if err != nil {
+		return &err
+	}
+	return &client.StreamGo("Cluster.RegisterHost", host, jobs).Error
+}
+
+// RemoveJobs is used by flynn-host to delete jobs from the cluster state. It
+// does not actually kill jobs running on hosts, and must not be used by
+// clients.
+func (c *Client) RemoveJobs(jobIDs []string) error {
+	if c := c.local(); c != nil {
+		return c.RemoveJobs(jobIDs)
+	}
+	client, err := c.RPCClient()
+	if err != nil {
+		return err
+	}
+	return client.Call("Cluster.RemoveJobs", jobIDs, &struct{}{})
 }
 
 func (c *Client) RPCClient() (RPCClient, error) {
