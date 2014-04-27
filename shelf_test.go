0c040015234dae2692d676d261ed1c2d64270009
Jonathan Rudenberg <jonathan@titanous.com>
2014-04-27 13:35:29 -0700

    blobstore: Add concurrent test suite
    refs #8
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- shelf_test.go
+++ shelf_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/flynn/go-sql"
@@ -51,6 +55,7 @@ func TestPostgresFilesystem(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer db.Close()
+	db.SetMaxOpenConns(1)
 
 	fs, err := NewPostgresFilesystem(db)
 	if err != nil {
@@ -59,172 +64,197 @@ func TestPostgresFilesystem(t *testing.T) {
 	testFilesystem(fs, true, t)
 }
 
+const concurrency = 5
+
 func testFilesystem(fs Filesystem, testMeta bool, t *testing.T) {
 	srv := httptest.NewServer(handler(fs))
 	defer srv.Close()
 
-	path := srv.URL + "/foo/bar"
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			path := srv.URL + "/foo/bar/" + random(16)
+			res, err := http.Get(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 404 {
+				t.Errorf("Expected 404 for non-existent file, got %d", res.StatusCode)
+			}
 
-	res, err := http.Get(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 404 {
-		t.Errorf("Expected 404 for non-existent file, got %d", res.StatusCode)
+			res, err = http.Head(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 404 {
+				t.Errorf("Expected 404 for non-existent file, got %d", res.StatusCode)
+			}
+
+			data := random(16)
+			req, err := http.NewRequest("PUT", path, strings.NewReader(data))
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header.Set("Content-Type", "text/plain")
+			res, err = http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 200 {
+				t.Errorf("Expected 200 for successful PUT, got %d", res.StatusCode)
+			}
+
+			res, err = http.Get(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			resData, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if res.StatusCode != 200 {
+				t.Errorf("Expected 200 for GET, got %d", res.StatusCode)
+			}
+			if string(resData) != data {
+				t.Errorf("Expected data to be %q, got %q", data, string(resData))
+			}
+
+			res, err = http.Head(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 200 {
+				t.Errorf("Expected 200 for HEAD, got %d", res.StatusCode)
+			}
+			if cl := res.Header.Get("Content-Length"); cl != "32" {
+				t.Errorf(`Expected Content-Length to be "32", got %q`, cl)
+			}
+			if testMeta {
+				if ct := res.Header.Get("Content-Type"); ct != "text/plain" {
+					t.Errorf(`Expected Content-Type to be "text/plain", got %q`, ct)
+				}
+
+				etag := res.Header.Get("Etag")
+				if etag == "" {
+					t.Error("Expected ETag to be set")
+				}
+				req, err := http.NewRequest("GET", path, nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				req.Header.Set("If-None-Match", etag)
+				res, err = http.DefaultClient.Do(req)
+				if err != nil {
+					t.Fatal(err)
+				}
+				res.Body.Close()
+				if res.StatusCode != http.StatusNotModified {
+					t.Errorf("Expected ETag GET status to be 304, got %d", res.StatusCode)
+				}
+			}
+
+			newData := random(32)
+			req, err = http.NewRequest("PUT", path, strings.NewReader(newData))
+			if err != nil {
+				log.Fatal(err)
+			}
+			req.Header.Set("Content-Type", "application/text")
+			res, err = http.DefaultClient.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 200 {
+				t.Errorf("Expected 200 for update PUT, got %d", res.StatusCode)
+			}
+
+			var wg2 sync.WaitGroup
+			wg2.Add(concurrency)
+			for i := 0; i < concurrency; i++ {
+				go func() {
+					res, err := http.Get(path)
+					if err != nil {
+						t.Fatal(err)
+					}
+					resData, err := ioutil.ReadAll(res.Body)
+					res.Body.Close()
+					if err != nil {
+						t.Fatal(err)
+					}
+					if res.StatusCode != 200 {
+						t.Errorf("Expected 200 for update GET, got %d", res.StatusCode)
+					}
+					if string(resData) != newData {
+						t.Errorf("Expected data to be %q, got %q", newData, string(resData))
+					}
+
+					res, err = http.Head(path)
+					if err != nil {
+						t.Fatal(err)
+					}
+					res.Body.Close()
+					if res.StatusCode != 200 {
+						t.Errorf("Expected 200 for update HEAD, got %d", res.StatusCode)
+					}
+					if cl := res.Header.Get("Content-Length"); cl != "64" {
+						t.Errorf(`Expected Content-Length to be "64", got %q`, cl)
+					}
+					if testMeta {
+						if ct := res.Header.Get("Content-Type"); ct != "application/text" {
+							t.Errorf(`Expected Content-Type to be "application/text", got %q`, ct)
+						}
+					}
+					wg2.Done()
+				}()
+			}
+			wg2.Wait()
+
+			req, err = http.NewRequest("DELETE", path, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res, err = http.DefaultClient.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 200 {
+				t.Errorf("Expected 200 for DELETE, got %d", res.StatusCode)
+			}
+
+			res, err = http.Get(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 404 {
+				t.Errorf("Expected 200 for deleted GET, got %d", res.StatusCode)
+			}
+
+			res, err = http.Head(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res.Body.Close()
+			if res.StatusCode != 404 {
+				t.Errorf("Expected 200 for deleted HEAD, got %d", res.StatusCode)
+			}
+
+			wg.Done()
+		}()
 	}
 
-	res, err = http.Head(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 404 {
-		t.Errorf("Expected 404 for non-existent file, got %d", res.StatusCode)
-	}
+	wg.Wait()
+}
 
-	data := "foobar"
-	req, err := http.NewRequest("PUT", path, strings.NewReader(data))
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "text/plain")
-	res, err = http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for successful PUT, got %d", res.StatusCode)
-	}
-
-	res, err = http.Get(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	resData, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for GET, got %d", res.StatusCode)
-	}
-	if string(resData) != data {
-		t.Errorf("Expected data to be %q, got %q", data, string(resData))
-	}
-
-	res, err = http.Head(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for HEAD, got %d", res.StatusCode)
-	}
-	if cl := res.Header.Get("Content-Length"); cl != "6" {
-		t.Errorf(`Expected Content-Length to be "6", got %q`, cl)
-	}
-	if testMeta {
-		if ct := res.Header.Get("Content-Type"); ct != "text/plain" {
-			t.Errorf(`Expected Content-Type to be "text/plain", got %q`, ct)
-		}
-
-		etag := res.Header.Get("Etag")
-		if etag == "" {
-			t.Error("Expected ETag to be set")
-		}
-		req, err := http.NewRequest("GET", path, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		req.Header.Set("If-None-Match", etag)
-		res, err = http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-		res.Body.Close()
-		if res.StatusCode != http.StatusNotModified {
-			t.Errorf("Expected ETag GET status to be 304, got %d", res.StatusCode)
-		}
-	}
-
-	newData := "foobaz2"
-	req, err = http.NewRequest("PUT", path, strings.NewReader(newData))
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/text")
-	res, err = http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for update PUT, got %d", res.StatusCode)
-	}
-
-	res, err = http.Get(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	resData, err = ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for update GET, got %d", res.StatusCode)
-	}
-	if string(resData) != newData {
-		t.Errorf("Expected data to be %q, got %q", newData, string(resData))
-	}
-
-	res, err = http.Head(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for update HEAD, got %d", res.StatusCode)
-	}
-	if cl := res.Header.Get("Content-Length"); cl != "7" {
-		t.Errorf(`Expected Content-Length to be "7", got %q`, cl)
-	}
-	if testMeta {
-		if ct := res.Header.Get("Content-Type"); ct != "application/text" {
-			t.Errorf(`Expected Content-Type to be "application/text", got %q`, ct)
-		}
-	}
-
-	req, err = http.NewRequest("DELETE", path, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err = http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 200 {
-		t.Errorf("Expected 200 for DELETE, got %d", res.StatusCode)
-	}
-
-	res, err = http.Get(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 404 {
-		t.Errorf("Expected 200 for deleted GET, got %d", res.StatusCode)
-	}
-
-	res, err = http.Head(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode != 404 {
-		t.Errorf("Expected 200 for deleted HEAD, got %d", res.StatusCode)
-	}
+func random(n int) string {
+	data := make([]byte, n)
+	io.ReadFull(rand.Reader, data)
+	return hex.EncodeToString(data)
 }
