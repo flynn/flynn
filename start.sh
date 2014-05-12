edd85db7111accf3e97d5647369d88f4483475be
Jonathan Rudenberg <jonathan@titanous.com>
2014-05-12 18:39:56 -0400

    receiver: Update gitreceived flags
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-echo "$SSH_PRIVATE_KEY" > /tmp/id_rsa
-/bin/sdutil exec -s gitreceive:$PORT /bin/gitreceived /tmp/id_rsa /bin/flynn-key-check /bin/flynn-receive
+/bin/sdutil exec -s gitreceive:$PORT /bin/gitreceived /bin/flynn-key-check /bin/flynn-receive
