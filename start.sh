b70f0f47636b5060e087812a511d06b78a80c58d
Jonathan Rudenberg <jonathan@titanous.com>
2014-07-31 17:35:09 -0700

    receiver: exec binary in start.sh
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/bin/sdutil exec -s gitreceive:$PORT /bin/gitreceived /bin/flynn-key-check /bin/flynn-receive
+exec /bin/sdutil exec -s gitreceive:$PORT /bin/gitreceived /bin/flynn-key-check /bin/flynn-receive
