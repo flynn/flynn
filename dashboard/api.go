f88f1a777d85e17cb4c3ba73663a19e4cf84f75d
Jesse Stuart <jesse@jessestuart.ca>
2014-08-26 17:42:35 -0400

    dashboard: Server
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

---
+++ dashboard/api.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"crypto/subtle"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/badgerodon/ioutil"
+	"github.com/flynn/flynn/pkg/cors"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+	"github.com/martini-contrib/render"
+)
+
+type LoginInfo struct {
+	Token string `json:"token"`
+}
+
+func APIHandler(conf *Config) http.Handler {
+	r := martini.NewRouter()
+	m := martini.New()
+	m.Use(martini.Logger())
+	m.Use(martini.Recovery())
+	m.Use(render.Renderer(render.Options{
+		Directory:  "app/build",
+		Extensions: []string{".html"},
+	}))
+	m.Action(r.Handle)
+
+	m.Map(conf)
+
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:     []string{conf.InterfaceURL},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"Authorization", "Accept", "Content-Type", "If-Match", "If-None-Match"},
+		ExposeHeaders:    []string{"ETag"},
+		AllowCredentials: true,
+		MaxAge:           time.Hour,
+	}))
+
+	r.Group(conf.PathPrefix, func(r martini.Router) {
+		m.Use(reqHelperMiddleware)
+		r.Post("/user/sessions", binding.Json(LoginInfo{}), login)
+		r.Delete("/user/session", logout)
+
+		r.Get("/config", getConfig)
+
+		r.Any("/assets/application.*.js", serveApplicationJs)
+
+		r.Any("/assets.*", martini.Static("app/build/assets", martini.StaticOptions{
+			Prefix: "/assets",
+		}))
+
+		r.Get("/.*", func(r render.Render) {
+			r.HTML(200, "dashboard", "")
+		})
+	})
+
+	return m
+}
+
+func requireUserMiddleware(rh RequestHelper) {
+	if !rh.IsAuthenticated() {
+		rh.WriteHeader(401)
+	}
+}
+
+func login(req *http.Request, w http.ResponseWriter, info LoginInfo, rh RequestHelper, conf *Config) {
+	if len(info.Token) != len(conf.LoginToken) || subtle.ConstantTimeCompare([]byte(info.Token), []byte(conf.LoginToken)) != 1 {
+		rh.Error(ErrInvalidLoginToken)
+		return
+	}
+	rh.SetAuthenticated()
+	rh.WriteHeader(200)
+}
+
+func logout(req *http.Request, w http.ResponseWriter, rh RequestHelper) {
+	rh.UnsetAuthenticated()
+	rh.WriteHeader(200)
+}
+
+type OAuthToken struct {
+	AccessToken string `json:"access_token"`
+}
+
+type ExpandedUser struct {
+	Auths         map[string]*OAuthToken `json:"auths"`
+	ControllerKey string                 `json:"controller_key"`
+}
+
+type UserConfig struct {
+	User *ExpandedUser `json:"user,omitempty"`
+
+	Endpoints map[string]string `json:"endpoints"`
+}
+
+var baseConfig = UserConfig{
+	Endpoints: map[string]string{
+		"login":  "/user/sessions",
+		"logout": "/user/session",
+	},
+}
+
+func getConfig(rh RequestHelper, conf *Config) {
+	config := baseConfig
+
+	config.Endpoints["cluster_controller"] = fmt.Sprintf("http://%s", conf.ClusterDomain)
+
+	if rh.IsAuthenticated() {
+		config.User = &ExpandedUser{}
+		config.User.Auths = make(map[string]*OAuthToken)
+
+		if conf.GithubToken != "" {
+			config.User.Auths["github"] = &OAuthToken{AccessToken: conf.GithubToken}
+		}
+
+		config.User.ControllerKey = conf.ControllerKey
+	}
+
+	rh.JSON(200, config)
+}
+
+func serveApplicationJs(res http.ResponseWriter, req *http.Request, conf *Config) {
+	file := filepath.Join("app/build/assets", filepath.Base(req.URL.Path))
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+		res.WriteHeader(500)
+		return
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	jsConf := strings.NewReader(fmt.Sprintf(`
+    window.FlynnDashboardConfig = {
+      API_SERVER: "%s",
+      PATH_PREFIX: "%s"
+    };
+  `, conf.URL, conf.PathPrefix))
+
+	r := ioutil.NewMultiReadSeeker(jsConf, f)
+
+	http.ServeContent(res, req, file, fi.ModTime(), r)
+}
---
+++ dashboard/config.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path"
+
+	"github.com/gorilla/sessions"
+)
+
+type Config struct {
+	Addr          string
+	ClusterDomain string
+	ControllerKey string
+	URL           string
+	InterfaceURL  string
+	PathPrefix    string
+	CookiePath    string
+	SecureCookies bool
+	LoginToken    string
+	GithubToken   string
+	SessionStore  *sessions.CookieStore
+}
+
+func LoadConfigFromEnv() *Config {
+	conf := &Config{}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	conf.Addr = ":" + port
+
+	conf.ClusterDomain = os.Getenv("CLUSTER_DOMAIN")
+	if conf.ClusterDomain == "" {
+		log.Fatal("CLUSTER_DOMAIN is required!")
+	}
+
+	conf.ControllerKey = os.Getenv("CONTROLLER_KEY")
+	if conf.ControllerKey == "" {
+		log.Fatal("CONTROLLER_KEY is required!")
+	}
+
+	conf.URL = os.Getenv("URL")
+	if conf.URL == "" {
+		log.Fatal("URL is required!")
+	}
+	conf.InterfaceURL = conf.URL
+
+	sessionSecret := os.Getenv("SESSION_SECRET")
+	if sessionSecret == "" {
+		log.Fatal("SESSION_SECRET is required!")
+	}
+	conf.SessionStore = sessions.NewCookieStore([]byte(sessionSecret))
+
+	pathPrefix := path.Clean(os.Getenv("PATH_PREFIX"))
+	conf.CookiePath = pathPrefix
+	if pathPrefix == "/" || pathPrefix == "." {
+		pathPrefix = ""
+		conf.CookiePath = "/"
+	}
+	conf.PathPrefix = pathPrefix
+
+	conf.SecureCookies = os.Getenv("SECURE_COOKIES") != ""
+
+	conf.LoginToken = os.Getenv("LOGIN_TOKEN")
+	if conf.LoginToken == "" {
+		log.Fatal("LOGIN_TOKEN is required")
+	}
+
+	conf.GithubToken = os.Getenv("GITHUB_TOKEN")
+
+	return conf
+}
