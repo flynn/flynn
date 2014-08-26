8d45970a4452a8f536c37ad880ee35931d8a9915
Jesse Stuart <jesse@jessestuart.ca>
2014-08-26 18:15:01 -0400

    Refactor
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- dashboard-web-server/main.go
+++ dashboard-web-server/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
+
+	ct "github.com/flynn/flynn/controller/types"
 )
+
+var ErrNotFound = errors.New("common: not found")
+var ErrInvalidLoginToken = ct.ValidationError{Field: "token", Message: "Incorrect token"}
+
+type ServerError struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	conf := LoadConfigFromEnv()
