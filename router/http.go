c6d369c112ec06cc1cf200db9577077c5a3e9f2b
Michael Elsdörfer <michael@elsdoerfer.com>
2014-09-02 14:19:06 +0200

    Improve logging with sub-loggers.

--- router/http.go
+++ router/http.go
@@ -13,6 +13,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -26,7 +27,7 @@ import (
 	log "gopkg.in/inconshreveable/log15.v2"
 )
 
-var l = log.New("module", "app/server")
+var l = log.New("module", "http")
 
 type HTTPListener struct {
 	Watcher
@@ -90,6 +91,9 @@ func (s *HTTPListener) Close() error {
 }
 
 func (s *HTTPListener) Start() error {
+	l.SetHandler(log.MultiHandler(
+		log.StreamHandler(os.Stderr, log.TerminalFormat())))
+
 	started := make(chan error)
 
 	go s.ds.Sync(&httpSyncHandler{l: s}, started)
@@ -284,13 +288,13 @@ func fail(sc *httputil.ServerConn, req *http.Request, code int, msg string) {
 	sc.Write(req, resp)
 }
 
-// TODO: create a new logger for the request, pass around..
 func (s *HTTPListener) handle(conn net.Conn, isTLS bool) {
+	var connlog = l.New("conn", &conn, "raddr", conn.RemoteAddr())
 	defer func() {
-		log.Debug("End: Closing client connection", "conn", conn)
+		connlog.Debug("Closing client connection")
 		conn.Close()
 	}()
-	log.Debug("New client connection", "conn", conn, "tls", isTLS)
+	connlog.Debug("New client connection", "tls", isTLS)
 
 	var r *httpRoute
 
@@ -298,25 +302,25 @@ func (s *HTTPListener) handle(conn net.Conn, isTLS bool) {
 	// At this stage, if we don't find a match, we simply
 	// close the connection down.
 	if isTLS {
-		log.Debug("Going to determine domain via SNI", "conn", conn)
+		connlog.Debug("Going to determine domain via SNI")
 
 		// Parse out host via SNI first
 		vhostConn, err := vhost.TLS(conn)
 		if err != nil {
-			log.Warn("Failed to decode TLS connection", err)
+			connlog.Warn("Failed to decode TLS connection", err)
 			return
 		}
 		host := vhostConn.Host()
-		log.Debug("SNI host of the request found", "host", host)
+		connlog.Debug("SNI host of the request found")
 
 		// Find a backend for the key
 		r = s.findRouteForHost(host)
 		if r == nil {
-			log.Debug("The domain is not configured in strowger", "host", host)
+			connlog.Debug("SNI domain is not configured in strowger", "host", host)
 			return
 		}
 		if r.keypair == nil {
-			log.Info("Cannot serve TLS, no certificate defined for this domain")
+			connlog.Info("TLS client connection, but no certificate defined for SNI domain", "host", host)
 			return
 		}
 
@@ -327,11 +331,11 @@ func (s *HTTPListener) handle(conn net.Conn, isTLS bool) {
 
 	sc := httputil.NewServerConn(conn, nil)
 	for {
-		log.Debug("Reading next request from connection", "conn", conn)
+		connlog.Debug("Reading next request from client")
 		req, err := sc.Read()
 		if err != nil {
 			if err != io.EOF && err != httputil.ErrPersistEOF {
-				log.Error("Error reading client request", "err", err)
+				connlog.Error("Error reading client request", "err", err)
 			}
 			return
 		}
@@ -339,14 +343,16 @@ func (s *HTTPListener) handle(conn net.Conn, isTLS bool) {
 		if !isTLS {
 			r = s.findRouteForHost(req.Host)
 			if r == nil {
-				log.Debug("The domain is not configured in strowger", "host", req.Host)
+				connlog.Debug("Domain from Host header is not configured in strowger", "host", req.Host)
 				fail(sc, req, 404, "Not Found")
 				continue
 			}
 		}
 
+		var reqlog = connlog.New("req", &req, "srv", r.service.name)
+
 		req.RemoteAddr = conn.RemoteAddr().String()
-		if r.service.handle(req, sc, isTLS, r.Sticky) {
+		if r.service.handle(req, sc, isTLS, r.Sticky, reqlog) {
 			return
 		}
 	}
@@ -382,12 +388,12 @@ func (s *httpService) getBackend() *httputil.ClientConn {
 func (s *httpService) connectBackend() (*httputil.ClientConn, string) {
 	for _, addr := range shuffle(s.ss.Addrs()) {
 		// TODO: set connection timeout
-		log.Debug("Dialing the backend", "addr", addr)
+		l.Debug("Dialing the backend", "addr", addr)
 		backend, err := net.Dial("tcp", addr)
 		if err != nil {
 			// TODO: limit number of backends tried
 			// TODO: temporarily quarantine failing backends
-			log.Error("Error dialing the backend", "err", err)
+			l.Error("Error dialing the backend", "err", err)
 			continue
 		}
 		return httputil.NewClientConn(backend, nil), addr
@@ -454,12 +460,14 @@ func (s *httpService) getBackendSticky(req *http.Request) (*httputil.ClientConn,
 	return httputil.NewClientConn(backend, nil), nil
 }
 
-func (s *httpService) handle(req *http.Request, sc *httputil.ServerConn, tls, sticky bool) (done bool) {
-	log.Debug("Begin: request is matched to service, try to process")
+func (s *httpService) handle(req *http.Request, sc *httputil.ServerConn, tls, sticky bool, reqlog log.Logger) (done bool) {
+	reqlog.Debug("Begin processing request")
+	defer func() { reqlog.Debug("Done processing request") }()
 
 	req.Header.Set("X-Request-Start", strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10))
 	req.Header.Set("X-Request-Id", random.UUID())
 
+	reqlog.Debug("Select backend for service")
 	var backend *httputil.ClientConn
 	var stickyCookie *http.Cookie
 	if sticky {
@@ -468,7 +476,7 @@ func (s *httpService) handle(req *http.Request, sc *httputil.ServerConn, tls, st
 		backend = s.getBackend()
 	}
 	if backend == nil {
-		log.Warn("No backend for this service is online")
+		reqlog.Warn("No backend for this service is online")
 		fail(sc, req, 503, "Service Unavailable")
 		return
 	}
@@ -502,14 +510,14 @@ func (s *httpService) handle(req *http.Request, sc *httputil.ServerConn, tls, st
 	}
 	// TODO: Set X-Forwarded-Port
 
-	log.Debug("Starting I/O for request", "req", req)
+	reqlog.Debug("Starting I/O for request")
 	if err := backend.Write(req); err != nil {
-		log.Error("Error writing to backend", "err", err)
+		reqlog.Error("Error writing to backend", "err", err)
 		// TODO: return error to client here
 		return true
 	}
 	res, err := backend.Read(req)
-	log.Debug("Backend returned response", "res", res)
+	reqlog.Debug("Backend returned response")
 	if res != nil {
 		if stickyCookie != nil {
 			res.Header.Add("Set-Cookie", stickyCookie.String())
@@ -519,14 +527,14 @@ func (s *httpService) handle(req *http.Request, sc *httputil.ServerConn, tls, st
 		}
 		if err := sc.Write(req, res); err != nil {
 			if err != io.EOF && err != httputil.ErrPersistEOF {
-				log.Error("Error while writing response to client:", "err", err)
+				reqlog.Error("Error while writing response to client:", "err", err)
 			}
 			return true
 		}
 	}
 	if err != nil {
 		if err != io.EOF && err != httputil.ErrPersistEOF {
-			log.Error("Error while reading response from backend:", "err", err)
+			reqlog.Error("Error while reading response from backend:", "err", err)
 			fail(sc, req, 502, "Bad Gateway")
 		}
 		return
