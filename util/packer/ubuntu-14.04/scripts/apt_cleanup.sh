150ed0ef1433ab843344525b7c4ddc18c98f53e8
Lewis Marshall <lewis@lmars.net>
2014-09-05 18:35:55 +0100

    util/packer: Add EC2 template
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

---
+++ util/packer/ubuntu-14.04/scripts/apt_cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xeo pipefail
+
+echo "cleaning apt cache"
+apt-get autoremove
+apt-get clean
+
+echo "deleting old kernels"
+cur_kernel=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
+kernel_pkg="linux-(image|headers|ubuntu-modules|restricted-modules)"
+meta_pkg="${kernel_pkg}-(generic|i386|server|common|rt|xen|ec2|virtual)"
+apt-get purge -y $(dpkg -l | egrep $kernel_pkg | egrep -v "${cur_kernel}|${meta_pkg}" | awk '{print $2}')
