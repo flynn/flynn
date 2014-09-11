17d3b85a5bc5af37163625a489a844bc49a0d469
Lewis Marshall <lewis@lmars.net>
2014-09-12 00:19:19 +0100

    test: Paginate CI builds
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/runner/assets/builds.js
+++ test/runner/assets/builds.js
@@ -1,4 +1,6 @@
 $(function() {
+  var lastID
+  var count     = 10
   var tableBody = $("table tbody")
   var alertBox  = $(".alert")
   var template  = _.template($("#row-template").html())
@@ -9,11 +11,22 @@ $(function() {
     alertBox.removeClass("hide").find("p").text(msg)
   })
 
-  $.getJSON("/builds", function(builds) {
-    _.each(builds, function(build) {
-      build.created_at = moment(build.created_at)
-      var row = template(build)
-      tableBody.append(row)
+  window.fetch = function() {
+    params = { count: count }
+
+    if(typeof(lastID) != "undefined") {
+      params.before = lastID
+    }
+
+    $.getJSON("/builds", params, function(builds) {
+      _.each(builds, function(build) {
+	lastID = build.id
+	build.created_at = moment(build.created_at)
+	var row = template(build)
+	tableBody.append(row)
+      })
     })
-  })
+  }
+
+  window.fetch()
 })
