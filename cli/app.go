d6f0a6ff5559f6ff470e568c3027d5bd7d4f68c5
Wang Baoling <chinamerp@gmail.com>
2014-09-12 09:56:24 +0800

    receiver: Add app delete to CLI
    Signed-off-by: Wang Baoling <chinamerp@gmail.com>

--- cli/app.go
+++ cli/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 
@@ -16,6 +17,11 @@ usage: flynn create [<name>]
 Create an application in Flynn.
 `)
 
+	register("delete", runDelete, `
+usage: flynn delete [<name>]
+
+Delete an application in Flynn.
+`)
 	register("apps", runApps, `
 usage: flynn apps
 
@@ -37,6 +43,32 @@ func runCreate(args *docopt.Args, client *controller.Client) error {
 	return nil
 }
 
+func runDelete(args *docopt.Args, client *controller.Client) error {
+	appName := args.String["<name>"]
+
+	apps, err := client.AppList()
+	if err != nil {
+		return err
+	}
+	var appId string
+	for _, a := range apps {
+		if a.Name == appName {
+			appId = a.ID
+			break
+		}
+	}
+	if appId == "" {
+		return fmt.Errorf("No found app:%s, check the app name with flynn apps.", appName)
+	}
+
+	if err := client.DeleteApp(appId); err != nil {
+		return err
+	}
+
+	exec.Command("git", "remote", "remove", "flynn").Run()
+	log.Printf("Deleted %s", appName)
+	return nil
+}
 func runApps(args *docopt.Args, client *controller.Client) error {
 	apps, err := client.AppList()
 	if err != nil {
