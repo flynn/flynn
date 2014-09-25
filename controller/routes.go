6c8ff1ba59a6cf44f1f7dd7f83540cfc0fc0b330
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-09-25 15:34:37 +0200

    controller: Aggregate drain events from all routers.
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- controller/routes.go
+++ controller/routes.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"net/http"
+	"time"
 
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/go-martini/martini"
 	ct "github.com/flynn/flynn/controller/types"
+	"github.com/flynn/flynn/discoverd/client"
+	"github.com/flynn/flynn/pkg/sse"
 	routerc "github.com/flynn/flynn/router/client"
 	"github.com/flynn/flynn/router/types"
 )
@@ -64,28 +67,75 @@ func deleteRoute(route *router.Route, router routerc.Client, r ResponseHelper) {
 	r.WriteHeader(200)
 }
 
-func pauseService(router routerc.Client, pauseReq router.PauseReq, params martini.Params, r ResponseHelper, req *http.Request) {
-	err := router.PauseService(params["service_type"], params["service_name"], pauseReq.Paused)
+func pauseService(pauseReq router.PauseReq, params martini.Params, r ResponseHelper, req *http.Request) {
+	services, err := discoverd.Services("router-api", time.Second)
 	if err != nil {
 		r.Error(err)
 		return
 	}
+	done := make(chan error, len(services))
+	for _, service := range services {
+		go func() {
+			router := routerc.NewWithAddr(service.Addr)
+			defer router.Close()
+			if err := router.PauseService(params["service_type"], params["service_name"], pauseReq.Paused); err != nil {
+				done <- err
+				return
+			}
+			done <- nil
+		}()
+	}
+	for i := 0; i < len(services); i++ {
+		err := <-done
+		if err != nil {
+			r.Error(err)
+			return
+		}
+	}
 	r.WriteHeader(200)
 }
 
-func streamServiceDrain(req *http.Request, params martini.Params, router routerc.Client, r ResponseHelper, w http.ResponseWriter) {
-	stream, err := router.StreamServiceDrain(params["service_type"], params["service_name"])
-	defer stream.Close()
+func streamServiceDrain(req *http.Request, params martini.Params, r ResponseHelper, w http.ResponseWriter) {
+	services, err := discoverd.Services("router-api", time.Second)
 	if err != nil {
 		r.Error(err)
 		return
 	}
 	w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
-	w.WriteHeader(200)
 	if wf, ok := w.(http.Flusher); ok {
 		wf.Flush()
 	}
-	if _, err := io.Copy(w, stream); err != nil {
-		r.Error(err)
+	done := make(chan error, len(services))
+	for _, service := range services {
+		go func() {
+			router := routerc.NewWithAddr(service.Addr)
+			defer router.Close()
+			stream, err := router.StreamServiceDrain(params["service_type"], params["service_name"])
+			defer stream.Close()
+			if err != nil {
+				done <- err
+				return
+			}
+			dec := &sse.Reader{bufio.NewReader(stream)}
+			for {
+				line, err := dec.Read()
+				if err != nil {
+					done <- err
+					return
+				}
+				if string(line) == "all" {
+					done <- nil
+					break
+				}
+			}
+		}()
 	}
+	for i := 0; i < len(services); i++ {
+		err := <-done
+		if err != nil {
+			r.Error(err)
+			return
+		}
+	}
+	r.WriteHeader(200)
 }
