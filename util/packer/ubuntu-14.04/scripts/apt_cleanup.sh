3d9ff80f4ab3f1a7311651e29972af878ad06ba7
Lewis Marshall <lewis@lmars.net>
2014-09-29 02:50:43 +0100

    util/packer: Set DEBIAN_FRONTEND=noninteractive
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- util/packer/ubuntu-14.04/scripts/apt_cleanup.sh
+++ util/packer/ubuntu-14.04/scripts/apt_cleanup.sh
@@ -9,4 +9,5 @@ echo "deleting old kernels"
 cur_kernel=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
 kernel_pkg="linux-(image|headers|ubuntu-modules|restricted-modules)"
 meta_pkg="${kernel_pkg}-(generic|i386|server|common|rt|xen|ec2|virtual)"
+export DEBIAN_FRONTEND=noninteractive
 apt-get purge -y $(dpkg -l | egrep $kernel_pkg | egrep -v "${cur_kernel}|${meta_pkg}" | awk '{print $2}')
