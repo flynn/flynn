7ed3cbf39c06eb0a14e95a5fa077e366588f89bf
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-10-16 21:43:50 +0200

    controller: Add controller client documentation.
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- controller/client/client.go
+++ controller/client/client.go
@@ -1,3 +1,4 @@
+// Package controller provides a client for the controller API.
 package controller
 
 import (
@@ -23,6 +24,8 @@ import (
 	"github.com/flynn/flynn/router/types"
 )
 
+// NewClient creates a new Client pointing at uri and using key for
+// authentication.
 func NewClient(uri, key string) (*Client, error) {
 	if uri == "" {
 		uri = "discoverd+http://flynn-controller"
@@ -51,6 +54,7 @@ func NewClient(uri, key string) (*Client, error) {
 	return c, nil
 }
 
+// NewClientWithPin acts like NewClient, but specifies a TLS pin.
 func NewClientWithPin(uri, key string, pin []byte) (*Client, error) {
 	u, err := url.Parse(uri)
 	if err != nil {
@@ -70,6 +74,7 @@ func NewClientWithPin(uri, key string, pin []byte) (*Client, error) {
 	return c, nil
 }
 
+// Client is a client for the controller API.
 type Client struct {
 	url  string
 	key  string
@@ -80,6 +85,7 @@ type Client struct {
 	dialClose io.Closer
 }
 
+// Close closes the underlying transport connection.
 func (c *Client) Close() error {
 	if c.dialClose != nil {
 		c.dialClose.Close()
@@ -87,6 +93,7 @@ func (c *Client) Close() error {
 	return nil
 }
 
+// ErrNotFound is returned when a resource is not found (HTTP status 404).
 var ErrNotFound = errors.New("controller: not found")
 
 func toJSON(v interface{}) (io.Reader, error) {
@@ -177,12 +184,15 @@ func (c *Client) delete(path string) error {
 	return err
 }
 
+// FormationUpdates is a wrapper around a Chan channel, allowing us to close
+// the stream.
 type FormationUpdates struct {
 	Chan <-chan *ct.ExpandedFormation
 
 	conn net.Conn
 }
 
+// Close closes the underlying stream.
 func (u *FormationUpdates) Close() error {
 	if u.conn == nil {
 		return nil
@@ -190,6 +200,8 @@ func (u *FormationUpdates) Close() error {
 	return u.conn.Close()
 }
 
+// StreamFormations returns a FormationUpdates stream. If since is not nil, only
+// retrieves formation updates since the specified time.
 func (c *Client) StreamFormations(since *time.Time) (*FormationUpdates, *error) {
 	if since == nil {
 		s := time.Unix(0, 0)
@@ -215,26 +227,33 @@ func (c *Client) StreamFormations(since *time.Time) (*FormationUpdates, *error) 
 	return &FormationUpdates{ch, conn}, &client.StreamGo("Controller.StreamFormations", since, ch).Error
 }
 
+// CreateArtifact creates a new artifact.
 func (c *Client) CreateArtifact(artifact *ct.Artifact) error {
 	return c.post("/artifacts", artifact, artifact)
 }
 
+// CreateRelease creates a new release.
 func (c *Client) CreateRelease(release *ct.Release) error {
 	return c.post("/releases", release, release)
 }
 
+// CreateApp creates a new app.
 func (c *Client) CreateApp(app *ct.App) error {
 	return c.post("/apps", app, app)
 }
 
+// DeleteApp deletes an app.
 func (c *Client) DeleteApp(appID string) error {
 	return c.delete(fmt.Sprintf("/apps/%s", appID))
 }
 
+// CreateProvider cretes a new provider.
 func (c *Client) CreateProvider(provider *ct.Provider) error {
 	return c.post("/providers", provider, provider)
 }
 
+// ProvisionResource uses a provider to provision a new resource for the
+// application. Returns details about the resource.
 func (c *Client) ProvisionResource(req *ct.ResourceReq) (*ct.Resource, error) {
 	if req.ProviderID == "" {
 		return nil, errors.New("controller: missing provider id")
@@ -244,6 +263,7 @@ func (c *Client) ProvisionResource(req *ct.ResourceReq) (*ct.Resource, error) {
 	return res, err
 }
 
+// PutResource updates a resource.
 func (c *Client) PutResource(resource *ct.Resource) error {
 	if resource.ID == "" || resource.ProviderID == "" {
 		return errors.New("controller: missing id and/or provider id")
@@ -251,6 +271,7 @@ func (c *Client) PutResource(resource *ct.Resource) error {
 	return c.put(fmt.Sprintf("/providers/%s/resources/%s", resource.ProviderID, resource.ID), resource, resource)
 }
 
+// PutFormation updates an existing formation.
 func (c *Client) PutFormation(formation *ct.Formation) error {
 	if formation.AppID == "" || formation.ReleaseID == "" {
 		return errors.New("controller: missing app id and/or release id")
@@ -258,6 +279,7 @@ func (c *Client) PutFormation(formation *ct.Formation) error {
 	return c.put(fmt.Sprintf("/apps/%s/formations/%s", formation.AppID, formation.ReleaseID), formation, formation)
 }
 
+// PutJob updates an existing job.
 func (c *Client) PutJob(job *ct.Job) error {
 	if job.ID == "" || job.AppID == "" {
 		return errors.New("controller: missing job id and/or app id")
@@ -265,52 +287,64 @@ func (c *Client) PutJob(job *ct.Job) error {
 	return c.put(fmt.Sprintf("/apps/%s/jobs/%s", job.AppID, job.ID), job, job)
 }
 
+// DeleteJob kills a specific job id under the specified app.
 func (c *Client) DeleteJob(appID, jobID string) error {
 	return c.delete(fmt.Sprintf("/apps/%s/jobs/%s", appID, jobID))
 }
 
+// SetAppRelease sets the specified release as the current release for an app.
 func (c *Client) SetAppRelease(appID, releaseID string) error {
 	return c.put(fmt.Sprintf("/apps/%s/release", appID), &ct.Release{ID: releaseID}, nil)
 }
 
+// GetAppRelease returns the current release of an app.
 func (c *Client) GetAppRelease(appID string) (*ct.Release, error) {
 	release := &ct.Release{}
 	return release, c.get(fmt.Sprintf("/apps/%s/release", appID), release)
 }
 
+// RouteList returns all routes for an app.
 func (c *Client) RouteList(appID string) ([]*router.Route, error) {
 	var routes []*router.Route
 	return routes, c.get(fmt.Sprintf("/apps/%s/routes", appID), &routes)
 }
 
+// GetRoute returns details for the routeID under the specified app.
 func (c *Client) GetRoute(appID string, routeID string) (*router.Route, error) {
 	route := &router.Route{}
 	return route, c.get(fmt.Sprintf("/apps/%s/routes/%s", appID, routeID), route)
 }
 
+// CreateRoute creates a new route for the specified app.
 func (c *Client) CreateRoute(appID string, route *router.Route) error {
 	return c.post(fmt.Sprintf("/apps/%s/routes", appID), route, route)
 }
 
+// DeleteRoute deletes a route under the specified app.
 func (c *Client) DeleteRoute(appID string, routeID string) error {
 	return c.delete(fmt.Sprintf("/apps/%s/routes/%s", appID, routeID))
 }
 
+// GetFormation returns details for the specified formation under app and
+// release.
 func (c *Client) GetFormation(appID, releaseID string) (*ct.Formation, error) {
 	formation := &ct.Formation{}
 	return formation, c.get(fmt.Sprintf("/apps/%s/formations/%s", appID, releaseID), formation)
 }
 
+// GetRelease returns details for the specified release.
 func (c *Client) GetRelease(releaseID string) (*ct.Release, error) {
 	release := &ct.Release{}
 	return release, c.get(fmt.Sprintf("/releases/%s", releaseID), release)
 }
 
+// GetArtifact returns details for the specified artifact.
 func (c *Client) GetArtifact(artifactID string) (*ct.Artifact, error) {
 	artifact := &ct.Artifact{}
 	return artifact, c.get(fmt.Sprintf("/artifacts/%s", artifactID), artifact)
 }
 
+// GetApp returns details for the specified app.
 func (c *Client) GetApp(appID string) (*ct.App, error) {
 	app := &ct.App{}
 	return app, c.get(fmt.Sprintf("/apps/%s", appID), app)
@@ -334,15 +368,19 @@ func (dec *sseDecoder) Decode(v interface{}) error {
 	}
 }
 
+// JobEventStream is a wrapper around an Events channel, allowing us to close
+// the stream.
 type JobEventStream struct {
 	Events chan *ct.JobEvent
 	body   io.ReadCloser
 }
 
+// Close closes the underlying stream.
 func (s *JobEventStream) Close() {
 	s.body.Close()
 }
 
+// StreamJobEvents returns a JobEventStream for an app.
 func (c *Client) StreamJobEvents(appID string) (*JobEventStream, error) {
 	res, err := c.rawReq("GET", fmt.Sprintf("/apps/%s/jobs", appID), http.Header{"Accept": []string{"text/event-stream"}}, nil, nil)
 	if err != nil {
@@ -363,6 +401,9 @@ func (c *Client) StreamJobEvents(appID string) (*JobEventStream, error) {
 	return stream, nil
 }
 
+// GetJobLog returns a ReadCloser stream of the job with id of jobID, running
+// under appID. If tail is true, new log lines are streamed after the buffered
+// log.
 func (c *Client) GetJobLog(appID, jobID string, tail bool) (io.ReadCloser, error) {
 	path := fmt.Sprintf("/apps/%s/jobs/%s/log", appID, jobID)
 	if tail {
@@ -375,6 +416,9 @@ func (c *Client) GetJobLog(appID, jobID string, tail bool) (io.ReadCloser, error
 	return res.Body, nil
 }
 
+// RunJobAttached runs a new job under the specified app, attaching to the job
+// and returning a ReadWriteCloser stream, which can then be used for
+// communicating with the job.
 func (c *Client) RunJobAttached(appID string, job *ct.NewJob) (utils.ReadWriteCloser, error) {
 	data, err := toJSON(job)
 	if err != nil {
@@ -396,35 +440,43 @@ func (c *Client) RunJobAttached(appID string, job *ct.NewJob) (utils.ReadWriteCl
 	return rwc, nil
 }
 
+// RunJobDetached runs a new job under the specified app, returning the job's
+// details.
 func (c *Client) RunJobDetached(appID string, req *ct.NewJob) (*ct.Job, error) {
 	job := &ct.Job{}
 	return job, c.post(fmt.Sprintf("/apps/%s/jobs", appID), req, job)
 }
 
+// JobList returns a list of all jobs.
 func (c *Client) JobList(appID string) ([]*ct.Job, error) {
 	var jobs []*ct.Job
 	return jobs, c.get(fmt.Sprintf("/apps/%s/jobs", appID), &jobs)
 }
 
+// AppList returns a list of all apps.
 func (c *Client) AppList() ([]*ct.App, error) {
 	var apps []*ct.App
 	return apps, c.get("/apps", &apps)
 }
 
+// KeyList returns a list of all ssh public keys added.
 func (c *Client) KeyList() ([]*ct.Key, error) {
 	var keys []*ct.Key
 	return keys, c.get("/keys", &keys)
 }
 
+// CreateKey uploads pubKey as the ssh public key.
 func (c *Client) CreateKey(pubKey string) (*ct.Key, error) {
 	key := &ct.Key{}
 	return key, c.post("/keys", &ct.Key{Key: pubKey}, key)
 }
 
+// DeleteKey deletes a key with the specified id.
 func (c *Client) DeleteKey(id string) error {
 	return c.delete("/keys/" + strings.Replace(id, ":", "", -1))
 }
 
+// ProviderList returns a list of all providers.
 func (c *Client) ProviderList() ([]*ct.Provider, error) {
 	var providers []*ct.Provider
 	return providers, c.get("/providers", &providers)
