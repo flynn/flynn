5495bbcccb9b67f7e1f3ff3813d0beaf2c510d8d
Corey Powell <mistdragon100@gmail.com>
2014-10-29 11:26:13 -0500

    script: quote flip flop
    Signed-off-by: Corey Powell <mistdragon100@gmail.com>

--- script/lib/aws.sh
+++ script/lib/aws.sh
@@ -13,10 +13,10 @@ sync_cloudfront() {
   # the sync command will output another command to check CloudFront invalidation, so we need to capture that
   cf_cmd=$(s3cmd sync --acl-public --cf-invalidate --no-preserve "${src}" "${dst}" | tee /dev/stderr | grep -oP 's3cmd cfinvalinfo cf://\w+/\w+')
   if [[ "${cf_cmd:0:5}" != "s3cmd" ]]; then
-    fail "could not determine CloudFront invalidation command"
+    fail 'could not determine CloudFront invalidation command'
   fi
 
-  info "waiting for CloudFront invalidation (can take ~10mins)"
+  info 'waiting for CloudFront invalidation (can take ~10mins)'
   while true; do
     local status="$($cf_cmd | grep '^Status' | awk '{print $2}')"
     if [[ "${status}" = "Completed" ]]; then
@@ -25,5 +25,5 @@ sync_cloudfront() {
     info "invalidation status is currently ${status}, waiting 10s"
     sleep 10
   done
-  info "CloudFront invalidation complete"
+  info 'CloudFront invalidation complete'
 }
