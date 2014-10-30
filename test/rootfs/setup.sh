cf2e000f54724a3b3e20b5ff3525f6b65597eff4
Lewis Marshall <lewis@lmars.net>
2014-10-30 23:51:22 +0000

    test: Remove Docker version constraint
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/rootfs/setup.sh
+++ test/rootfs/setup.sh
@@ -73,7 +73,7 @@ EOF
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y lxc-docker-0.10.0 aufs-tools apparmor
+apt-get install -y lxc-docker aufs-tools apparmor
 
 # install flynn build dependencies
 apt-get install -y software-properties-common
