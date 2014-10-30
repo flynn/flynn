3fe4e48b317c7512fca519736d38eef197caf77c
Lewis Marshall <lewis@lmars.net>
2014-10-30 21:35:23 +0000

    test,util/packer: Update Docker apt repository
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/rootfs/setup.sh
+++ test/rootfs/setup.sh
@@ -71,7 +71,7 @@ EOF
 # install docker
 # apparmor is required - see https://github.com/dotcloud/docker/issues/4734
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y lxc-docker-0.10.0 aufs-tools apparmor
 
