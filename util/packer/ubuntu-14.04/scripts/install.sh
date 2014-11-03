6565ac06540682db29973445707f2247fb377437
Lewis Marshall <lewis@lmars.net>
2014-11-03 02:01:02 +0000

    util/packer: Remove dhclient pre-up sleep patch
    This was causing the network to not come up with errors like the
    following in the boot log:
    
    cloud-init-nonet [20.93] waiting 120 seconds network device
    cloud-init-nonet [148.93] gave up waiting for network device
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- util/packer/ubuntu-14.04/scripts/install.sh
+++ util/packer/ubuntu-14.04/scripts/install.sh
@@ -205,9 +205,6 @@ net_cleanup() {
   # Removing leftover leases and persistent rules
   echo "cleaning up dhcp leases"
   rm /var/lib/dhcp/*
-
-  echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
-  echo "pre-up sleep 2" >> /etc/network/interfaces
 }
 
 compress() {
