bf7c87090c055a9456d219685926542022ac6488
Jonathan Rudenberg <jonathan@titanous.com>
2014-11-28 12:35:02 -0500

    flannel: Add build scripts
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

---
+++ flannel/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eo pipefail
+
+commit=1ed42bdc0d370815c02dd4f54373357917f37b98
+dir=flannel-${commit}
+tmpdir=$(mktemp --directory)
+
+cleanup() {
+  rm -rf "${tmpdir}"
+}
+trap cleanup EXIT
+
+mkdir -p bin
+pushd "${tmpdir}" >/dev/null
+curl -L "https://github.com/flynn/flannel/archive/${commit}.tar.gz" | tar xz
+cd "${dir}"
+./build
+popd >/dev/null
+
+cp "${tmpdir}/${dir}/bin/flanneld" bin/
