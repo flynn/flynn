4448ee48983bd99e4e78754a128f4241096ece82
Jesse Stuart <jesse@jessestuart.ca>
2014-11-29 17:01:57 -0500

    controller/examples: ResourceReq.Config is required
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- controller/examples/examples.go
+++ controller/examples/examples.go
@@ -369,8 +369,10 @@ func (e *generator) listProviders() {
 }
 
 func (e *generator) createProviderResource() {
+	resourceConfig := json.RawMessage(`{}`)
 	resourceReq := &ct.ResourceReq{
 		ProviderID: e.resourceIds["provider"],
+		Config:     &resourceConfig,
 	}
 	resource, err := e.client.ProvisionResource(resourceReq)
 	if err != nil {
