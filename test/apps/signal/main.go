6850506a09407fe65943dc1ebc6502f541627236
Lewis Marshall <lewis@lmars.net>
2014-12-09 16:00:45 +0000

    test: Add logging to test/apps/signal
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/apps/signal/main.go
+++ test/apps/signal/main.go
@@ -13,11 +13,15 @@ import (
 const service = "signal-service"
 
 func main() {
+	log.SetFlags(log.Lmicroseconds)
 	ch := make(chan os.Signal)
+	log.Println("setting signal handler")
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("registering service")
 	if err := discoverd.Register(service, ":12345"); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("waiting for signal")
 	sig := <-ch
 	fmt.Printf("got signal: %s", sig)
 }
