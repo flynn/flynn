73adbd7ea9ac72f961b0c27e69187ab3cdd0e173
Jonathan Rudenberg <jonathan@titanous.com>
2014-12-15 22:57:22 -0500

    util/packer: Check certificate when downloading vagrant insecure key
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- util/packer/ubuntu-14.04/scripts/install.sh
+++ util/packer/ubuntu-14.04/scripts/install.sh
@@ -66,7 +66,7 @@ install_vagrant_ssh_key() {
   if [[ ! -f /home/vagrant/.ssh/authorized_keys ]]; then
     mkdir /home/vagrant/.ssh
     chmod 700 /home/vagrant/.ssh
-    wget --no-check-certificate ${pub} \
+    wget ${pub} \
       -O /home/vagrant/.ssh/authorized_keys
     chmod 600 /home/vagrant/.ssh/authorized_keys
     chown -R vagrant /home/vagrant/.ssh
