8eb82a4b8805ccd792d4ec098d03a5a2c3808b58
Eric Myhre <eric@flynn.io>
2014-12-19 20:07:03 -0600

    host: Split apart volume snapshotting tests.
    Also specifically test over alterations to both sides of a cloning operation.
    
    Signed-off-by: Eric Myhre <eric@flynn.io>

--- host/volume/zfs/zfs_basics_test.go
+++ host/volume/zfs/zfs_basics_test.go
@@ -36,7 +36,7 @@ okay, minutes.  ugh.
 
 //
 
-func (S) TestCoolerThings(c *C) {
+func (S) TestSnapshotShouldCarryFiles(c *C) {
 	err := WithTmpfileZpool("testpool", func() error {
 		provider, err := NewProvider("testpool")
 		if err != nil {
@@ -66,6 +66,86 @@ func (S) TestCoolerThings(c *C) {
 		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{"alpha"})
 		// the newly mounted snapshot in the new location should have the same content:
 		c.Assert(v2.(*zfsVolume).basemount, DirContains, []string{"alpha"})
+
+		return nil
+	})
+	c.Assert(err, IsNil)
+}
+
+func (S) TestSnapshotShouldIsolateNewChangesToSource(c *C) {
+	err := WithTmpfileZpool("testpool", func() error {
+		provider, err := NewProvider("testpool")
+		if err != nil {
+			return err
+		}
+
+		v, err := provider.NewVolume()
+		if err != nil {
+			return err
+		}
+
+		// a new volume should start out empty:
+		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{})
+
+		_, err = os.Create(filepath.Join(v.(*zfsVolume).basemount, "alpha"))
+		c.Assert(err, IsNil)
+
+		// sanity check, can we so much as even write a file:
+		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{"alpha"})
+
+		v2, err := v.TakeSnapshot()
+		if err != nil {
+			return err
+		}
+
+		// write another file to the source
+		_, err = os.Create(filepath.Join(v.(*zfsVolume).basemount, "beta"))
+		c.Assert(err, IsNil)
+
+		// the source dir should contain our changes:
+		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{"alpha", "beta"})
+		// the snapshot should be unaffected:
+		c.Assert(v2.(*zfsVolume).basemount, DirContains, []string{"alpha"})
+
+		return nil
+	})
+	c.Assert(err, IsNil)
+}
+
+func (S) TestSnapshotShouldIsolateNewChangesToFork(c *C) {
+	err := WithTmpfileZpool("testpool", func() error {
+		provider, err := NewProvider("testpool")
+		if err != nil {
+			return err
+		}
+
+		v, err := provider.NewVolume()
+		if err != nil {
+			return err
+		}
+
+		// a new volume should start out empty:
+		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{})
+
+		_, err = os.Create(filepath.Join(v.(*zfsVolume).basemount, "alpha"))
+		c.Assert(err, IsNil)
+
+		// sanity check, can we so much as even write a file:
+		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{"alpha"})
+
+		v2, err := v.TakeSnapshot()
+		if err != nil {
+			return err
+		}
+
+		// write another file to the fork
+		_, err = os.Create(filepath.Join(v2.(*zfsVolume).basemount, "beta"))
+		c.Assert(err, IsNil)
+
+		// the source dir should be unaffected:
+		c.Assert(v.(*zfsVolume).basemount, DirContains, []string{"alpha"})
+		// the snapshot should contain our changes:
+		c.Assert(v2.(*zfsVolume).basemount, DirContains, []string{"alpha", "beta"})
 
 		return nil
 	})
