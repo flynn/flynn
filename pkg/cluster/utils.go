855ff39c17230292427ab0f43083b6e55de87bc7
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-01-02 22:09:08 +0900

    pkg/cluster: Move RandomJobID to utils.
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- pkg/cluster/utils.go
+++ pkg/cluster/utils.go
@@ -3,6 +3,8 @@ package cluster
 import (
 	"errors"
 	"strings"
+
+	"github.com/flynn/flynn/pkg/random"
 )
 
 // ParseJobID splits a compound job ID into its host and job ID components
@@ -14,3 +16,6 @@ func ParseJobID(id string) (string, string, error) {
 	}
 	return ids[0], ids[1], nil
 }
+
+// RandomJobID returns a random job identifier with an optional prefix.
+func RandomJobID(prefix string) string { return prefix + random.UUID() }
