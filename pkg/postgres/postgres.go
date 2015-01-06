49ecb67005048bf9a1eb1ad63c92c6ab0b0282d1
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-01-06 12:00:19 +0900

    postgres: Provide db.Addr().
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -68,8 +68,9 @@ type DB struct {
 
 	dsnSuffix string
 
-	mtx sync.RWMutex
-	dsn string
+	mtx  sync.RWMutex
+	dsn  string
+	addr string
 
 	stmts map[string]*sql.Stmt
 }
@@ -93,6 +94,7 @@ func (db *DB) followLeader(firstErr chan<- error) {
 		dsn := fmt.Sprintf("host=%s port=%s %s", leader.Host, leader.Port, db.dsnSuffix)
 		db.mtx.Lock()
 		db.dsn = dsn
+		db.addr = leader.Addr
 		db.mtx.Unlock()
 
 		if db.DB == nil {
@@ -113,6 +115,12 @@ func (db *DB) DSN() string {
 	db.mtx.RLock()
 	defer db.mtx.RUnlock()
 	return db.dsn
+}
+
+func (db *DB) Addr() string {
+	db.mtx.RLock()
+	defer db.mtx.RUnlock()
+	return db.addr
 }
 
 func (db *DB) Close() error {
