97da29056484a8d954fefde77b845012101c9c10
Jonathan Rudenberg <jonathan@titanous.com>
2015-01-12 16:32:17 -0500

    discoverd2: Move Instance, Event, and EventKind to client package
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- discoverd2/server/backend.go
+++ discoverd2/server/backend.go
@@ -1,9 +1,11 @@
 package server
 
+import "github.com/flynn/flynn/discoverd2/client"
+
 type Backend interface {
 	AddService(service string) error
 	RemoveService(service string) error
-	AddInstance(service string, inst *Instance) error
+	AddInstance(service string, inst *discoverd.Instance) error
 	RemoveInstance(service, id string) error
 	StartSync() error
 	Close() error
@@ -12,8 +14,8 @@ type Backend interface {
 type SyncHandler interface {
 	AddService(service string)
 	RemoveService(service string)
-	AddInstance(service string, inst *Instance)
+	AddInstance(service string, inst *discoverd.Instance)
 	RemoveInstance(service, id string)
-	SetService(service string, data []*Instance)
+	SetService(service string, data []*discoverd.Instance)
 	ListServices() []string
 }
