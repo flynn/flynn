5f2a80de036ada9acaf1e542281fdb0d3e939954
Jonathan Rudenberg <jonathan@titanous.com>
2015-01-20 11:22:03 -0500

    sdutil: Update for discoverd2
    Removed a few commands that aren’t worth porting.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- sdutil/services.go
+++ sdutil/services.go
@@ -4,39 +4,38 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/flynn/flynn/discoverd/client"
 )
 
-type services struct {
-	clientCmd
+type instances struct {
 	onlyOne *bool
 }
 
-func (cmd *services) Name() string {
-	return "services"
+func (cmd *instances) Name() string {
+	return "instances"
 }
 
-func (cmd *services) DefineFlags(fs *flag.FlagSet) {
-	cmd.onlyOne = fs.Bool("1", false, "only show one service")
+func (cmd *instances) DefineFlags(fs *flag.FlagSet) {
+	cmd.onlyOne = fs.Bool("1", false, "only show one instance")
 }
 
-func (cmd *services) Run(fs *flag.FlagSet) {
+func (cmd *instances) Run(fs *flag.FlagSet) {
 	if fs.Arg(0) == "" {
 		log.Fatal("missing service name argument")
 	}
-	cmd.InitClient(false)
-	services, err := cmd.client.Services(fs.Arg(0), discoverd.DefaultTimeout)
+	instances, err := discoverd.GetInstances(fs.Arg(0), time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if *cmd.onlyOne {
-		if len(services) > 0 {
-			fmt.Println(services[0].Addr)
+		if len(instances) > 0 {
+			fmt.Println(instances[0].Addr)
 		}
 		return
 	}
-	for _, service := range services {
-		fmt.Println(service.Addr)
+	for _, inst := range instances {
+		fmt.Println(inst.Addr)
 	}
 }
