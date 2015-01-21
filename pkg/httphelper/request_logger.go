433d81ea2dd4c8239dba2578171a7dd9ba485d3f
Jesse Stuart <jesse@jessestuart.ca>
2015-01-21 13:21:18 -0500

    httphelper: Update request logger to use log15 and add random request id
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- pkg/httphelper/request_logger.go
+++ pkg/httphelper/request_logger.go
@@ -1,26 +1,23 @@
 package httphelper
 
 import (
-	"log"
 	"net/http"
 	"time"
+
+	log "github.com/flynn/flynn/Godeps/_workspace/src/gopkg.in/inconshreveable/log15.v2"
+	"github.com/flynn/flynn/pkg/random"
 )
 
-func NewRequestLogger(log *log.Logger, handler http.Handler) http.Handler {
+func NewRequestLogger(ctx log.Ctx, handler http.Handler) http.Handler {
+	l := log.New(ctx)
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		start := time.Now()
-		addr := req.Header.Get("X-Real-IP")
-		if addr == "" {
-			addr = req.Header.Get("X-Forwarded-For")
-			if addr == "" {
-				addr = req.RemoteAddr
-			}
-		}
-		log.Printf("Started %s %s for %s", req.Method, req.URL.Path, addr)
+		logger := l.New(log.Ctx{"req_id": random.UUID()})
+		logger.Info("request started", "method", req.Method, "path", req.URL.Path)
 
 		rw := NewResponseWriter(w)
 		handler.ServeHTTP(rw, req)
 
-		log.Printf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start))
+		logger.Info("request completed", "status", rw.Status(), "duration", time.Since(start))
 	})
 }
