44d400ee8406ce15576ae1454ddc5fe9b77da88e
Blake Gentry <blakesgentry@gmail.com>
2015-01-22 14:05:39 -0800

    router: switch fwdProtoHandler test to use gocheck
    All the other tests use gocheck, so the tests for this handler shouldn't
    use the testing package.
    
    Signed-off-by: Blake Gentry <blakesgentry@gmail.com>

--- router/handlers_test.go
+++ router/handlers_test.go
@@ -3,12 +3,13 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"testing"
+
+	. "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-check"
 )
 
 var nopHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
-func TestFwdProtoHandler(t *testing.T) {
+func (s *S) TestFwdProtoHandler(c *C) {
 	prevForwardedProto := "fakeproto"
 	prevForwardedPort := "8080"
 
@@ -17,15 +18,9 @@ func TestFwdProtoHandler(t *testing.T) {
 	request.RemoteAddr = "1.2.3.4:5678"
 	h := fwdProtoHandler{Handler: nopHandler, Proto: "https", Port: "443"}
 	h.ServeHTTP(rec, request)
-	if v := request.Header.Get("X-Forwarded-For"); v != "1.2.3.4" {
-		t.Errorf("want X-Forwarded-For %s, got %s", "1.2.3.4", v)
-	}
-	if v := request.Header.Get("X-Forwarded-Proto"); v != "https" {
-		t.Errorf("want X-Forwarded-Proto %s, got %s", "https", v)
-	}
-	if v := request.Header.Get("X-Forwarded-Port"); v != "443" {
-		t.Errorf("want X-Forwarded-Port %s, got %s", "443", v)
-	}
+	c.Assert(request.Header.Get("X-Forwarded-For"), Equals, "1.2.3.4")
+	c.Assert(request.Header.Get("X-Forwarded-Proto"), Equals, "https")
+	c.Assert(request.Header.Get("X-Forwarded-Port"), Equals, "443")
 
 	// test with headers already set, make sure we append correctly
 	rec = httptest.NewRecorder()
@@ -34,14 +29,6 @@ func TestFwdProtoHandler(t *testing.T) {
 	request.Header.Set("X-Forwarded-Port", prevForwardedPort)
 
 	h.ServeHTTP(rec, request)
-	want := prevForwardedProto + ", https"
-	got := request.Header.Get("X-Forwarded-Proto")
-	if want != got {
-		t.Errorf("want X-Forwarded-Proto %q, got %q", want, got)
-	}
-	want = prevForwardedPort + ", 443"
-	got = request.Header.Get("X-Forwarded-Port")
-	if want != got {
-		t.Errorf("want X-Forwarded-Port %q, got %q", want, got)
-	}
+	c.Assert(request.Header.Get("X-Forwarded-Proto"), Equals, prevForwardedProto+", https")
+	c.Assert(request.Header.Get("X-Forwarded-Port"), Equals, prevForwardedPort+", 443")
 }
