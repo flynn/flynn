feb0cce193d4256994304cc3effa57f1caed9421
Eric Myhre <eric@flynn.io>
2015-01-23 16:27:20 -0800

    test: Add postgres to rootfs.
    Signed-off-by: Eric Myhre <eric@flynn.io>

--- test/scripts/test-unit.sh
+++ test/scripts/test-unit.sh
@@ -5,4 +5,9 @@ util/commit-validator/validate-dco
 
 util/commit-validator/validate-gofmt
 
+export PGHOST=/var/run/postgresql
+sudo service postgresql start
+
 go test -race -cover ./...
+
+sudo service postgresql stop
