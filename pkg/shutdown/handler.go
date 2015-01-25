b1d81f4fb9dc7ff1f1b406b5f2b0ba90c117912c
Eric Myhre <eric@flynn.io>
2015-01-25 11:59:20 -0800

    pkg/shutdown: Make shutdown flag check thread-safe
    Signed-off-by: Eric Myhre <eric@flynn.io>

--- pkg/shutdown/handler.go
+++ pkg/shutdown/handler.go
@@ -7,26 +7,27 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"sync/atomic"
 	"syscall"
 )
 
 var h = newHandler()
 
 type handler struct {
-	Active bool
-
-	mtx   sync.Mutex
-	stack []func()
+	active atomic.Value
+	mtx    sync.Mutex
+	stack  []func()
 }
 
 func newHandler() *handler {
 	h := &handler{}
+	h.active.Store(false)
 	go h.wait()
 	return h
 }
 
 func IsActive() bool {
-	return h.Active
+	return h.active.Load().(bool)
 }
 
 func BeforeExit(f func()) {
@@ -56,7 +57,7 @@ func (h *handler) wait() {
 
 func (h *handler) exit(err error) {
 	h.mtx.Lock()
-	h.Active = true
+	h.active.Store(true)
 	for i := len(h.stack) - 1; i > 0; i-- {
 		h.stack[i]()
 	}
