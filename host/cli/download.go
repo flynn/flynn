d7eac5a2be6314ef47699d9ff6918d6c85bbbbfd
Lewis Marshall <lewis@lmars.net>
2015-01-27 19:36:20 -0800

    host/cli: Download images from a TUF repository
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- host/cli/download.go
+++ host/cli/download.go
@@ -1,28 +1,39 @@
 package cli
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/url"
+	"io"
 	"os"
+	"path/filepath"
 
-	_ "github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/docker/daemon/graphdriver/aufs"
-	_ "github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/docker/daemon/graphdriver/btrfs"
-	_ "github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/docker/daemon/graphdriver/devmapper"
-	_ "github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/docker/daemon/graphdriver/vfs"
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-docopt"
+	tuf "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-tuf/client"
+	tufdata "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-tuf/data"
 	"github.com/flynn/flynn/pinkerton"
-	"github.com/flynn/flynn/pkg/cliutil"
+	"github.com/flynn/flynn/pkg/tufutil"
 )
 
+const rootKeysJSON = `[{"keytype":"ed25519","keyval":{"public":"8c13396bf5e722fc292b3cbe5b6c4947374787e69fc4c2bb9791f060a5394e0f"}}]`
+
+var rootKeys []*tufdata.Key
+
 func init() {
+	if err := json.Unmarshal([]byte(rootKeysJSON), &rootKeys); err != nil {
+		panic("error decoding root keys")
+	}
+
 	Register("download", runDownload, `
-usage: flynn-host download [--driver=<name>] [--root=<path>] [<manifest>]
+usage: flynn-host download [--driver=<name>] [--root=<path>] [--repository=<uri>] [--tuf-db=<path>] [--manifest-dir=<dir>]
 
 Options:
-  -d --driver=<name>  image storage driver [default: aufs]
-  -r --root=<path>    image storage root [default: /var/lib/docker]
+  -d --driver=<name>       image storage driver [default: aufs]
+  -r --root=<path>         image storage root [default: /var/lib/docker]
+  -u --repository=<uri>    image repository URI [default: https://dl.flynn.io/images]
+  -t --tuf-db=<path>       local TUF file [default: /etc/flynn/tuf.db]
+  -m --manifest-dir=<dir>  directory to copy manifests into [default: /etc/flynn]
 
-Download container images listed in a manifest`)
+Download container images from a TUF repository`)
 }
 
 func runDownload(args *docopt.Args) error {
@@ -30,33 +41,63 @@ func runDownload(args *docopt.Args) error {
 		return fmt.Errorf("error creating root dir: %s", err)
 	}
 
-	manifestFile := args.String["<manifest>"]
-	if manifestFile == "" {
-		manifestFile = "/etc/flynn/version.json"
+	// create a TUF client, initialize it if the DB doesn't exist and update it
+	tufDB := args.String["--tuf-db"]
+	needsInit := false
+	if _, err := os.Stat(tufDB); os.IsNotExist(err) {
+		needsInit = true
 	}
-
-	var manifest map[string]string
-	if err := cliutil.DecodeJSONArg(manifestFile, &manifest); err != nil {
-		return err
-	}
-
-	ctx, err := pinkerton.BuildContext(args.String["--driver"], args.String["--root"])
+	local, err := tuf.FileLocalStore(tufDB)
 	if err != nil {
 		return err
 	}
-
-	for image, id := range manifest {
-		parsedURL, err := url.Parse(image)
-		if err != nil {
+	remote, err := tuf.HTTPRemoteStore(args.String["--repository"], nil)
+	if err != nil {
+		return err
+	}
+	client := tuf.NewClient(local, remote)
+	if needsInit {
+		if err := client.Init(rootKeys, len(rootKeys)); err != nil {
 			return err
 		}
-		// Hide login info from printing.
-		parsedURL.User = nil
-		fmt.Printf("Downloading %s %s...\n", parsedURL, id)
-		image += "?id=" + id
-		if err := ctx.PullDocker(image, pinkerton.InfoPrinter(false)); err != nil {
+	}
+	if _, err := client.Update(); err != nil && !tuf.IsLatestSnapshot(err) {
+		return err
+	}
+
+	// pull images from the TUF repo
+	if err := pinkerton.PullImagesWithClient(
+		client,
+		args.String["--repository"],
+		args.String["--driver"],
+		args.String["--root"],
+		pinkerton.InfoPrinter(false),
+	); err != nil {
+		return err
+	}
+
+	// download the host and bootstrap manifests
+	if err := os.MkdirAll(args.String["--manifest-dir"], 0755); err != nil {
+		return fmt.Errorf("error creating manifest dir: %s", err)
+	}
+	for _, path := range []string{"/host-manifest.json", "/bootstrap-manifest.json"} {
+		if err := downloadManifest(client, path, args.String["--manifest-dir"]); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func downloadManifest(client *tuf.Client, path, dir string) error {
+	file, err := tufutil.Download(client, path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	out, err := os.Create(filepath.Join(dir, path))
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(out, file)
+	return err
 }
--- util/release/main.go
+++ util/release/main.go
@@ -9,16 +9,16 @@ func main() {
 
 Usage:
   flynn-release status <commit>
-  flynn-release manifest [--output=<dest>] [--image-url-prefix=<prefix>] [--id-file=<file>] <template>
+  flynn-release manifest [--output=<dest>] [--image-repository=<url>] [--id-file=<file>] <template>
   flynn-release vagrant <url> <checksum> <version> <provider>
   flynn-release amis <version> <ids>
   flynn-release version <version> <commit>
   flynn-release export <manifest> <dir>
 
 Options:
-  -o --output=<dest>              output destination file ("-" for stdout) [default: -]
-  -i --id-file=<file>             JSON file containing ID mappings
-  -s --image-url-prefix=<prefix>  the image URL prefix [default: https://registry.hub.docker.com/flynn]
+  -o --output=<dest>           output destination file ("-" for stdout) [default: -]
+  -i --id-file=<file>          JSON file containing ID mappings
+  -r --image-repository=<url>  the image repository URL [default: https://dl.flynn.io/images]
 `
 	args, _ := docopt.Parse(usage, nil, true, "", false)
 
