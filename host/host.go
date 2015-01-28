cc47b613676b625668a9f6f49474f39026bae0ab
Lewis Marshall <lewis@lmars.net>
2015-01-27 19:36:20 -0800

    host/cli: Download images from a TUF repository
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- host/host.go
+++ host/host.go
@@ -38,7 +38,7 @@ usage: flynn-host daemon [options] [--meta=<KEY=VAL>...]
 
 options:
   --external=IP          external IP of host
-  --manifest=PATH        path to manifest file [default: /etc/flynn-host.json]
+  --manifest=PATH        path to manifest file [default: /etc/flynn/host-manifest.json]
   --state=PATH           path to state file [default: /var/lib/flynn/host-state.bolt]
   --id=ID                host id
   --force                kill all containers booted by flynn-host before starting
