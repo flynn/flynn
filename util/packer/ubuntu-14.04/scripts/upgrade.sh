6612c7a11a651e97358e8537c4540de35128e04e
Jonathan Rudenberg <jonathan@titanous.com>
2015-01-28 10:57:21 -0800

    util/packer,test/rootfs: Install kernel 3.16.0
    The default trusty kernel is 3.13, but the backported “LTS Enablement”
    package is already available and will be the default in 14.04.2
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- util/packer/ubuntu-14.04/scripts/upgrade.sh
+++ util/packer/ubuntu-14.04/scripts/upgrade.sh
@@ -4,6 +4,10 @@ set -xeo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
+apt-get install --install-recommends linux-generic-lts-utopic \
+  -y \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confold"
 apt-get dist-upgrade -y \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confold"
