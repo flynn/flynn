daa93f9c194530ffad8bfb05d18a2b1f3285482a
Eric Myhre <eric@flynn.io>
2015-01-29 15:19:26 -0800

    host,test: Deduplicate test job setup code.
    Along the way, added a "merge" feature to host.ContainerConfig, which makes it easier for functions to assemble relevant pieces of configuration in stages.
    
    Signed-off-by: Eric Myhre <eric@flynn.io>

--- test/test_host.go
+++ test/test_host.go
@@ -8,7 +8,9 @@ import (
 	"time"
 
 	c "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-check"
+	"github.com/flynn/flynn/discoverd/client"
 	"github.com/flynn/flynn/host/types"
+	"github.com/flynn/flynn/pkg/cluster"
 	"github.com/flynn/flynn/pkg/exec"
 	"github.com/flynn/flynn/pkg/random"
 )
@@ -58,14 +60,17 @@ func (s *HostSuite) TestAttachFinishedInteractiveJob(t *c.C) {
 	}
 }
 
-func (s *HostSuite) TestNetworkedPersistentJob(t *c.C) {
-	// this isn't much more impressive than what's already running by the time we've got a cluster engaged
-	// but the idea is to use this basic design to enable testing a series manipulations on a single container.
+/*
+	Make an 'ish' application on the given host, returning it when
+	it has registered readiness with discoverd.
 
-	cluster := s.clusterClient(t)
+	User will want to defer cmd.Kill() to clean up.
+*/
+func makeIshApp(cluster *cluster.Client, h cluster.Host, dc *discoverd.Client, extraConfig host.ContainerConfig) (*exec.Cmd, *discoverd.Instance, error) {
+	// pick a unique string to use as service name so this works with concurrent tests.
+	serviceName := "ish-service-" + random.String(6)
 
 	// run a job that accepts tcp connections and performs tasks we ask of it in its container
-	serviceName := "ish-service-" + random.String(6)
 	cmd := exec.JobUsingCluster(cluster, exec.DockerImage(imageURIs["test-apps"]), &host.Job{
 		Config: host.ContainerConfig{
 			Cmd:   []string{"/bin/ish"},
@@ -73,28 +78,58 @@ func (s *HostSuite) TestNetworkedPersistentJob(t *c.C) {
 			Env: map[string]string{
 				"NAME": serviceName,
 			},
-		},
+		}.Merge(extraConfig),
 	})
-	cmd.Start()
+	cmd.HostID = h.ID()
+	if err := cmd.Start(); err != nil {
+		return nil, nil, err
+	}
+
+	// wait for the job to heartbeat and return its address
+	services, err := dc.Instances(serviceName, time.Second*100)
+	if err != nil {
+		cmd.Kill()
+		return nil, nil, err
+	}
+	if len(services) != 1 {
+		cmd.Kill()
+		return nil, nil, fmt.Errorf("test setup: expected exactly one service instance, got %d", len(services))
+	}
+
+	return cmd, services[0], nil
+}
+
+func runIshCommand(service *discoverd.Instance, cmd string) (string, error) {
+	resp, err := http.Post(
+		fmt.Sprintf("http://%s/ish", service.Addr),
+		"text/plain",
+		strings.NewReader(cmd),
+	)
+	if err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (s *HostSuite) TestNetworkedPersistentJob(t *c.C) {
+	// this isn't much more impressive than what's already running by the time we've got a cluster engaged
+	// but the idea is to use this basic design to enable testing a series manipulations on a single container.
+
+	cluster := s.clusterClient(t)
+
+	// run a job that accepts tcp connections and performs tasks we ask of it in its container
+	cmd, service, err := makeIshApp(cluster, s.anyHostClient(t), s.discoverdClient(t), host.ContainerConfig{})
+	t.Assert(err, c.IsNil)
 	defer cmd.Kill()
 
-	// get the ip:port that that job exposed.
-	// phone discoverd and ask by serviceName -- we set a unique one so this works with concurrent tests.
-	instances, err := s.discoverdClient(t).Instances(serviceName, 15*time.Second)
+	// test that we can interact with that job
+	resp, err := runIshCommand(service, "echo echocococo")
 	t.Assert(err, c.IsNil)
-	t.Assert(instances, c.HasLen, 1)
-
-	resp, err := http.Post(
-		fmt.Sprintf("http://%s/ish", instances[0].Addr),
-		"text/plain",
-		strings.NewReader("echo echocococo"),
-	)
-	t.Assert(err, c.IsNil)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	t.Assert(err, c.IsNil)
-
-	t.Assert(string(body), c.Equals, "echocococo\n")
+	t.Assert(resp, c.Equals, "echocococo\n")
 }
 
 func (s *HostSuite) TestVolumeCreation(t *c.C) {
@@ -123,80 +158,33 @@ func (s *HostSuite) TestVolumePersistence(t *c.C) {
 	vol, err := h.CreateVolume("default")
 	t.Assert(err, c.IsNil)
 
-	// FIXME: yes, there's massive code duplication here.  boil it down later.
-
-	// run a job that accepts tcp connections and performs tasks we ask of it in its container
-	serviceName := "ish-service-" + random.String(6)
-	cmd := exec.JobUsingCluster(cluster, exec.DockerImage(imageURIs["test-apps"]), &host.Job{
-		Config: host.ContainerConfig{
-			Cmd:   []string{"/bin/ish"},
-			Ports: []host.Port{{Proto: "tcp"}},
-			Env: map[string]string{
-				"NAME": serviceName,
-			},
-			Volumes: []host.VolumeBinding{{
-				Target:    "/vol",
-				VolumeID:  vol.ID,
-				Writeable: true,
-			}},
-		},
+	// create first job
+	cmd, service, err := makeIshApp(cluster, h, s.discoverdClient(t), host.ContainerConfig{
+		Volumes: []host.VolumeBinding{{
+			Target:    "/vol",
+			VolumeID:  vol.ID,
+			Writeable: true,
+		}},
 	})
-	cmd.HostID = h.ID()
-	err = cmd.Start()
 	t.Assert(err, c.IsNil)
 	defer cmd.Kill()
-
-	// get the ip:port that that job exposed.
-	// phone discoverd and ask by serviceName -- we set a unique one so this works with concurrent tests.
-	services, err := s.discoverdClient(t).Instances(serviceName, time.Second*100)
+	// add data to the volume
+	resp, err := runIshCommand(service, "echo 'testcontent' > /vol/alpha ; echo $?")
 	t.Assert(err, c.IsNil)
-	t.Assert(services, c.HasLen, 1)
-
-	resp, err := http.Post(
-		fmt.Sprintf("http://%s/ish", services[0].Addr),
-		"text/plain",
-		strings.NewReader("echo 'testcontent' > /vol/alpha ; echo $?"),
-	)
-	t.Assert(err, c.IsNil)
-	body, err := ioutil.ReadAll(resp.Body)
-	t.Assert(err, c.IsNil)
-	t.Assert(string(body), c.Equals, "0\n")
+	t.Assert(resp, c.Equals, "0\n")
 
 	// start another one that mounts the same volume
-	serviceName = "ish-service-" + random.String(6)
-	cmd = exec.JobUsingCluster(cluster, exec.DockerImage(imageURIs["test-apps"]), &host.Job{
-		Config: host.ContainerConfig{
-			Cmd:   []string{"/bin/ish"},
-			Ports: []host.Port{{Proto: "tcp"}},
-			Env: map[string]string{
-				"NAME": serviceName,
-			},
-			Volumes: []host.VolumeBinding{{
-				Target:    "/vol",
-				VolumeID:  vol.ID,
-				Writeable: false,
-			}},
-		},
+	cmd, service, err = makeIshApp(cluster, h, s.discoverdClient(t), host.ContainerConfig{
+		Volumes: []host.VolumeBinding{{
+			Target:    "/vol",
+			VolumeID:  vol.ID,
+			Writeable: false,
+		}},
 	})
-	cmd.HostID = h.ID()
-	err = cmd.Start()
 	t.Assert(err, c.IsNil)
 	defer cmd.Kill()
-
-	// get the ip:port that that job exposed.
-	// phone discoverd and ask by serviceName -- we set a unique one so this works with concurrent tests.
-	services, err = s.discoverdClient(t).Instances(serviceName, time.Second*100)
+	// read data back from the volume
+	resp, err = runIshCommand(service, "cat /vol/alpha")
 	t.Assert(err, c.IsNil)
-	t.Assert(services, c.HasLen, 1)
-
-	resp, err = http.Post(
-		fmt.Sprintf("http://%s/ish", services[0].Addr),
-		"text/plain",
-		strings.NewReader("cat /vol/alpha"),
-	)
-	t.Assert(err, c.IsNil)
-	body, err = ioutil.ReadAll(resp.Body)
-	t.Assert(err, c.IsNil)
-
-	t.Assert(string(body), c.Equals, "testcontent\n")
+	t.Assert(resp, c.Equals, "testcontent\n")
 }
