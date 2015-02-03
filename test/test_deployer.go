9e51590b9bd45869778d1be5a85aea66e054a88b
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-02-03 15:36:13 -0800

    controller/deployer: Test for no downtime during deploys.
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- test/test_deployer.go
+++ test/test_deployer.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	c "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-check"
@@ -13,10 +15,24 @@ type DeployerSuite struct {
 
 var _ = c.Suite(&DeployerSuite{})
 
-func (s *DeployerSuite) createDeployment(t *c.C, process, strategy string) *ct.Deployment {
-	app, release := s.createApp(t)
+func (s *DeployerSuite) createDeployment(t *c.C, app *ct.App, strategy string) *ct.Deployment {
 	app.Strategy = strategy
 	s.controllerClient(t).UpdateApp(app)
+	release, err := s.controllerClient(t).GetAppRelease(app.ID)
+	t.Assert(err, c.IsNil)
+
+	// create a new release for the deployment
+	release.ID = ""
+	t.Assert(s.controllerClient(t).CreateRelease(release), c.IsNil)
+
+	deployment, err := s.controllerClient(t).CreateDeployment(app.ID, release.ID)
+	t.Assert(err, c.IsNil)
+	return deployment
+}
+
+func (s *DeployerSuite) createFormation(t *c.C, app *ct.App, processes map[string]int) *ct.Release {
+	release, err := s.controllerClient(t).GetAppRelease(app.ID)
+	t.Assert(err, c.IsNil)
 
 	jobStream := make(chan *ct.JobEvent)
 	scale, err := s.controllerClient(t).StreamJobEvents(app.Name, 0, jobStream)
@@ -26,18 +42,18 @@ func (s *DeployerSuite) createDeployment(t *c.C, process, strategy string) *ct.D
 	t.Assert(s.controllerClient(t).PutFormation(&ct.Formation{
 		AppID:     app.ID,
 		ReleaseID: release.ID,
-		Processes: map[string]int{process: 2},
+		Processes: processes,
 	}), c.IsNil)
 
-	waitForJobEvents(t, scale, jobStream, jobEvents{process: {"up": 2}})
-
-	// create a new release for the deployment
-	release.ID = ""
-	t.Assert(s.controllerClient(t).CreateRelease(release), c.IsNil)
-
-	deployment, err := s.controllerClient(t).CreateDeployment(app.ID, release.ID)
-	t.Assert(err, c.IsNil)
-	return deployment
+	waitEvents := jobEvents{}
+	for proctype, i := range processes {
+		if proctype == "failer" {
+			continue
+		}
+		waitEvents[proctype] = map[string]int{"up": i}
+	}
+	waitForJobEvents(t, scale, jobStream, waitEvents)
+	return release
 }
 
 func waitForDeploymentEvents(t *c.C, stream chan *ct.DeploymentEvent, expected []*ct.DeploymentEvent) {
@@ -70,7 +86,9 @@ loop:
 }
 
 func (s *DeployerSuite) TestOneByOneStrategy(t *c.C) {
-	deployment := s.createDeployment(t, "printer", "one-by-one")
+	app, _ := s.createApp(t)
+	s.createFormation(t, app, map[string]int{"printer": 2})
+	deployment := s.createDeployment(t, app, "one-by-one")
 	events := make(chan *ct.DeploymentEvent)
 	stream, err := s.controllerClient(t).StreamDeployment(deployment.ID, events)
 	t.Assert(err, c.IsNil)
@@ -93,7 +111,9 @@ func (s *DeployerSuite) TestOneByOneStrategy(t *c.C) {
 }
 
 func (s *DeployerSuite) TestAllAtOnceStrategy(t *c.C) {
-	deployment := s.createDeployment(t, "printer", "all-at-once")
+	app, _ := s.createApp(t)
+	s.createFormation(t, app, map[string]int{"printer": 2})
+	deployment := s.createDeployment(t, app, "all-at-once")
 	events := make(chan *ct.DeploymentEvent)
 	stream, err := s.controllerClient(t).StreamDeployment(deployment.ID, events)
 	t.Assert(err, c.IsNil)
@@ -147,4 +167,67 @@ func (s *DeployerSuite) TestRollback(t *c.C) {
 	t.Assert(f.Processes, c.DeepEquals, map[string]int{"crasher": 2})
 	_, err = s.controllerClient(t).GetFormation(deployment.AppID, releaseID)
 	t.Assert(err, c.NotNil)
+}
+
+func (s *DeployerSuite) TestNoDowntime(t *c.C) {
+	app, _ := s.createApp(t)
+	route := "ping-transitioning.dev"
+	s.createFormation(t, app, map[string]int{"ping": 1})
+	t.Assert(flynn(t, "/", "-a", app.Name, "route", "add", "http", "-s", "ping-service", route), Succeeds)
+	_, err := s.discoverdClient(t).Instances("ping-service", 10*time.Second)
+	t.Assert(err, c.IsNil)
+
+	done := make(chan bool)
+	fail := make(chan error)
+
+	go func() {
+	outer:
+		for {
+			select {
+			case <-done:
+				close(fail)
+				break outer
+			default:
+				client := &http.Client{}
+				req, err := http.NewRequest("GET", "http://"+routerIP, nil)
+				if err != nil {
+					fail <- err
+					break outer
+				}
+				req.Host = route
+				res, err := client.Do(req)
+				if err != nil {
+					fail <- err
+					break outer
+				}
+				res.Body.Close()
+				if res.StatusCode != 200 {
+					fail <- fmt.Errorf("Expected res.StatusCode to equal 200, got %s", res.StatusCode)
+					break outer
+				}
+			}
+		}
+	}()
+
+	deployment := s.createDeployment(t, app, "all-at-once")
+	events := make(chan *ct.DeploymentEvent)
+	stream, err := s.controllerClient(t).StreamDeployment(deployment.ID, events)
+	t.Assert(err, c.IsNil)
+	defer stream.Close()
+	releaseID := deployment.NewReleaseID
+	oldReleaseID := deployment.OldReleaseID
+
+	expected := []*ct.DeploymentEvent{
+		{ReleaseID: releaseID, JobType: "ping", JobState: "starting", Status: "running"},
+		{ReleaseID: releaseID, JobType: "ping", JobState: "up", Status: "running"},
+		{ReleaseID: oldReleaseID, JobType: "ping", JobState: "stopping", Status: "running"},
+		{ReleaseID: oldReleaseID, JobType: "ping", JobState: "down", Status: "running"},
+		{ReleaseID: releaseID, JobType: "", JobState: "", Status: "complete"},
+	}
+
+	waitForDeploymentEvents(t, events, expected)
+	close(done)
+	if err := <-fail; err != nil {
+		t.Error(err)
+	}
 }
