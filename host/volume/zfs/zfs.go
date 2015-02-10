102627d4f803a3cb943e5c6cc00c6643ccaf7880
Eric Myhre <eric@flynn.io>
2015-02-10 11:23:54 -0800

    host/volume/zfs: Snapshots and transmission.
    Snapshots can now be transmitted between zfs providers.  Both first-time full data transfer is supported as well as upgrade to incremental transmission where possible.
    
    Signed-off-by: Eric Myhre <eric@flynn.io>

--- host/volume/zfs/zfs.go
+++ host/volume/zfs/zfs.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path"
@@ -250,6 +251,139 @@ func (b *Provider) ForkVolume(vol volume.Volume) (volume.Volume, error) {
 	}
 	b.volumes[id] = v2
 	return v2, nil
+}
+
+type zfsHaves struct {
+	SnapID string `json:"snap_id"`
+}
+
+/*
+	Returns the set of snapshot UIDs available in this volume's backing dataset.
+*/
+func (b *Provider) ListHaves(vol volume.Volume) ([]json.RawMessage, error) {
+	zvol, err := b.owns(vol)
+	if err != nil {
+		return nil, err
+	}
+	snapshots, err := zvol.dataset.Snapshots()
+	if err != nil {
+		return nil, err
+	}
+	res := make([]json.RawMessage, len(snapshots))
+	for i, snapshot := range snapshots {
+		have := &zfsHaves{SnapID: strings.SplitN(snapshot.Name, "@", 2)[1]}
+		serial, err := json.Marshal(have)
+		if err != nil {
+			return nil, err
+		}
+		res[i] = serial
+	}
+	return res, nil
+}
+
+func (b *Provider) SendSnapshot(vol volume.Volume, haves []json.RawMessage, output io.Writer) error {
+	zvol, err := b.owns(vol)
+	if err != nil {
+		return err
+	}
+	if !vol.IsSnapshot() {
+		return fmt.Errorf("can only send a snapshot")
+	}
+	// zfs recv can only really accept snapshots that apply to the current tip
+	var latestRemote string
+	if haves != nil && len(haves) > 0 {
+		have := &zfsHaves{}
+		if err := json.Unmarshal(haves[len(haves)-1], have); err == nil {
+			latestRemote = have.SnapID
+		}
+	}
+	// look for intersection of existing snapshots on this volume; if so do incremental
+	parentName := strings.SplitN(zvol.dataset.Name, "@", 2)[0]
+	parentDataset, err := zfs.GetDataset(parentName)
+	if err != nil {
+		return err
+	}
+	snapshots, err := parentDataset.Snapshots()
+	if err != nil {
+		return err
+	}
+	// we can fly incremental iff the latest snap on the remote is available here
+	useIncremental := false
+	if latestRemote != "" {
+		for _, snap := range snapshots {
+			if strings.SplitN(snap.Name, "@", 2)[1] == latestRemote {
+				useIncremental = true
+				break
+			}
+		}
+	}
+	// at last, send:
+	if useIncremental {
+		sendCmd := exec.Command("zfs", "send", "-i", latestRemote, zvol.dataset.Name)
+		sendCmd.Stdout = output
+		return sendCmd.Run()
+	}
+	return zvol.dataset.SendSnapshot(output)
+}
+
+/*
+	ReceiveSnapshot both accepts a snapshotted filesystem as a byte stream,
+	and applies that state to the given `vol` (i.e., if this were git, it's like
+	`git fetch && git pull` at the same time; regretably, it's pretty hard to get
+	zfs to separate those operations).  If there are local working changes in
+	the volume, they will be overwritten.
+
+	In addition to the given volume being mutated on disk, a reference to the
+	new snapshot will be returned (this can be used for cleanup, though be aware
+	that with zfs, removing snapshots may impact the ability to use incremental
+	deltas when receiving future snapshots).
+
+	Also note that ZFS is *extremely* picky about receiving snapshots; in
+	addition to obvious failure modes like an incremental snapshot with
+	insufficient data, the following complications apply:
+	- Sending an incremental snapshot with too much history will fail.
+	- Sending a full snapshot to a volume with any other snapshots will fail.
+	In the former case, you can renegociate; in the latter, you will have to
+	either *destroy snapshots* or make a new volume.
+*/
+func (b *Provider) ReceiveSnapshot(vol volume.Volume, input io.Reader) (volume.Volume, error) {
+	zvol, err := b.owns(vol)
+	if err != nil {
+		return nil, err
+	}
+	// recv does the right thing with input either fresh or incremental.
+	// recv with the dataset name and no snapshot suffix means the snapshot name from farside is kept;
+	// this is important because though we've assigned it a new UUID, the zfs dataset name match is used for incr hinting.
+	var buf bytes.Buffer
+	recvCmd := exec.Command("zfs", "recv", "-F", zvol.dataset.Name)
+	recvCmd.Stdin = input
+	recvCmd.Stderr = &buf
+	if err := recvCmd.Run(); err != nil {
+		return nil, fmt.Errorf("zfs recv rejected snapshot data: %s (%s)", err, strings.TrimSpace(buf.String()))
+	}
+	// get the dataset reference back; whatever the latest snapshot is must be what we received
+	snapshots, err := zvol.dataset.Snapshots()
+	if err != nil {
+		return nil, err
+	}
+	if len(snapshots) == 0 {
+		// should never happen, unless someone else is racing the zfs controls
+		return nil, fmt.Errorf("zfs recv misplaced snapshot data")
+	}
+	snapds := snapshots[len(snapshots)-1]
+	// reassemble as a flynn volume for return
+	id := random.UUID()
+	snap := &zfsVolume{
+		info:      &volume.Info{ID: id},
+		provider:  zvol.provider,
+		dataset:   snapds,
+		basemount: b.mountPath(id),
+	}
+	if err := b.mountSnapshot(snap); err != nil {
+		return nil, err
+	}
+	b.volumes[id] = snap
+	return snap, nil
 }
 
 func (v *zfsVolume) Provider() volume.Provider {
