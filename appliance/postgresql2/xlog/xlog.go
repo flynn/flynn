4f9b7ed4b700b891e47abd171d55a54ca6b10a01
Jonathan Rudenberg <jonathan@titanous.com>
2015-02-11 19:29:38 -0800

    appliance/postgresql: Implement cluster state machine
    This commit adds a Go translation of the manatee-state-machine
    project[0] from Joyent. It is translated verbatim with the following
    minor differences:
    
    - A few extra tests were added for completeness
    - Changes were to translate from the Node.js async model to a more
      straightforward model that uses channels.
    - The term 'one-node-write-mode' (abbreviated as ONWM) was replaced
      with 'singleton' as they are used interchangeably upstream.
    
    The state machine provides a fault-tolerant system for running
    Postgres clusters that automatically fail over with minimal downtime
    and no possibility for data loss or split brain during failovers.
    
    A simulator is used for testing and is also available in an
    interactive REPL for experimenting with various scenarios.
    
    [0] https://github.com/joyent/manatee-state-machine/tree/d441fe941faddb51d6e6237d792dd4d7fae64cc6
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

---
+++ appliance/postgresql2/xlog/xlog.go
@@ -0,0 +1,104 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This file is derived from:
+// https://github.com/joyent/manatee-state-machine/blob/d441fe941faddb51d6e6237d792dd4d7fae64cc6/lib/xlog.js
+//
+// Copyright (c) 2014, Joyent, Inc.
+// Copyright (c) 2015, Prime Directive, Inc.
+//
+
+/*
+
+Package xlog provides constants and functions for working with PostgreSQL xlog positions.
+
+The package makes a number of assumptions about the format of xlog positions.
+It's not totally clear that this is a committed Postgres interface, but it seems
+to be true.
+
+We assume that postgres xlog positions are represented as strings of the form:
+
+    filepart/offset		e.g., "0/17BB660"
+
+where both "filepart" and "offset" are hexadecimal numbers. xlog position F1/O1
+is at least as new as F2/O2 if (F1 > F2) or (F1 == F2 and O1 >= O2). We try to
+avoid assuming that they're zero-padded (i.e., that a simple string comparison
+might do the right thing). We also don't make any assumptions about the size of
+each file, which means we can't compute the actual difference between two
+positions.
+
+*/
+package xlog
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Position string
+
+const Zero Position = "0/00000000"
+
+// Increment increments an xlog position by the given number.
+func Increment(xlog Position, increment int) (Position, error) {
+	parts, err := parse(xlog)
+	if err != nil {
+		return "", err
+	}
+	return makePosition(parts[0], parts[1]+increment), nil
+}
+
+// Compare compares two xlog positions returning -1 if xlog1 < xlog2, 0 if xlog1
+// == xlog2, and 1 if xlog1 > xlog2.
+func Compare(xlog1, xlog2 Position) (int, error) {
+	p1, err := parse(xlog1)
+	if err != nil {
+		return 0, err
+	}
+	p2, err := parse(xlog2)
+	if err != nil {
+		return 0, err
+	}
+
+	if p1[0] == p2[0] && p1[1] == p2[1] {
+		return 0, nil
+	}
+	if p1[0] > p2[0] || p1[0] == p2[0] && p1[1] > p2[1] {
+		return 1, nil
+	}
+	return -1, nil
+}
+
+// parse takes an xlog position emitted by postgres and returns an array of two
+// integers representing the filepart and offset components of the xlog
+// position. This is an internal representation that should not be exposed
+// outside of this package.
+func parse(xlog Position) (res [2]int, err error) {
+	parts := strings.SplitN(string(xlog), "/", 2)
+	if len(parts) != 2 {
+		err = fmt.Errorf("malformed xlog position %q", xlog)
+		return
+	}
+
+	res[0], err = parseHex(parts[0])
+	if err != nil {
+		return
+	}
+	res[1], err = parseHex(parts[1])
+
+	return
+}
+
+// MakePosition constructs an xlog position string from a numeric file part and
+// offset.
+func makePosition(filepart int, offset int) Position {
+	return Position(fmt.Sprintf("%X/%08X", filepart, offset))
+}
+
+func parseHex(s string) (int, error) {
+	res, err := strconv.ParseInt(s, 16, 64)
+	return int(res), err
+}
