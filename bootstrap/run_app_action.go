8dd60b09f53b85331067fadf4fa559a6f7c5bbd8
Lewis Marshall <lewis@lmars.net>
2015-02-17 03:15:59 +0000

    bootstrap: Remove unused RunJobAction
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- bootstrap/run_app_action.go
+++ bootstrap/run_app_action.go
@@ -2,12 +2,15 @@ package bootstrap
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"sort"
 	"time"
 
 	ct "github.com/flynn/flynn/controller/types"
 	"github.com/flynn/flynn/controller/utils"
+	"github.com/flynn/flynn/host/types"
+	"github.com/flynn/flynn/pkg/cluster"
 	"github.com/flynn/flynn/pkg/random"
 	"github.com/flynn/flynn/pkg/resource"
 	"github.com/flynn/flynn/pkg/schedutil"
@@ -121,4 +124,76 @@ func (a *RunAppAction) Run(s *State) error {
 	}
 
 	return nil
+}
+
+func startJob(s *State, hostID string, job *host.Job) (*Job, error) {
+	cc, err := s.ClusterClient()
+	if err != nil {
+		return nil, err
+	}
+	if hostID == "" {
+		hostID, err = randomHost(cc)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// TODO: filter by tags
+
+	job.ID = cluster.RandomJobID("")
+	data := &Job{HostID: hostID, JobID: job.ID}
+
+	hc, err := cc.DialHost(hostID)
+	if err != nil {
+		return nil, err
+	}
+
+	jobStatus := make(chan error)
+	events := make(chan *host.Event)
+	stream, err := hc.StreamEvents(data.JobID, events)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer stream.Close()
+		for e := range events {
+			switch e.Event {
+			case "start", "stop":
+				jobStatus <- nil
+				return
+			case "error":
+				job, err := hc.GetJob(data.JobID)
+				if err != nil {
+					jobStatus <- err
+					return
+				}
+				if job.Error == nil {
+					jobStatus <- fmt.Errorf("bootstrap: unknown error from host")
+					return
+				}
+				jobStatus <- fmt.Errorf("bootstrap: host error while launching job: %q", *job.Error)
+				return
+			default:
+			}
+		}
+		jobStatus <- fmt.Errorf("bootstrap: host job stream disconnected unexpectedly: %q", stream.Err())
+	}()
+
+	_, err = cc.AddJobs(map[string][]*host.Job{hostID: {job}})
+	if err != nil {
+		return nil, err
+	}
+
+	return data, <-jobStatus
+}
+
+func randomHost(cc *cluster.Client) (string, error) {
+	hosts, err := cc.ListHosts()
+	if err != nil {
+		return "", err
+	}
+	if len(hosts) == 0 {
+		return "", cluster.ErrNoServers
+	}
+	return schedutil.PickHost(hosts).ID, nil
 }
