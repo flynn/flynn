e8d66db2176016c8c6c8ea66cc48b4f249fd9650
Jonathan Rudenberg <jonathan@titanous.com>
2015-02-23 13:31:06 -0800

    appliance/postgresql: Implement postgres runner main and discoverd wrapper
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

---
+++ appliance/postgresql/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/flynn/flynn/Godeps/_workspace/src/gopkg.in/inconshreveable/log15.v2"
+	"github.com/flynn/flynn/appliance/postgresql/state"
+	"github.com/flynn/flynn/discoverd/client"
+	"github.com/flynn/flynn/pkg/httphelper"
+	"github.com/flynn/flynn/pkg/shutdown"
+)
+
+func main() {
+	serviceName := os.Getenv("FLYNN_POSTGRES")
+	if serviceName == "" {
+		serviceName = "pg"
+	}
+	singleton := os.Getenv("SINGLETON") == "true"
+	password := os.Getenv("PGPASSWORD")
+
+	err := discoverd.DefaultClient.AddService(serviceName, &discoverd.ServiceConfig{
+		LeaderType: discoverd.LeaderTypeManual,
+	})
+	if je, ok := err.(httphelper.JSONError); err != nil && (!ok || je.Code != httphelper.ObjectExistsError) {
+		shutdown.Fatal(err)
+	}
+	inst := &discoverd.Instance{Addr: ":5432"}
+	hb, err := discoverd.DefaultClient.RegisterInstance(serviceName, inst)
+	if err != nil {
+		shutdown.Fatal(err)
+	}
+	shutdown.BeforeExit(func() { hb.Close() })
+
+	log := log15.New("app", "postgres")
+
+	pg := NewPostgres(Config{
+		ID:           inst.ID,
+		Singleton:    singleton,
+		BinDir:       "/usr/lib/postgresql/9.4/bin/",
+		Password:     password,
+		Logger:       log.New("component", "postgres"),
+		ExtWhitelist: true,
+	})
+	dd := NewDiscoverd(discoverd.DefaultClient.Service(serviceName), log.New("component", "discoverd"))
+
+	peer := state.NewPeer(inst, singleton, dd, pg, log.New("component", "peer"))
+	shutdown.BeforeExit(func() { peer.Close() })
+
+	peer.Run()
+	// TODO(titanous): clean shutdown of postgres
+}
