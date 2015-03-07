32135cf2ea4c9461fa6c58c9af5a3b2dcedc130a
Jonathan Rudenberg <jonathan@titanous.com>
2015-03-07 15:18:09 -0800

    discoverd: Wait for flannel interface to come up
    discoverd binds to port 53 on the flannel interface in order to
    provide DNS to containers. When resurrecting, discoverd may start
    before flannel is finished setting up the interface.
    
    This patch uses a startup script that parses the expected IP from
    the arguments and waits for it to show up in ipconfig before
    exec'ing discoverd.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

---
+++ discoverd/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+start=$(date +%s)
+dnsaddr=$(echo "$@" | sed -r 's/.*dns-addr=([0-9.]+):.*/\1/')
+
+# Wait for flannel interface to show up, as we may have started before flannel.
+while true; do
+  ifconfig | grep -qF "$dnsaddr" && break || sleep 0.2
+  elapsed=$(($(date +%s) - $start))
+  if [ $elapsed -gt 60 ]; then
+    echo "$dnsaddr did not appear within 60 seconds"
+  fi
+done
+
+exec /bin/discoverd $*
