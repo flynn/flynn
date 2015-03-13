flynn/flynn

commit bc37712e143dac89c36a2ef7eb98a99e5ee66854

pkg/postgres: Uniqueness error helper.
Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

controller/jobs.go
@@ -70,7 +70,7 @@ func (r *JobRepo) Add(job *ct.Job) error {
 	// TODO: actually validate
 	err = r.db.QueryRow("INSERT INTO job_cache (job_id, host_id, app_id, release_id, process_type, state, meta) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING created_at, updated_at",
 		jobID, hostID, job.AppID, job.ReleaseID, job.Type, job.State, meta).Scan(&job.CreatedAt, &job.UpdatedAt)
-	if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" {
+	if postgres.IsUniquenessError(err, "") {
 		err = r.db.QueryRow("UPDATE job_cache SET state = $3, updated_at = now() WHERE job_id = $1 AND host_id = $2 RETURNING created_at, updated_at",
 			jobID, hostID, job.State).Scan(&job.CreatedAt, &job.UpdatedAt)
 		if e, ok := err.(*pq.Error); ok && e.Code.Name() == "check_violation" {
@@ -83,7 +83,7 @@ func (r *JobRepo) Add(job *ct.Job) error {
 
 	// create a job event, ignoring possible duplications
 	err = r.db.Exec("INSERT INTO job_events (job_id, host_id, app_id, state) VALUES ($1, $2, $3, $4)", jobID, hostID, job.AppID, job.State)
-	if e, ok := err.(*pq.Error); !ok || e.Code.Name() != "unique_violation" {
+	if postgres.IsUniquenessError(err, "") {
 		return err
 	}
 	return nil

blobstore/postgres_filesystem.go
@@ -49,7 +49,7 @@ func (p *PostgresFilesystem) Put(name string, r io.Reader, typ string) error {
 	var id oid.Oid
 create:
 	err = tx.QueryRow("INSERT INTO files (name, type) VALUES ($1, $2) RETURNING file_id", name, typ).Scan(&id)
-	if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" {
+	if postgres.IsUniquenessError(err, "") {
 		tx.Rollback()
 		tx, err = p.db.Begin()
 		if err != nil {

controller/artifact.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-sql"
-	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/pq"
 	ct "github.com/flynn/flynn/controller/types"
 	"github.com/flynn/flynn/pkg/postgres"
 	"github.com/flynn/flynn/pkg/random"
@@ -30,7 +29,7 @@ func (r *ArtifactRepo) Add(data interface{}) error {
 	}
 	err := r.db.QueryRow("INSERT INTO artifacts (artifact_id, type, uri) VALUES ($1, $2, $3) RETURNING created_at",
 		a.ID, a.Type, a.URI).Scan(&a.CreatedAt)
-	if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" {
+	if postgres.IsUniquenessError(err, "") {
 		err = r.db.QueryRow("SELECT artifact_id, created_at FROM artifacts WHERE type = $1 AND uri = $2",
 			a.Type, a.URI).Scan(&a.ID, &a.CreatedAt)
 		if err != nil {

controller/deployment.go
@@ -189,7 +189,7 @@ func (c *controllerAPI) CreateDeployment(ctx context.Context, w http.ResponseWri
 	}
 
 	if err := c.deploymentRepo.Add(deployment); err != nil {
-		if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" && e.Constraint == "isolate_deploys" {
+		if postgres.IsUniquenessError(err, "isolate_deploys") {
 			httphelper.ValidationError(w, "", "Cannot create deploy, there is already one in progress for this app.")
 			return
 		}

controller/formation.go
@@ -60,7 +60,7 @@ func (r *FormationRepo) Add(f *ct.Formation) error {
 	procs := procsHstore(f.Processes)
 	err := r.db.QueryRow("INSERT INTO formations (app_id, release_id, processes) VALUES ($1, $2, $3) RETURNING created_at, updated_at",
 		f.AppID, f.ReleaseID, procs).Scan(&f.CreatedAt, &f.UpdatedAt)
-	if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" {
+	if postgres.IsUniquenessError(err, "") {
 		err = r.db.QueryRow("UPDATE formations SET processes = $3, updated_at = now(), deleted_at = NULL WHERE app_id = $1 AND release_id = $2 RETURNING created_at, updated_at",
 			f.AppID, f.ReleaseID, procs).Scan(&f.CreatedAt, &f.UpdatedAt)
 	}

controller/key.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-sql"
-	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/pq"
 	"github.com/flynn/flynn/Godeps/_workspace/src/golang.org/x/crypto/ssh"
 	ct "github.com/flynn/flynn/controller/types"
 	"github.com/flynn/flynn/pkg/postgres"
@@ -38,7 +37,7 @@ func (r *KeyRepo) Add(data interface{}) error {
 	key.Comment = comment
 
 	err = r.db.QueryRow("INSERT INTO keys (fingerprint, key, comment) VALUES ($1, $2, $3) RETURNING created_at", key.ID, key.Key, key.Comment).Scan(&key.CreatedAt)
-	if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" {
+	if postgres.IsUniquenessError(err, "") {
 		return nil
 	}
 	return err

pkg/postgres/postgres.go
@@ -201,3 +201,10 @@ func CleanUUID(u string) string {
 func FormatUUID(s string) string {
 	return s[:8] + "-" + s[8:12] + "-" + s[12:16] + "-" + s[16:20] + "-" + s[20:]
 }
+
+func IsUniquenessError(err error, constraint string) bool {
+	if e, ok := err.(*pq.Error); ok && e.Code.Name() == "unique_violation" {
+		return constraint == "" || constraint == e.Constraint
+	}
+	return false
+}

commit c54a751ab0a04b15f7e5c5c94769b8a4e72a23ee

controller: Fix ignoring job event uniqueness errors
This logic was broken in https://github.com/flynn/flynn/commit/bc37712

Signed-off-by: Lewis Marshall <lewis@lmars.net>

controller/jobs.go
@@ -84,9 +84,9 @@ func (r *JobRepo) Add(job *ct.Job) error {
 	// create a job event, ignoring possible duplications
 	err = r.db.Exec("INSERT INTO job_events (job_id, host_id, app_id, state) VALUES ($1, $2, $3, $4)", jobID, hostID, job.AppID, job.State)
 	if postgres.IsUniquenessError(err, "") {
-		return err
+		return nil
 	}
-	return nil
+	return err
 }
 
 func scanJob(s postgres.Scanner) (*ct.Job, error) {

controller/jobs_test.go
@@ -57,6 +57,9 @@ func (s *S) TestJobStateTransition(c *C) {
 	job.State = "up"
 	c.Assert(s.c.PutJob(job), IsNil)
 
+	// duplicates are ignored
+	c.Assert(s.c.PutJob(job), IsNil)
+
 	job.State = "starting"
 	c.Assert(s.c.PutJob(job), ErrorMatches, ".*invalid job state transition.*")
 