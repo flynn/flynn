0c42bb2b8fe1a10691e0bbaff17f7b77ebcd7fec
Jonathan Rudenberg <jonathan@titanous.com>
2015-03-18 15:38:37 -0700

    cli: Remove unused sprintf of install usage
    The format string was removed when the web installer landed.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- cli/install.go
+++ cli/install.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-docopt"
 	"github.com/flynn/flynn/installer"
 )
 
 func init() {
-	register("install", runInstaller, fmt.Sprintf(`
+	register("install", runInstaller, `
 usage: flynn install
 
 Starts server for installer web interface.
@@ -16,7 +14,7 @@ Starts server for installer web interface.
 Examples:
 
 	$ flynn install
-`, installer.DefaultInstanceType))
+`)
 }
 
 func runInstaller(args *docopt.Args) error {
