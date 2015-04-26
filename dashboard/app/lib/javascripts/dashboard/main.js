11fda4bbc38df10733ab25606c751a6528e8b744
aeneasr <aeneas.rekkas@serlo.org>
2015-04-26 19:32:27 +0200

    dashboard: Should detect SSL problems with CORS
    Signed-off-by: aeneasr <aeneas.rekkas@serlo.org>

--- dashboard/app/lib/javascripts/dashboard/main.js
+++ dashboard/app/lib/javascripts/dashboard/main.js
@@ -13,8 +13,20 @@ window.Dashboard = {
 	config: {},
 
 	waitForRouteHandler: Promise.resolve(),
+	errCertNotInstalled: new Error("HTTPS certificate is not trusted."),
+	errServiceUnavailable: new Error("Service is unavailable."),
 
 	run: function () {
+		var self = this, loadURL, resolveWaitForNav;
+		this.waitForNav = new Promise(function(resolve, reject) {
+			resolveWaitForNav = resolve;
+		});
+
+		loadURL = function() {
+			resolveWaitForNav();
+			Marbles.history.loadURL();
+		};
+
 		if ( Marbles.history && Marbles.history.started ) {
 			throw new Error("Marbles.history already started!");
 		}
@@ -38,11 +50,11 @@ window.Dashboard = {
 			dispatcher: this.Dispatcher,
 			trigger: false
 		});
-		if (window.location.protocol === "http:" && this.config.INSTALL_CERT) {
-			Marbles.history.navigate("installcert");
-			Marbles.history.loadURL();
+
+		if (this.config.INSTALL_CERT) {
+			this.__isCertInstalled().then(loadURL);
 		} else {
-			Marbles.history.loadURL();
+			loadURL();
 		}
 	},
 
@@ -63,6 +75,92 @@ window.Dashboard = {
 	__redirectToLogin: function () {
 		var redirectPath = Marbles.history.path ? '?redirect='+ encodeURIComponent(Marbles.history.path) : '';
 		Marbles.history.navigate('login'+ redirectPath);
+	},
+
+	__catchInsecurePingResponse: function(httpsArgs) {
+		var httpsXhr = httpsArgs[1], self = this,
+			handleSuccess, handleError;
+
+		handleSuccess = function (httpArgs) {
+			var httpXhr = httpArgs[1];
+			// https did not work but http did...something is wrong with the cert
+			self.Dispatcher.handleAppEvent({
+				name: "HTTPS_CERT_MISSING",
+				status: httpXhr.status
+			});
+			return Promise.reject(self.errCertNotInstalled);
+		};
+		handleError = function (httpArgs) {
+			var httpXhr = httpArgs[1];
+			if (httpArgs.length === 1) {
+				return httpArgs;
+			}
+
+			if (httpXhr.status === 0) {
+				// https is failing as well...service is unavailable
+				self.Dispatcher.handleAppEvent({
+					name: "SERVICE_UNAVAILABLE",
+					status: httpXhr.status
+				});
+				return Promise.reject(self.errServiceUnavailable);
+			}
+			// https did not work but http did without a network error
+			// => missing ssl exception for controller
+			self.Dispatcher.handleAppEvent({
+				name: "HTTPS_CERT_MISSING",
+				status: httpXhr.status
+			});
+			return Promise.reject(self.errCertNotInstalled);
+		};
+
+		if (httpsXhr.status === 0) {
+			// https is unavailable, let's see if http works
+			return self.client.ping("controller", "http").then(handleSuccess).catch(handleError);
+		}
+		// an error code other than 0
+		self.Dispatcher.handleAppEvent({
+			name: "SERVICE_UNAVAILABLE",
+			status: httpsXhr.status
+		});
+		return Promise.reject(this.errServiceUnavailable);
+	},
+
+	__catchSecurePingResponse: function(args) {
+		var xhr = args[1];
+		if (xhr.status === 0) {
+			// We were not able to access the controller due to a network error (ssl, timeout)
+			// In order to understand what's happening, we have to switch to http.
+			this.Dispatcher.handleAppEvent({
+				name: "CONTROLLER_UNREACHABLE_FROM_HTTPS",
+				status: xhr.status
+			});
+			return Promise.reject(new Error("CONTROLLER_UNREACHABLE_FROM_HTTPS"));
+		}
+
+		// an error code other than 0
+		this.Dispatcher.handleAppEvent({
+			name: "SERVICE_UNAVAILABLE",
+			status: xhr.status
+		});
+		return Promise.reject(this.errServiceUnavailable);
+	},
+
+	__successPingResponse: function(args) {
+		var xhr = args[1];
+		if (xhr.status === 200) {
+			window.location.href = window.location.href.replace("http:", "https:");
+		}
+	},
+
+	__isCertInstalled: function() {
+		var self = this;
+		if (window.location.protocol === "https:") {
+			return this.client.ping("controller", "https").catch(this.__catchSecurePingResponse.bind(this));
+		} else {
+			return this.client.ping("controller", "https")
+				.then(this.__successPingResponse.bind(this))
+				.catch(this.__catchInsecurePingResponse.bind(this));
+		}
 	},
 
 	__handleEvent: function (event) {
@@ -95,11 +193,7 @@ window.Dashboard = {
 	__handleAppEvent: function (event) {
 		switch (event.name) {
 			case "CONFIG_READY":
-				if (window.location.protocol === "http:" && this.config.INSTALL_CERT) {
-					window.location.href = window.location.href.replace("http", "https");
-				} else {
-					this.__handleConfigReady();
-				}
+				this.__handleConfigReady();
 			break;
 
 			case "AUTH_CHANGE":
@@ -110,12 +204,17 @@ window.Dashboard = {
 				this.__handleGithubAuthChange(event.authenticated);
 			break;
 
+			case "CONTROLLER_UNREACHABLE_FROM_HTTPS":
+				// Controller isn't accessible via https. Redirect to http and try again.
+				window.location.href = window.location.href.replace("https", "http");
+			break;
+
+			case "HTTPS_CERT_MISSING":
+				Marbles.history.navigate("installcert", {force: true});
+			break;
+
 			case "SERVICE_UNAVAILABLE":
-				if (window.location.protocol === "http:" && this.config.INSTALL_CERT) {
-					this.__handleConfigReady();
-				} else {
-					this.__handleServiceUnavailable(event.status);
-				}
+				this.__handleServiceUnavailable(event.status);
 			break;
 		}
 	},
@@ -132,12 +231,16 @@ window.Dashboard = {
 		this.__renderNavComponent();
 
 		if ( !authenticated && !this.__isLoginPath() ) {
-			var currentHandler = Marbles.history.getHandler();
+			var currentHandler = Marbles.history.getHandler(),
+				self = this;
 			if (currentHandler && currentHandler.opts.auth === false) {
 				// Don't redirect to login from page not requiring auth
 				return;
 			}
-			this.__redirectToLogin();
+
+			this.waitForNav.then(function() {
+				self.__redirectToLogin();
+			});
 		}
 	},
 
