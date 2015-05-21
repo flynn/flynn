d9228951e2c89a0903d056ab6a290e2ebe5378ba
John Zila <john@jzila.com>
2015-05-21 14:41:49 -0700

    scheduler: For the Scheduler V2 rewrite, implemented much of the functionality in #1484. We now keep job state in 1 place, and coordinate within the scheduler through events.
    Signed-off-by: John Zila <john@jzila.com>
    
    scheduler-v2: WIP implemented @lmars feedback and added more support for unit testing of formation changes
    
    Signed-off-by: John Zila <john@jzila.com>
    
    scheduler-v2: WIP worked further on formation change events. Coordinated with @lmars to figure out omni processes.
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP formation changes in scheduler-v2
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP modified the scheduler and fake cluster/host to handle @titanous's host refactor
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP refactored interfaces to support stubbing out for new scheduler
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP Finished initial pass for watching formations and implemented more of cluster syncing
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP Added a fake controller client for unit tests and fixed a few deadlocks in the test run. Test currently fails but we're close to it working.
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP Tests working for cluster sync and formation change
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP scheduler-v2: removed unnecessary mutexes and extraneous data
    
    Signed-off-by: John Zila <john@jzila.com>
    
    Refactored jobs to use interface-based accessors
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP: Refactored formations and jobs to coordinate with the scheduler over a channel. Also implemented PutJob. Tests currently failing due to a null pointer dereference that I'm trying to find.
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP scheduler-v2: successfully refactored formation rectifying to trigger JobRequest events. Added test cases to watch for job start events
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP scheduler-v2: handling a job creation event no longer queries the hosts for their job lists
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP scheduler-v2: Added events for stopping jobs (but didn't plug them in yet). Also refactored in-memory job tracking to have a unified API so we're not doing the same work in multiple locations.
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP scheduler-v2: consolidated some repeated code and filtered Sync() to only consider active jobs
    
    Signed-off-by: John Zila <john@jzila.com>
    
    WIP scheduler-v2: fixed controller/jobs_test to use the updated host client interface
    
    Signed-off-by: John Zila <john@jzila.com>

--- controller/scheduler-v2/scheduler_test.go
+++ controller/scheduler-v2/scheduler_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	. "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-check"
+	. "github.com/flynn/flynn/controller/testutils"
+	ct "github.com/flynn/flynn/controller/types"
 	"github.com/flynn/flynn/host/types"
+	"github.com/flynn/flynn/pkg/random"
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -14,62 +18,116 @@ type TestSuite struct{}
 
 var _ = Suite(&TestSuite{})
 
-type FakeCluster struct {
-	hosts []Host
+func createTestScheduler(appID string, processes map[string]int) *Scheduler {
+	artifact := &ct.Artifact{ID: "artifact-1"}
+	release := NewRelease("release-1", artifact, processes)
+	h := NewFakeHostClient("host-1")
+	for k, c := range processes {
+		for i := 0; i < c; i++ {
+			jobID := random.UUID()
+			h.AddJob(&host.Job{
+				ID: jobID,
+				Metadata: map[string]string{
+					"flynn-controller.app":     appID,
+					"flynn-controller.release": release.ID,
+					"flynn-controller.type":    k,
+				},
+				Artifact: host.Artifact{
+					Type: artifact.Type,
+					URI:  artifact.URI,
+				},
+			})
+		}
+	}
+	cluster := NewFakeCluster()
+	cluster.SetHosts(map[string]*FakeHostClient{h.ID(): h})
+	cc := NewFakeControllerClient(appID, release, artifact, processes)
+
+	return NewScheduler(cluster, cc)
 }
 
-func (f *FakeCluster) ListHosts() ([]Host, error) {
-	return f.hosts, nil
+func waitForEventType(events chan Event, etype EventType) (Event, error) {
+	for {
+		select {
+		case event, ok := <-events:
+			if !ok {
+				return nil, fmt.Errorf("unexpected close of scheduler event stream")
+			}
+			if event.Type() == etype {
+				if event.Err() != nil {
+					return nil, fmt.Errorf("Error occurred occurred while processing event of type %q. Error: %v", etype, event.Err())
+				}
+				return event, nil
+			}
+		case <-time.After(time.Second):
+			return nil, fmt.Errorf("timed out waiting for cluster sync event")
+		}
+	}
 }
 
-type FakeHost struct {
-	id   string
-	jobs map[string]host.ActiveJob
-}
+func (ts *TestSuite) TestInitialClusterSync(c *C) {
+	s := createTestScheduler("testApp", map[string]int{"web": 1})
 
-func (f *FakeHost) ID() string {
-	return f.id
-}
-
-func (f *FakeHost) ListJobs() (map[string]host.ActiveJob, error) {
-	return f.jobs, nil
-}
-
-func (TestSuite) TestInitialClusterSync(c *C) {
-	jobID := "job-1"
-	host := &FakeHost{id: "host-1", jobs: map[string]host.ActiveJob{
-		jobID: {Job: &host.Job{ID: jobID}},
-	}}
-	cluster := &FakeCluster{hosts: []Host{host}}
-
-	s := NewScheduler(cluster)
-	events := make(chan *Event)
+	events := make(chan Event)
 	stream := s.Subscribe(events)
 	defer stream.Close()
 	go s.Run()
 	defer s.Stop()
 
 	// wait for a cluster sync event
-loop:
-	for {
-		select {
-		case event, ok := <-events:
-			if !ok {
-				c.Fatal("unexpected close of scheduler event stream")
-			}
-			if event.Type == EventTypeClusterSync {
-				break loop
-			}
-		case <-time.After(time.Second):
-			c.Fatal("timed out waiting for cluster sync event")
-		}
-	}
+	_, err := waitForEventType(events, EventTypeClusterSync)
+	fatalIfError(c, err)
 
 	// check the scheduler has the job
-	job := s.GetJob(jobID)
-	c.Assert(job, NotNil)
-	c.Assert(job.Job.ID, Equals, jobID)
+	formation, err := s.getFormation("testApp", "testApp", "release-1")
+	jobs := formation.GetJobsForType("web")
+	c.Assert(err, IsNil)
+	c.Assert(jobs, NotNil)
+	for _, j := range jobs {
+		c.Assert(j.HostID, Equals, "host-1")
+	}
+	c.Assert(len(jobs), Equals, 1)
 }
 
-func (TestSuite) TestFormationChange(c *C) {
+func (ts *TestSuite) TestFormationChange(c *C) {
+	s := createTestScheduler("testApp", map[string]int{"web": 1})
+	release, _ := s.GetRelease("release-1")
+	artifact, _ := s.GetArtifact(release.ArtifactID)
+	s.log.Info("Testing formation change")
+
+	events := make(chan Event, 1)
+	stream := s.Subscribe(events)
+	defer stream.Close()
+	go s.Run()
+	defer s.Stop()
+
+	// wait for a cluster sync event
+	_, err := waitForEventType(events, EventTypeClusterSync)
+	fatalIfError(c, err)
+
+	s.formationChange <- &ct.ExpandedFormation{
+		App: &ct.App{
+			Name: "test-formation-change",
+			ID:   "test-formation-change",
+		},
+		Release:   release,
+		Artifact:  artifact,
+		Processes: map[string]int{"web": 2},
+	}
+
+	_, err = waitForEventType(events, EventTypeFormationChange)
+	fatalIfError(c, err)
+	e, err := waitForEventType(events, EventTypeJobStart)
+	fatalIfError(c, err)
+	je, ok := e.(*JobStartEvent)
+	c.Assert(ok, Equals, true)
+	c.Assert(je.Job, NotNil)
+
+	s.stop <- struct{}{}
+}
+
+func fatalIfError(c *C, err error) {
+	if err != nil {
+		c.Fatal(err.Error())
+	}
 }
