1092694cb67caa9183f38f54a4214a3ec3f8b59a
Lewis Marshall <lewis@lmars.net>
2015-07-04 13:59:57 +0100

    scheduler: Refactor changes to move more responsibility to the scheduler and less in Formation.
    See https://github.com/flynn/flynn/pull/1585#issuecomment-118634703 for
    details.
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>
    
    scheduler: Remove unused method
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>
    
    scheduler: Fix shadowed error variable
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>
    
    scheduler: Refactor tests
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>
    
    scheduler: Rename JobType field to Type
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>
    
    scheduler: Remove formation check from TestInitialClusterSync
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>
    
    scheduler: Move job start / stop method onto Scheduler type
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- controller/scheduler-v2/scheduler_test.go
+++ controller/scheduler-v2/scheduler_test.go
@@ -18,19 +18,24 @@ type TestSuite struct{}
 
 var _ = Suite(&TestSuite{})
 
+const (
+	testHostID    = "host-1"
+	testReleaseID = "release-1"
+)
+
 func createTestScheduler(appID string, processes map[string]int) *Scheduler {
 	artifact := &ct.Artifact{ID: "artifact-1"}
-	release := NewRelease("release-1", artifact, processes)
-	h := NewFakeHostClient("host-1")
-	for k, c := range processes {
-		for i := 0; i < c; i++ {
+	release := NewRelease(testReleaseID, artifact, processes)
+	h := NewFakeHostClient(testHostID)
+	for typ, count := range processes {
+		for i := 0; i < count; i++ {
 			jobID := random.UUID()
 			h.AddJob(&host.Job{
 				ID: jobID,
 				Metadata: map[string]string{
 					"flynn-controller.app":     appID,
 					"flynn-controller.release": release.ID,
-					"flynn-controller.type":    k,
+					"flynn-controller.type":    typ,
 				},
 				Artifact: host.Artifact{
 					Type: artifact.Type,
@@ -46,21 +51,21 @@ func createTestScheduler(appID string, processes map[string]int) *Scheduler {
 	return NewScheduler(cluster, cc)
 }
 
-func waitForEventType(events chan Event, etype EventType) (Event, error) {
+func waitForEvent(events chan Event, typ EventType) (Event, error) {
 	for {
 		select {
 		case event, ok := <-events:
 			if !ok {
 				return nil, fmt.Errorf("unexpected close of scheduler event stream")
 			}
-			if event.Type() == etype {
-				if event.Err() != nil {
-					return nil, fmt.Errorf("Error occurred occurred while processing event of type %q. Error: %v", etype, event.Err())
-				}
+			if err := event.Err(); err != nil {
+				return nil, fmt.Errorf("unexpected event error: %s", err)
+			}
+			if event.Type() == typ {
 				return event, nil
 			}
 		case <-time.After(time.Second):
-			return nil, fmt.Errorf("timed out waiting for cluster sync event")
+			return nil, fmt.Errorf("timed out waiting for %s event", typ)
 		}
 	}
 }
@@ -75,25 +80,26 @@ func (ts *TestSuite) TestInitialClusterSync(c *C) {
 	defer s.Stop()
 
 	// wait for a cluster sync event
-	_, err := waitForEventType(events, EventTypeClusterSync)
-	fatalIfError(c, err)
+	_, err := waitForEvent(events, EventTypeClusterSync)
+	c.Assert(err, IsNil)
 
 	// check the scheduler has the job
-	formation, err := s.getFormation("testApp", "testApp", "release-1")
-	jobs := formation.GetJobsForType("web")
-	c.Assert(err, IsNil)
-	c.Assert(jobs, NotNil)
-	for _, j := range jobs {
-		c.Assert(j.HostID, Equals, "host-1")
+	jobs := s.Jobs()
+	c.Assert(jobs, HasLen, 1)
+	for _, job := range jobs {
+		c.Assert(job.Type, Equals, "web")
+		c.Assert(job.HostID, Equals, testHostID)
+		c.Assert(job.AppID, Equals, "testApp")
 	}
-	c.Assert(len(jobs), Equals, 1)
 }
 
 func (ts *TestSuite) TestFormationChange(c *C) {
-	s := createTestScheduler("testApp", map[string]int{"web": 1})
-	release, _ := s.GetRelease("release-1")
-	artifact, _ := s.GetArtifact(release.ArtifactID)
-	s.log.Info("Testing formation change")
+	app := &ct.App{ID: random.UUID(), Name: "test-formation-change"}
+	s := createTestScheduler(app.ID, map[string]int{"web": 1})
+	release, err := s.GetRelease(testReleaseID)
+	c.Assert(err, IsNil)
+	artifact, err := s.GetArtifact(release.ArtifactID)
+	c.Assert(err, IsNil)
 
 	events := make(chan Event, 1)
 	stream := s.Subscribe(events)
@@ -101,33 +107,22 @@ func (ts *TestSuite) TestFormationChange(c *C) {
 	go s.Run()
 	defer s.Stop()
 
-	// wait for a cluster sync event
-	_, err := waitForEventType(events, EventTypeClusterSync)
-	fatalIfError(c, err)
-
 	s.formationChange <- &ct.ExpandedFormation{
-		App: &ct.App{
-			Name: "test-formation-change",
-			ID:   "test-formation-change",
-		},
+		App:       app,
 		Release:   release,
 		Artifact:  artifact,
 		Processes: map[string]int{"web": 2},
 	}
 
-	_, err = waitForEventType(events, EventTypeFormationChange)
-	fatalIfError(c, err)
-	e, err := waitForEventType(events, EventTypeJobStart)
-	fatalIfError(c, err)
-	je, ok := e.(*JobStartEvent)
+	_, err = waitForEvent(events, EventTypeFormationChange)
+	c.Assert(err, IsNil)
+	e, err := waitForEvent(events, EventTypeJobStart)
+	c.Assert(err, IsNil)
+	event, ok := e.(*JobStartEvent)
 	c.Assert(ok, Equals, true)
-	c.Assert(je.Job, NotNil)
-
-	s.stop <- struct{}{}
-}
-
-func fatalIfError(c *C, err error) {
-	if err != nil {
-		c.Fatal(err.Error())
-	}
+	c.Assert(event.Job, NotNil)
+	job := event.Job
+	c.Assert(job.Type, Equals, "web")
+	c.Assert(job.AppID, Equals, app.ID)
+	c.Assert(job.ReleaseID, Equals, testReleaseID)
 }
