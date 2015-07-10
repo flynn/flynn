b767285064d26eb3203d8737cdb4bc67bc17653d
Jonathan Rudenberg <jonathan@titanous.com>
2015-07-10 15:29:29 -0400

    all: Fix toolchain path management
    Consistently use the go binary from our toolchain and set GOROOT.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- flannel/build.sh
+++ flannel/build.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 commit=e096282d63d1ca8c3d9b6e689da6a7d2b8ea0740
 dir=flannel-${commit}
 tmpdir=$(mktemp --directory)
+PATH=$(readlink -f ../util/_toolchain/go/bin):$PATH
 
 cleanup() {
   rm -rf "${tmpdir}"
