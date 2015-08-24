931e5d175b23e5bac2e072946db6814f7878fe38
Jesse Stuart <jesse@jessestuart.ca>
2015-08-24 12:38:09 -0500

    Godeps: Update asset-matrix-go
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- Godeps/_workspace/src/github.com/jvatic/asset-matrix-go/matrix.go
+++ Godeps/_workspace/src/github.com/jvatic/asset-matrix-go/matrix.go
@@ -174,7 +174,7 @@ func (m *Matrix) RemoveOldAssets() {
 
 func installNpmPackages(names []string) error {
 	for _, n := range names {
-		if _, err := os.Stat("node_modules/" + n); err == nil {
+		if _, err := os.Stat("node_modules/" + strings.Split(n, "@")[0]); err == nil {
 			continue
 		}
 		cmd := exec.Command("npm", "install", n)
@@ -304,22 +304,23 @@ func (m *Matrix) compileTree(tree []Asset) error {
 	type result struct {
 		io.Reader
 		error
+		idx int
 	}
 	results := make(chan result)
 	compileAsset := func(i int, a Asset) {
 		r, err := a.Compile()
-		results <- result{r, err}
+		results <- result{r, err, i}
 	}
 	for i, a := range tree {
 		go compileAsset(i, a)
 	}
 	readers := make([]io.Reader, len(tree))
-	for i := range tree {
+	for _ = range tree {
 		res := <-results
 		if res.error != nil {
 			return res.error
 		}
-		readers[i] = res.Reader
+		readers[res.idx] = res.Reader
 	}
 	outputPath := tree[len(tree)-1].OutputPath()
 	manifestKey := outputPath
