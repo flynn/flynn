25359d76e7d5516ef01fa36a16e462ac818daf8f
Ben Burkert <ben@benburkert.com>
2015-09-13 12:32:08 -0400

    logaggregator: improve test
    Signed-off-by: Ben Burkert <ben@benburkert.com>

--- logaggregator/server_test.go
+++ logaggregator/server_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"net"
 
 	. "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-check"
+	"github.com/flynn/flynn/logaggregator/client"
+	"github.com/flynn/flynn/pkg/syslog/rfc6587"
 )
 
 const (
@@ -11,12 +14,39 @@ const (
 	sampleLogLine2 = "79 <40>1 2012-11-30T06:45:26+00:00 host app web.2 - - 25 yay this is a message!!!\n"
 )
 
-func (s *LogAggregatorTestSuite) TestServerShutdown(c *C) {
+type ServerTestSuite struct{}
+
+var _ = Suite(&ServerTestSuite{})
+
+func (s *ServerTestSuite) TestServerDurability(c *C) {
 	srv := testServer(c)
+	go srv.Run()
+	defer srv.Shutdown()
+
+	cl := testClient(c, srv)
+
 	conn, err := net.Dial("tcp", srv.SyslogAddr().String())
 	c.Assert(err, IsNil)
 	defer conn.Close()
 
-	conn.Write([]byte(sampleLogLine1))
-	srv.Shutdown()
+	zero := 0
+	rc, err := cl.GetLog("app-A", &client.LogOpts{Follow: true, Lines: &zero})
+	c.Assert(err, IsNil)
+
+	for _, msg := range appAMessages {
+		conn.Write(rfc6587.Bytes(msg))
+	}
+
+	var got client.Message
+	dec := json.NewDecoder(rc)
+	for _, want := range appAMessages {
+		c.Assert(dec.Decode(&got), IsNil)
+		c.Assert(got.HostID, Equals, string(want.Hostname))
+		c.Assert(got.Stream, Equals, streamName(want.MsgID))
+		c.Assert(got.Timestamp, Equals, want.Timestamp)
+
+		procType, jobID := splitProcID(want.ProcID)
+		c.Assert(got.ProcessType, Equals, string(procType))
+		c.Assert(got.JobID, Equals, string(jobID))
+	}
 }
