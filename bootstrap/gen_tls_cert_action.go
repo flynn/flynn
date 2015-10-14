8e04aaac3fcea40b0f51d9b917b72a98f6b8b50a
Jonathan Rudenberg <jonathan@titanous.com>
2015-10-14 14:27:13 -0400

    bootstrap: Allow passing pre-generated TLS details in env vars
    This allows a cluster to be bootstrapped using a previous cluster's
    certificates, so a cluster can be seamlessly replaced without
    breaking clients.
    
    Closes #1039
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- bootstrap/gen_tls_cert_action.go
+++ bootstrap/gen_tls_cert_action.go
@@ -1,6 +1,9 @@
 package bootstrap
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/pem"
 	"fmt"
 
 	"github.com/flynn/flynn/pkg/certgen"
@@ -9,6 +12,10 @@ import (
 type GenTLSCertAction struct {
 	ID    string   `json:"id"`
 	Hosts []string `json:"hosts"`
+
+	CACert     string `json:"ca_cert"`
+	Cert       string `json:"cert"`
+	PrivateKey string `json:"key"`
 }
 
 func init() {
@@ -30,6 +37,21 @@ func (c *TLSCert) String() string {
 func (a *GenTLSCertAction) Run(s *State) (err error) {
 	data := &TLSCert{}
 	s.StepData[a.ID] = data
+
+	a.CACert = interpolate(s, a.CACert)
+	a.Cert = interpolate(s, a.Cert)
+	a.PrivateKey = interpolate(s, a.PrivateKey)
+	if a.CACert != "" && a.Cert != "" && a.PrivateKey != "" {
+		data.CACert = a.CACert
+		data.Cert = a.Cert
+		data.PrivateKey = a.PrivateKey
+
+		// calculate cert pin
+		b, _ := pem.Decode([]byte(data.Cert))
+		sha := sha256.Sum256(b.Bytes)
+		data.Pin = base64.StdEncoding.EncodeToString(sha[:])
+		return nil
+	}
 
 	for i, h := range a.Hosts {
 		a.Hosts[i] = interpolate(s, h)
