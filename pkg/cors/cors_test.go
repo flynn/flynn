flynn/flynn

commit 19c71badd6298c7c5a727b6328bc9f3585ced403

all: Fix CORS handler usage
Avoid a double WriteHeader call by only calling the next handler when
the request is not an OPTIONS request.

Closes #2229

Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

dashboard/api.go
@@ -119,23 +119,20 @@ func (api *API) CorsHandler(main http.Handler) http.Handler {
 	if strings.HasPrefix(api.conf.InterfaceURL, "https") {
 		httpInterfaceURL = "http" + strings.TrimPrefix(api.conf.InterfaceURL, "https")
 	}
-	corsHandler := cors.Allow(&cors.Options{
+	return (&cors.Options{
 		AllowOrigins:     []string{api.conf.InterfaceURL, httpInterfaceURL},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 		AllowHeaders:     []string{"Authorization", "Accept", "Content-Type", "If-Match", "If-None-Match"},
 		ExposeHeaders:    []string{"ETag"},
 		AllowCredentials: true,
 		MaxAge:           time.Hour,
-	})
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if strings.HasSuffix(req.URL.Path, "/ping") || req.Method == "OPTIONS" {
-			httphelper.CORSAllowAllHandler(w, req)
+	}).Handler(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if strings.HasSuffix(req.URL.Path, "/ping") {
 			w.WriteHeader(200)
 			return
 		}
-		corsHandler(w, req)
 		main.ServeHTTP(w, req)
-	})
+	}))
 }
 
 func (api *API) ServeStatic(ctx context.Context, w http.ResponseWriter, req *http.Request, path string) {

pkg/cors/cors.go
@@ -112,30 +112,29 @@ func (o *Options) IsOriginAllowed(origin string) (allowed bool) {
 }
 
 // Allows CORS for requests those match the provided options.
-func Allow(opts *Options) http.HandlerFunc {
+func (o *Options) Handler(next http.Handler) http.HandlerFunc {
 	// Allow default headers if nothing is specified.
-	if len(opts.AllowHeaders) == 0 {
-		opts.AllowHeaders = defaultAllowHeaders
+	if len(o.AllowHeaders) == 0 {
+		o.AllowHeaders = defaultAllowHeaders
 	}
 
-	for _, origin := range opts.AllowOrigins {
+	for _, origin := range o.AllowOrigins {
 		pattern := regexp.QuoteMeta(origin)
 		pattern = strings.Replace(pattern, "\\*", ".*", -1)
 		pattern = strings.Replace(pattern, "\\?", ".", -1)
 		allowOriginPatterns = append(allowOriginPatterns, "^"+pattern+"$")
 	}
 
-	return func(res http.ResponseWriter, req *http.Request) {
-		origin := req.Header.Get(headerOrigin)
-		if origin == "" {
-			return
+	return func(w http.ResponseWriter, req *http.Request) {
+		if origin := req.Header.Get(headerOrigin); origin != "" {
+			for key, value := range o.Header(origin) {
+				w.Header().Set(key, value)
+			}
+			if req.Method == "OPTIONS" {
+				w.WriteHeader(200)
+				return
+			}
 		}
-
-		for key, value := range opts.Header(origin) {
-			res.Header().Set(key, value)
-		}
-		if req.Method == "OPTIONS" {
-			res.WriteHeader(200)
-		}
+		next.ServeHTTP(w, req)
 	}
 }

pkg/cors/cors_test.go
@@ -23,8 +23,7 @@ import (
 )
 
 func serveHTTP(w http.ResponseWriter, req *http.Request, opts *Options) {
-	Allow(opts)(w, req)
-	w.WriteHeader(200)
+	opts.Handler(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {})).ServeHTTP(w, req)
 }
 
 func Test_AllowAll(t *testing.T) {

controller/controller.go
@@ -293,9 +293,8 @@ func appHandler(c handlerConfig) http.Handler {
 }
 
 func muxHandler(main http.Handler, authKeys []string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		httphelper.CORSAllowAllHandler(w, r)
-		if r.URL.Path == "/ping" || r.Method == "OPTIONS" {
+	return httphelper.CORSAllowAll.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/ping" {
 			w.WriteHeader(200)
 			return
 		}
@@ -315,7 +314,7 @@ func muxHandler(main http.Handler, authKeys []string) http.Handler {
 			return
 		}
 		main.ServeHTTP(w, r)
-	})
+	}))
 }
 
 type controllerAPI struct {

installer/http.go
@@ -105,18 +105,14 @@ func ServeHTTP() error {
 }
 
 func (api *httpAPI) CorsHandler(main http.Handler, addr string) http.Handler {
-	corsHandler := cors.Allow(&cors.Options{
+	return (&cors.Options{
 		AllowOrigins:     []string{addr},
 		AllowMethods:     []string{"GET", "POST"},
 		AllowHeaders:     []string{"Authorization", "Accept", "Content-Type", "If-Match", "If-None-Match"},
 		ExposeHeaders:    []string{"ETag"},
 		AllowCredentials: false,
 		MaxAge:           time.Hour,
-	})
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		corsHandler(w, r)
-		main.ServeHTTP(w, r)
-	})
+	}).Handler(main)
 }
 
 func (api *httpAPI) Asset(path string) (io.ReadSeeker, error) {

pkg/httphelper/httphelper.go
@@ -78,14 +78,14 @@ func IsRetryableError(err error) bool {
 	return ok && e.Retry
 }
 
-var CORSAllowAllHandler = cors.Allow(&cors.Options{
+var CORSAllowAll = &cors.Options{
 	AllowAllOrigins:  true,
 	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 	AllowHeaders:     []string{"Authorization", "Accept", "Content-Type", "If-Match", "If-None-Match"},
 	ExposeHeaders:    []string{"ETag"},
 	AllowCredentials: true,
 	MaxAge:           time.Hour,
-})
+}
 
 // Handler is an extended version of http.Handler that also takes a context
 // argument ctx.

commit c3a009e51a1c6bd73006ab353bf161f08971337a

dashboard,pkg/cors: Fix CORS for dashboard ping endpoint
`/ping` needs to set CORS headers for all origins (used by the installer)

19c71badd6298c7c5a727b6328bc9f3585ced403 broke this behaviour

Closes #2294

Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

dashboard/api.go
@@ -121,8 +121,19 @@ func (api *API) CorsHandler(main http.Handler) http.Handler {
 	if strings.HasPrefix(api.conf.InterfaceURL, "https") {
 		httpInterfaceURL = "http" + strings.TrimPrefix(api.conf.InterfaceURL, "https")
 	}
+	allowedOrigins := []string{api.conf.InterfaceURL, httpInterfaceURL}
 	return (&cors.Options{
-		AllowOrigins:     []string{api.conf.InterfaceURL, httpInterfaceURL},
+		ShouldAllowOrigin: func(origin string, req *http.Request) bool {
+			for _, o := range allowedOrigins {
+				if origin == o {
+					return true
+				}
+			}
+			if strings.HasSuffix(req.URL.Path, "/ping") {
+				return true
+			}
+			return false
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 		AllowHeaders:     []string{"Authorization", "Accept", "Content-Type", "If-Match", "If-None-Match"},
 		ExposeHeaders:    []string{"ETag"},

pkg/cors/cors.go
@@ -50,6 +50,8 @@ type Options struct {
 	AllowAllOrigins bool
 	// A list of allowed origins. Wild cards and FQDNs are supported.
 	AllowOrigins []string
+	// A func for determining if `origin` is allowed at request time
+	ShouldAllowOrigin func(origin string, req *http.Request) bool
 	// If set, allows to share auth credentials such as cookies.
 	AllowCredentials bool
 	// A list of allowed HTTP methods.
@@ -63,11 +65,11 @@ type Options struct {
 }
 
 // Converts options into CORS headers.
-func (o *Options) Header(origin string) (headers map[string]string) {
+func (o *Options) Header(origin string, req *http.Request) (headers map[string]string) {
 	headers = make(map[string]string)
 	// if origin is not alowed, don't extend the headers
 	// with CORS headers.
-	if !o.AllowAllOrigins && !o.IsOriginAllowed(origin) {
+	if !o.AllowAllOrigins && !o.IsOriginAllowed(origin, req) {
 		return
 	}
 
@@ -101,7 +103,10 @@ func (o *Options) Header(origin string) (headers map[string]string) {
 
 // Looks up if the origin matches one of the patterns
 // generated from Options.AllowOrigins patterns.
-func (o *Options) IsOriginAllowed(origin string) (allowed bool) {
+func (o *Options) IsOriginAllowed(origin string, req *http.Request) (allowed bool) {
+	if o.ShouldAllowOrigin != nil {
+		return o.ShouldAllowOrigin(origin, req)
+	}
 	for _, pattern := range allowOriginPatterns {
 		allowed, _ = regexp.MatchString(pattern, origin)
 		if allowed {
@@ -127,7 +132,7 @@ func (o *Options) Handler(next http.Handler) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, req *http.Request) {
 		if origin := req.Header.Get(headerOrigin); origin != "" {
-			for key, value := range o.Header(origin) {
+			for key, value := range o.Header(origin, req) {
 				w.Header().Set(key, value)
 			}
 			if req.Method == "OPTIONS" {

pkg/cors/cors_test.go
@@ -71,6 +71,40 @@ func Test_AllowRegexNoMatch(t *testing.T) {
 	}
 }
 
+func Test_AllowFuncMatch(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	origin := "https://ww.foo.com.evil.com"
+	r, _ := http.NewRequest("PUT", "foo", nil)
+	r.Header.Add("Origin", origin)
+	serveHTTP(recorder, r, &Options{
+		ShouldAllowOrigin: func(o string, req *http.Request) bool {
+			return true
+		},
+	})
+
+	headerValue := recorder.HeaderMap.Get(headerAllowOrigin)
+	if headerValue != origin {
+		t.Errorf("Allow-Origin header should be %v, found %v", origin, headerValue)
+	}
+}
+
+func Test_AllowFuncNoMatch(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	origin := "https://ww.foo.com.evil.com"
+	r, _ := http.NewRequest("PUT", "foo", nil)
+	r.Header.Add("Origin", origin)
+	serveHTTP(recorder, r, &Options{
+		ShouldAllowOrigin: func(o string, req *http.Request) bool {
+			return false
+		},
+	})
+
+	headerValue := recorder.HeaderMap.Get(headerAllowOrigin)
+	if headerValue != "" {
+		t.Errorf("Allow-Origin header should not exist, found %v", headerValue)
+	}
+}
+
 func Test_OtherHeaders(t *testing.T) {
 	recorder := httptest.NewRecorder()
 	r, _ := http.NewRequest("PUT", "foo", nil)