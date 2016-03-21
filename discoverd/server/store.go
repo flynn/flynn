flynn/flynn

commit b727a82a073b84c6d7c5bd1438e55b5c629b8a85

discoverd/server: Fix data race in AddInstance
Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

discoverd/server/store.go
@@ -450,14 +450,21 @@ func (s *Store) AddInstance(service string, inst *discoverd.Instance) error {
 		return ErrNotLeader
 	}
 
-	// Track heartbeat time, if leader.
-	s.heartbeats[instanceKey{service, inst.ID}] = time.Now()
+	if err := func() error {
+		s.mu.Lock()
+		defer s.mu.Unlock()
+		// Track heartbeat time, if leader.
+		s.heartbeats[instanceKey{service, inst.ID}] = time.Now()
 
-	// Ignore if instance already exists and it hasn't changed.
-	if m := s.data.Instances[service]; m != nil {
-		if prev := m[inst.ID]; prev != nil && inst.Equal(prev) {
-			return nil
+		// Ignore if instance already exists and it hasn't changed.
+		if m := s.data.Instances[service]; m != nil {
+			if prev := m[inst.ID]; prev != nil && inst.Equal(prev) {
+				return nil
+			}
 		}
+		return nil
+	}(); err != nil {
+		return err
 	}
 
 	// Serialize command.

commit 1bc350be0e287d0e123ba7eaa47e40e6cf9f0b03

discoverd/server: Fix regression sending heartbeats through raft
b727a82 introduced a regression that sent all heartbeats through raft,
instead of just new instances. Fix that regression while maintaining
the correct mutex usage.

Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

discoverd/server/store.go
@@ -450,22 +450,18 @@ func (s *Store) AddInstance(service string, inst *discoverd.Instance) error {
 		return ErrNotLeader
 	}
 
-	if err := func() error {
-		s.mu.Lock()
-		defer s.mu.Unlock()
-		// Track heartbeat time, if leader.
-		s.heartbeats[instanceKey{service, inst.ID}] = time.Now()
+	s.mu.Lock()
+	// Track heartbeat time, if leader.
+	s.heartbeats[instanceKey{service, inst.ID}] = time.Now()
 
-		// Ignore if instance already exists and it hasn't changed.
-		if m := s.data.Instances[service]; m != nil {
-			if prev := m[inst.ID]; prev != nil && inst.Equal(prev) {
-				return nil
-			}
+	// Ignore if instance already exists and it hasn't changed.
+	if m := s.data.Instances[service]; m != nil {
+		if prev := m[inst.ID]; prev != nil && inst.Equal(prev) {
+			s.mu.Unlock()
+			return nil
 		}
-		return nil
-	}(); err != nil {
-		return err
 	}
+	s.mu.Unlock()
 
 	// Serialize command.
 	cmd, err := json.Marshal(&addInstanceCommand{