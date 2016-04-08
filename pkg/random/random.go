e5eb8d913de5a5379c4efd7cd04d3fa32a451bae
Jonathan Rudenberg <jonathan@titanous.com>
2016-04-08 12:54:17 -0400

    pkg/random: Use random value as seed for RNG
    We don’t use random.Math for cryptographic purposes, so it just needs to
    be seeded with a random value, no need to read from /dev/urandom for every
    call.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -6,13 +6,22 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"math/big"
 	mathrand "math/rand"
 	"strings"
-
-	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/wadey/cryptorand"
+	"sync"
 )
 
-var Math = mathrand.New(cryptorand.Source)
+var Math *mathrand.Rand
+
+func init() {
+	maxInt63 := new(big.Int).SetUint64(1 << 63)
+	seed, err := rand.Int(rand.Reader, maxInt63)
+	if err != nil {
+		panic(err)
+	}
+	Math = mathrand.New(&lockedSource{src: mathrand.NewSource(seed.Int64())})
+}
 
 func String(n int) string {
 	return Hex(n/2 + 1)[:n]
@@ -42,4 +51,22 @@ func UUID() string {
 	id[8] &= 0x3F // clear variant
 	id[8] |= 0x80 // set to IETF variant
 	return fmt.Sprintf("%x-%x-%x-%x-%x", id[0:4], id[4:6], id[6:8], id[8:10], id[10:])
+}
+
+type lockedSource struct {
+	lk  sync.Mutex
+	src mathrand.Source
+}
+
+func (r *lockedSource) Int63() (n int64) {
+	r.lk.Lock()
+	n = r.src.Int63()
+	r.lk.Unlock()
+	return
+}
+
+func (r *lockedSource) Seed(seed int64) {
+	r.lk.Lock()
+	r.src.Seed(seed)
+	r.lk.Unlock()
 }
