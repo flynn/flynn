431270dabb7975d74ec6449c928c457eeab0660e
Lewis Marshall <lewis@lmars.net>
2016-05-16 03:44:42 +0100

    docker-receive: Create artifacts when manifests are pushed
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- docker-receive/main.go
+++ docker-receive/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/Sirupsen/logrus"
+	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution"
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/configuration"
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/context"
+	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/digest"
+	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/manifest"
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/registry/handlers"
+	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/registry/middleware/repository"
 	_ "github.com/flynn/flynn/Godeps/_workspace/src/github.com/docker/distribution/registry/storage/driver/filesystem"
+	"github.com/flynn/flynn/controller/client"
+	ct "github.com/flynn/flynn/controller/types"
+	"github.com/flynn/flynn/host/types"
 	"github.com/flynn/flynn/pkg/version"
 )
 
@@ -21,11 +29,23 @@ func main() {
 	ctx = context.WithValue(ctx, "version", version.String())
 	ctx = context.WithLogger(ctx, context.GetLogger(ctx, "version"))
 
+	client, err := controller.NewClient("", os.Getenv("CONTROLLER_KEY"))
+	if err != nil {
+		context.GetLogger(ctx).Fatalln(err)
+	}
+
+	middleware.Register("flynn", repositoryMiddleware(client))
+
 	config := configuration.Configuration{
 		Version: configuration.CurrentVersion,
 		Storage: configuration.Storage{
 			"filesystem": configuration.Parameters{
 				"rootdirectory": "/data",
+			},
+		},
+		Middleware: map[string][]configuration.Middleware{
+			"repository": {
+				{Name: "flynn"},
 			},
 		},
 	}
@@ -38,4 +58,73 @@ func main() {
 	if err := http.ListenAndServe(addr, app); err != nil {
 		context.GetLogger(app).Fatalln(err)
 	}
+}
+
+func repositoryMiddleware(client controller.Client) middleware.InitFunc {
+	return func(ctx context.Context, r distribution.Repository, _ map[string]interface{}) (distribution.Repository, error) {
+		return &repository{
+			Repository: r,
+			client:     client,
+		}, nil
+	}
+}
+
+// repository is a repository middleware which returns a custom ManifestService
+// in order to create Flynn artifacts when image manifests are pushed
+type repository struct {
+	distribution.Repository
+
+	client controller.Client
+}
+
+func (r *repository) Manifests(ctx context.Context, options ...distribution.ManifestServiceOption) (distribution.ManifestService, error) {
+	m, err := r.Repository.Manifests(ctx, options...)
+	if err != nil {
+		return nil, err
+	}
+	return &manifestService{
+		ManifestService: m,
+		repository:      r,
+		client:          r.client,
+	}, nil
+}
+
+type manifestService struct {
+	distribution.ManifestService
+
+	repository distribution.Repository
+	client     controller.Client
+}
+
+func (m *manifestService) Put(manifest *manifest.SignedManifest) error {
+	if err := m.ManifestService.Put(manifest); err != nil {
+		return err
+	}
+
+	dgst, err := digestManifest(manifest)
+	if err != nil {
+		return err
+	}
+
+	return m.createArtifact(dgst)
+}
+
+func (m *manifestService) createArtifact(dgst digest.Digest) error {
+	return m.client.CreateArtifact(&ct.Artifact{
+		Type: host.ArtifactTypeDocker,
+		URI:  fmt.Sprintf("http://docker-receive.discoverd?name=%s&id=%s", m.repository.Name(), dgst),
+		Meta: map[string]string{
+			"docker-receive.repository": m.repository.Name(),
+		},
+	})
+}
+
+// digestManifest is a modified version of:
+// https://github.com/docker/distribution/blob/6ba799b/registry/handlers/images.go#L228-L251
+func digestManifest(manifest *manifest.SignedManifest) (digest.Digest, error) {
+	p, err := manifest.Payload()
+	if err != nil {
+		return "", err
+	}
+	return digest.FromBytes(p)
 }
