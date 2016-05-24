c050824de04e3317068a809a1d8757ead1752e53
Lewis Marshall <lewis@lmars.net>
2016-05-25 00:04:56 +0100

    Revert "test: Fix configuring docker client (again)"
    This reverts commit 22e593d6f65169b8402c1509a5c469f2decb0817.
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/runner/runner.go
+++ test/runner/runner.go
@@ -322,7 +322,7 @@ script/configure-docker "{{ .Cluster.ClusterDomain }}"
 cli/bin/flynn cluster add \
   --tls-pin "{{ .Config.TLSPin }}" \
   --git-url "{{ .Config.GitURL }}" \
-  {{ if .Config.DockerURL }}--docker-url "{{ .Config.DockerURL }}"{{ end }} \
+  --docker-url "{{ .Config.DockerURL }}" \
   default \
   {{ .Config.ControllerURL }} \
   {{ .Config.Key }}
