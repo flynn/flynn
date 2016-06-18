a3f956c3544a4291819002ee1b05a4b80e457b34
Jonathan Rudenberg <jonathan@titanous.com>
2016-06-18 12:48:30 -0400

    vendor: Hack in docker autogen/dockerversion
    This package is generated by the docker build scripts, but is required
    to compile many packages. Manually add the file.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

---
+++ vendor/github.com/docker/docker/autogen/dockerversion/dockerversion.go
@@ -0,0 +1,11 @@
+package dockerversion
+
+var (
+	GITCOMMIT string = ""
+	VERSION   string = ""
+	BUILDTIME string = ""
+
+	IAMSTATIC string = "true"
+	INITSHA1  string = ""
+	INITPATH  string = ""
+)
