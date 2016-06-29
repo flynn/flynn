flynn/flynn

commit 6ec4de6cb5eb8c6b9f098d0402fe79f824719ab1

all: Move route TLS cert management into own object
`route.Certificate` replaces `route.TLSCert` and `route.TLSKey`.

Added router API endpoints:
`POST /certificates` creates a certificate and assigns it to zero or more routes.
`GET /certificates/:id` returns a certificate.
`DELETE /certificates/:id` removes the specified certificate.
`GET /certificates/:id/routes` returns all routes assigned to the specified certificate.
`GET /certificates` returns all certificates.

CLI uses `route.LegacyTLSCert` and `route.LegacyTLSKey` when sending data
to server to maintain backwards compatibility.

Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

controller/worker/domain_migration/domain_migration.go
@@ -369,12 +369,13 @@ func (m *migration) appMaybeCreateRoute(appID string, oldRoute *router.Route, ro
 		Sticky:  oldRoute.Sticky,
 		Service: oldRoute.Service,
 	}
-	if oldRoute.TLSCert == m.dm.OldTLSCert.Cert {
-		route.TLSCert = m.dm.TLSCert.Cert
-		route.TLSKey = m.dm.TLSCert.PrivateKey
+	if oldRoute.Certificate != nil && oldRoute.Certificate.Cert == m.dm.OldTLSCert.Cert {
+		route.Certificate = &router.Certificate{
+			Cert: m.dm.TLSCert.Cert,
+			Key:  m.dm.TLSCert.PrivateKey,
+		}
 	} else {
-		route.TLSCert = oldRoute.TLSCert
-		route.TLSKey = oldRoute.TLSKey
+		route.Certificate = oldRoute.Certificate
 	}
 	err := m.client.CreateRoute(appID, route)
 	if err != nil && err.Error() == "conflict: Duplicate route" {

bootstrap/add_route_action.go
@@ -44,8 +44,10 @@ func (a *AddRouteAction) Run(s *State) error {
 			if err != nil {
 				return err
 			}
-			route.TLSCert = cert.Cert
-			route.TLSKey = cert.PrivateKey
+			route.Certificate = &router.Certificate{
+				Cert: cert.Cert,
+				Key:  cert.PrivateKey,
+			}
 		}
 		a.Route = route.ToRoute()
 	}

cli/app.go
@@ -184,7 +184,7 @@ func runInfo(_ *docopt.Args, client controller.Client) error {
 			if k.Type == "http" {
 				route := k.HTTPRoute()
 				protocol := "https"
-				if route.TLSCert == "" {
+				if route.Certificate == nil && route.LegacyTLSCert == "" {
 					protocol = "http"
 				}
 				listRec(w, "Web URL:", protocol+"://"+route.Domain)

cli/route.go
@@ -97,7 +97,8 @@ func runRoute(args *docopt.Args, client controller.Client) error {
 		case "http":
 			route = k.HTTPRoute().Domain
 			service = k.TCPRoute().Service
-			if k.HTTPRoute().TLSCert == "" {
+			httpRoute := k.HTTPRoute()
+			if httpRoute.Certificate == nil && httpRoute.LegacyTLSCert == "" {
 				protocol = "http"
 			} else {
 				protocol = "https"
@@ -157,13 +158,13 @@ func runRouteAddHTTP(args *docopt.Args, client controller.Client) error {
 	}
 
 	hr := &router.HTTPRoute{
-		Service: service,
-		Domain:  u.Host,
-		TLSCert: tlsCert,
-		TLSKey:  tlsKey,
-		Sticky:  args.Bool["--sticky"],
-		Leader:  args.Bool["--leader"],
-		Path:    u.Path,
+		Service:       service,
+		Domain:        u.Host,
+		LegacyTLSCert: tlsCert,
+		LegacyTLSKey:  tlsKey,
+		Sticky:        args.Bool["--sticky"],
+		Leader:        args.Bool["--leader"],
+		Path:          u.Path,
 	}
 	route := hr.ToRoute()
 	if err := client.CreateRoute(mustApp(), route); err != nil {
@@ -215,7 +216,8 @@ func runRouteUpdateHTTP(args *docopt.Args, client controller.Client) error {
 		route.Service = service
 	}
 
-	route.TLSCert, route.TLSKey, err = parseTLSCert(args)
+	route.Certificate = nil
+	route.LegacyTLSCert, route.LegacyTLSKey, err = parseTLSCert(args)
 	if err != nil {
 		return err
 	}

controller/routes_test.go
@@ -73,6 +73,26 @@ func (r *fakeRouter) StreamEvents(output chan *router.StreamEvent) (stream.Strea
 	return &fakeStream{}, nil
 }
 
+func (r *fakeRouter) CreateCert(cert *router.Certificate) error {
+	return nil
+}
+
+func (r *fakeRouter) GetCert(id string) (*router.Certificate, error) {
+	return nil, nil
+}
+
+func (r *fakeRouter) DeleteCert(id string) error {
+	return nil
+}
+
+func (r *fakeRouter) ListCerts() ([]*router.Certificate, error) {
+	return nil, nil
+}
+
+func (r *fakeRouter) ListCertRoutes(id string) ([]*router.Route, error) {
+	return nil, nil
+}
+
 type sortedRoutes []*router.Route
 
 func (p sortedRoutes) Len() int           { return len(p) }

router/api.go
@@ -30,6 +30,11 @@ func apiHandler(rtr *Router) http.Handler {
 	r.GET("/routes", httphelper.WrapHandler(api.GetRoutes))
 	r.GET("/routes/:route_type/:id", httphelper.WrapHandler(api.GetRoute))
 	r.DELETE("/routes/:route_type/:id", httphelper.WrapHandler(api.DeleteRoute))
+	r.POST("/certificates", httphelper.WrapHandler(api.CreateCert))
+	r.GET("/certificates/:id", httphelper.WrapHandler(api.GetCert))
+	r.GET("/certificates/:id/routes", httphelper.WrapHandler(api.GetCertRoutes))
+	r.DELETE("/certificates/:id", httphelper.WrapHandler(api.DeleteCert))
+	r.GET("/certificates", httphelper.WrapHandler(api.GetCerts))
 	r.GET("/events", httphelper.WrapHandler(api.StreamEvents))
 
 	r.HandlerFunc("GET", "/debug/*path", pprof.Handler.ServeHTTP)
@@ -197,6 +202,89 @@ func (api *API) DeleteRoute(ctx context.Context, w http.ResponseWriter, req *htt
 			return
 		default:
 			log.Error(err.Error())
+			httphelper.Error(w, err)
+			return
+		}
+	}
+	w.WriteHeader(200)
+}
+
+func (api *API) CreateCert(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	var cert *router.Certificate
+	if err := json.NewDecoder(req.Body).Decode(&cert); err != nil {
+		httphelper.Error(w, err)
+		return
+	}
+
+	l := api.router.HTTP.(*HTTPListener)
+	err := l.AddCert(cert)
+	if err != nil {
+		jsonError := httphelper.JSONError{}
+		switch err {
+		case ErrConflict:
+			jsonError.Code = httphelper.ConflictErrorCode
+			jsonError.Message = "Duplicate cert"
+		case ErrInvalid:
+			jsonError.Code = httphelper.ValidationErrorCode
+			jsonError.Message = "Invalid cert"
+		default:
+			httphelper.Error(w, err)
+			return
+		}
+	}
+	httphelper.JSON(w, 200, cert)
+}
+
+func (api *API) GetCert(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	params, _ := ctxhelper.ParamsFromContext(ctx)
+
+	l := api.router.HTTP.(*HTTPListener)
+	cert, err := l.GetCert(params.ByName("id"))
+	if err == ErrNotFound {
+		httphelper.ObjectNotFoundError(w, "certificate not found")
+		return
+	}
+	if err != nil {
+		httphelper.Error(w, err)
+		return
+	}
+
+	httphelper.JSON(w, 200, cert)
+}
+
+func (api *API) GetCertRoutes(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	params, _ := ctxhelper.ParamsFromContext(ctx)
+
+	l := api.router.HTTP.(*HTTPListener)
+	routes, err := l.GetCertRoutes(params.ByName("id"))
+	if err != nil {
+		httphelper.Error(w, err)
+		return
+	}
+	httphelper.JSON(w, 200, routes)
+}
+
+func (api *API) GetCerts(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	l := api.router.HTTP.(*HTTPListener)
+	certs, err := l.GetCerts()
+	if err != nil {
+		httphelper.Error(w, err)
+		return
+	}
+	httphelper.JSON(w, 200, certs)
+}
+
+func (api *API) DeleteCert(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	params, _ := ctxhelper.ParamsFromContext(ctx)
+
+	l := api.router.HTTP.(*HTTPListener)
+	err := l.RemoveCert(params.ByName("id"))
+	if err != nil {
+		switch err {
+		case ErrNotFound:
+			httphelper.ObjectNotFoundError(w, "certificate not found")
+			return
+		default:
 			httphelper.Error(w, err)
 			return
 		}

router/client/client.go
@@ -65,6 +65,17 @@ type Client interface {
 	// are filtered by the reference (ex: "controller/apps/myapp").
 	ListRoutes(parentRef string) ([]*router.Route, error)
 	StreamEvents(output chan *router.StreamEvent) (stream.Stream, error)
+
+	// CreateCert creates a new route certificate.
+	CreateCert(*router.Certificate) error
+	// GetCert returns a route cert with specified id.
+	GetCert(id string) (*router.Certificate, error)
+	// DeleteCert deletes the route certificate with the specified id.
+	DeleteCert(id string) error
+	// ListCerts returns a list of certificates.
+	ListCerts() ([]*router.Certificate, error)
+	// ListCertRoutes returns a list of routes assigned to the specified certificate.
+	ListCertRoutes(id string) ([]*router.Route, error)
 }
 
 func (c *client) CreateRoute(r *router.Route) error {
@@ -99,4 +110,30 @@ func (c *client) ListRoutes(parentRef string) ([]*router.Route, error) {
 
 func (c *client) StreamEvents(output chan *router.StreamEvent) (stream.Stream, error) {
 	return c.ResumingStream("GET", "/events", output)
+}
+
+func (c *client) CreateCert(cert *router.Certificate) error {
+	return c.Post("/certificates", cert, cert)
+}
+
+func (c *client) GetCert(id string) (*router.Certificate, error) {
+	res := &router.Certificate{}
+	err := c.Get(fmt.Sprintf("/certificates/%s", id), res)
+	return res, err
+}
+
+func (c *client) DeleteCert(id string) error {
+	return c.Delete("/certificates/" + id)
+}
+
+func (c *client) ListCerts() ([]*router.Certificate, error) {
+	var res []*router.Certificate
+	err := c.Get("/certificates", &res)
+	return res, err
+}
+
+func (c *client) ListCertRoutes(id string) ([]*router.Route, error) {
+	var res []*router.Route
+	err := c.Get(fmt.Sprintf("/certificates/%s/routes", id), &res)
+	return res, err
 }

router/data_store.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/jackc/pgx"
@@ -17,10 +20,15 @@ var ErrInvalid = errors.New("router: invalid route")
 
 type DataStore interface {
 	Add(route *router.Route) error
+	AddCert(cert *router.Certificate) error
 	Update(route *router.Route) error
 	Get(id string) (*router.Route, error)
+	GetCert(id string) (*router.Certificate, error)
 	List() ([]*router.Route, error)
+	ListCerts() ([]*router.Certificate, error)
+	ListCertRoutes(id string) ([]*router.Route, error)
 	Remove(id string) error
+	RemoveCert(id string) error
 	Sync(ctx context.Context, h SyncHandler, startc chan<- struct{}) error
 	Ping() error
 }
@@ -45,10 +53,12 @@ type pgDataStore struct {
 }
 
 const (
-	routeTypeHTTP = "http"
-	routeTypeTCP  = "tcp"
-	tableNameHTTP = "http_routes"
-	tableNameTCP  = "tcp_routes"
+	routeTypeHTTP              = "http"
+	routeTypeTCP               = "tcp"
+	tableNameHTTP              = "http_routes"
+	tableNameTCP               = "tcp_routes"
+	tableNameCertificates      = "certificates"
+	tableNameRoutesCertificate = "route_certificates"
 )
 
 // NewPostgresDataStore returns a DataStore that stores route information in a
@@ -77,8 +87,8 @@ func (d *pgDataStore) Ping() error {
 }
 
 const sqlAddRouteHTTP = `
-INSERT INTO ` + tableNameHTTP + ` (parent_ref, service, leader, domain, tls_cert, tls_key, sticky, path)
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+INSERT INTO ` + tableNameHTTP + ` (parent_ref, service, leader, domain, sticky, path)
+	VALUES ($1, $2, $3, $4, $5, $6)
 	RETURNING id, created_at, updated_at`
 
 const sqlAddRouteTCP = `
@@ -89,38 +99,226 @@ INSERT INTO ` + tableNameTCP + ` (parent_ref, service, leader, port)
 func (d *pgDataStore) Add(r *router.Route) (err error) {
 	switch d.tableName {
 	case tableNameHTTP:
-		err = d.pgx.QueryRow(
-			sqlAddRouteHTTP,
-			r.ParentRef,
-			r.Service,
-			r.Leader,
-			r.Domain,
-			r.TLSCert,
-			r.TLSKey,
-			r.Sticky,
-			r.Path,
-		).Scan(&r.ID, &r.CreatedAt, &r.UpdatedAt)
+		err = d.addHTTP(r)
 	case tableNameTCP:
-		err = d.pgx.QueryRow(
-			sqlAddRouteTCP,
-			r.ParentRef,
-			r.Service,
-			r.Leader,
-			r.Port,
-		).Scan(&r.ID, &r.CreatedAt, &r.UpdatedAt)
+		err = d.addTCP(r)
 	}
 	r.Type = d.routeType
-	if postgres.IsUniquenessError(err, "") {
-		err = ErrConflict
-	} else if postgres.IsPostgresCode(err, postgres.RaiseException) {
-		err = ErrInvalid
+	if err != nil {
+		if postgres.IsUniquenessError(err, "") {
+			err = ErrConflict
+		} else if postgres.IsPostgresCode(err, postgres.RaiseException) {
+			err = ErrInvalid
+		}
+		return err
 	}
-	return err
+	return nil
+}
+
+func (d *pgDataStore) addHTTP(r *router.Route) error {
+	tx, err := d.pgx.Begin()
+	if err != nil {
+		return err
+	}
+	if err := tx.QueryRow(
+		sqlAddRouteHTTP,
+		r.ParentRef,
+		r.Service,
+		r.Leader,
+		r.Domain,
+		r.Sticky,
+		r.Path,
+	).Scan(&r.ID, &r.CreatedAt, &r.UpdatedAt); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err := d.addRouteCertWithTx(tx, r); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (d *pgDataStore) addTCP(r *router.Route) error {
+	return d.pgx.QueryRow(
+		sqlAddRouteTCP,
+		r.ParentRef,
+		r.Service,
+		r.Leader,
+		r.Port,
+	).Scan(&r.ID, &r.CreatedAt, &r.UpdatedAt)
+}
+
+const sqlSelectCert = `
+SELECT id, created_at, updated_at FROM ` + tableNameCertificates + `
+	WHERE cert_sha256 = $1 AND deleted_at IS NULL`
+
+const sqlAddCert = `
+INSERT INTO ` + tableNameCertificates + ` (cert, key, cert_sha256)
+	VALUES ($1, $2, $3)
+	RETURNING id, created_at, updated_at
+`
+
+const sqlAddCertificate = `
+INSERT INTO ` + tableNameRoutesCertificate + ` (http_route_id, certificate_id)
+	VALUES ($1, $2)
+`
+
+const sqlCleanupCertificates = `
+DELETE FROM ` + tableNameRoutesCertificate + `
+	WHERE http_route_id = $1`
+
+func (d *pgDataStore) AddCert(c *router.Certificate) error {
+	tx, err := d.pgx.Begin()
+	if err != nil {
+		return err
+	}
+	if err := d.addCertWithTx(tx, c); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (d *pgDataStore) addCertWithTx(tx *pgx.Tx, c *router.Certificate) error {
+	c.Cert = strings.Trim(c.Cert, " \n")
+	c.Key = strings.Trim(c.Key, " \n")
+	tlsCertSHA256 := hex.EncodeToString(sha256.New().Sum([]byte(c.Cert)))
+	if err := tx.QueryRow(sqlSelectCert, tlsCertSHA256).Scan(&c.ID, &c.CreatedAt, &c.UpdatedAt); err != nil {
+		if err := tx.QueryRow(sqlAddCert, c.Cert, c.Key, tlsCertSHA256).Scan(&c.ID, &c.CreatedAt, &c.UpdatedAt); err != nil {
+			return err
+		}
+	}
+	for _, rid := range c.Routes {
+		if _, err := tx.Exec(sqlCleanupCertificates, rid); err != nil {
+			return err
+		}
+		if _, err := tx.Exec(sqlAddCertificate, rid, c.ID); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *pgDataStore) addRouteCertWithTx(tx *pgx.Tx, r *router.Route) error {
+	var cert *router.Certificate
+	if r.LegacyTLSCert != "" || r.LegacyTLSKey != "" {
+		cert = &router.Certificate{
+			Cert: r.LegacyTLSCert,
+			Key:  r.LegacyTLSKey,
+		}
+	} else {
+		cert = r.Certificate
+	}
+	if cert == nil {
+		return nil
+	}
+	cert.Routes = []string{r.ID}
+	if err := d.addCertWithTx(tx, cert); err != nil {
+		return err
+	}
+	r.Certificate = &router.Certificate{
+		ID:        cert.ID,
+		Cert:      cert.Cert,
+		Key:       cert.Key,
+		CreatedAt: cert.CreatedAt,
+		UpdatedAt: cert.UpdatedAt,
+	}
+	return nil
+}
+
+const sqlGetCert = `
+SELECT ` + selectColumnsHTTPCert + `, ARRAY(
+	SELECT http_route_id::varchar FROM ` + tableNameRoutesCertificate + ` WHERE certificate_id = $1
+) FROM ` + tableNameCertificates + ` AS c WHERE c.id = $1
+`
+
+func (d *pgDataStore) GetCert(id string) (*router.Certificate, error) {
+	cert := &router.Certificate{Routes: []string{}}
+	if err := d.pgx.QueryRow(sqlGetCert, id).Scan(&cert.ID, &cert.Cert, &cert.Key, &cert.CreatedAt, &cert.UpdatedAt, &cert.Routes); err != nil {
+		return nil, err
+	}
+	return cert, nil
+}
+
+const sqlListCerts = `
+SELECT ` + selectColumnsHTTPCert + `, ARRAY(
+	SELECT http_route_id::varchar FROM ` + tableNameRoutesCertificate + ` AS rc WHERE rc.certificate_id = c.id
+) FROM ` + tableNameCertificates + ` AS c
+`
+
+func (d *pgDataStore) ListCerts() ([]*router.Certificate, error) {
+	rows, err := d.pgx.Query(sqlListCerts)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	certs := []*router.Certificate{}
+	for rows.Next() {
+		cert := &router.Certificate{Routes: []string{}}
+		if err := rows.Scan(&cert.ID, &cert.Cert, &cert.Key, &cert.CreatedAt, &cert.UpdatedAt, &cert.Routes); err != nil {
+			return nil, err
+		}
+		certs = append(certs, cert)
+	}
+	return certs, rows.Err()
+}
+
+const sqlListCertRoutes = `
+SELECT ` + selectColumnsHTTP + ` FROM ` + tableNameHTTP + ` AS r
+INNER JOIN ` + tableNameRoutesCertificate + ` AS rc
+	ON rc.http_route_id = r.id AND rc.certificate_id = $1
+`
+
+func (d *pgDataStore) ListCertRoutes(id string) ([]*router.Route, error) {
+	rows, err := d.pgx.Query(sqlListCertRoutes, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	routes := []*router.Route{}
+	for rows.Next() {
+		r := &router.Route{}
+		err := d.scanRouteWithoutCert(r, rows)
+		if err != nil {
+			return nil, err
+		}
+		r.Type = d.routeType
+		routes = append(routes, r)
+	}
+	return routes, rows.Err()
+}
+
+const sqlRemoveCert = `
+UPDATE ` + tableNameCertificates + ` SET deleted_at = now() WHERE id = $1
+`
+
+const sqlRemoveRoutesCert = `
+DELETE FROM ` + tableNameRoutesCertificate + ` WHERE certificate_id = $1
+`
+
+func (d *pgDataStore) RemoveCert(id string) error {
+	tx, err := d.pgx.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Exec(sqlRemoveCert, id); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := tx.Exec(sqlRemoveRoutesCert, id); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
 }
 
 const sqlUpdateRouteHTTP = `
-UPDATE ` + tableNameHTTP + ` SET parent_ref = $1, service = $2, leader = $3, tls_cert = $4, tls_key = $5, sticky = $6, path = $7
-	WHERE id = $8 AND domain = $9 AND deleted_at IS NULL
+UPDATE ` + tableNameHTTP + ` AS r
+	SET parent_ref = $1, service = $2, leader = $3, sticky = $4, path = $5
+	WHERE id = $6 AND domain = $7 AND deleted_at IS NULL
 	RETURNING %s`
 
 const sqlUpdateRouteTCP = `
@@ -129,37 +327,54 @@ UPDATE ` + tableNameTCP + ` SET parent_ref = $1, service = $2, leader = $3
 	RETURNING %s`
 
 func (d *pgDataStore) Update(r *router.Route) error {
-	var row *pgx.Row
+	var err error
 
 	switch d.tableName {
 	case tableNameHTTP:
-		row = d.pgx.QueryRow(
-			fmt.Sprintf(sqlUpdateRouteHTTP, d.columnNames()),
-			r.ParentRef,
-			r.Service,
-			r.Leader,
-			r.TLSCert,
-			r.TLSKey,
-			r.Sticky,
-			r.Path,
-			r.ID,
-			r.Domain,
-		)
+		err = d.updateHTTP(r)
 	case tableNameTCP:
-		row = d.pgx.QueryRow(
-			fmt.Sprintf(sqlUpdateRouteTCP, d.columnNames()),
-			r.ParentRef,
-			r.Service,
-			r.Leader,
-			r.ID,
-			r.Port,
-		)
+		err = d.updateTCP(r)
 	}
-	err := d.scanRoute(r, row)
 	if err == pgx.ErrNoRows {
 		return ErrNotFound
 	}
 	return err
+}
+
+func (d *pgDataStore) updateHTTP(r *router.Route) error {
+	tx, err := d.pgx.Begin()
+	if err != nil {
+		return err
+	}
+	if err := d.scanRouteWithoutCert(r, d.pgx.QueryRow(
+		fmt.Sprintf(sqlUpdateRouteHTTP, selectColumnsHTTP),
+		r.ParentRef,
+		r.Service,
+		r.Leader,
+		r.Sticky,
+		r.Path,
+		r.ID,
+		r.Domain,
+	)); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err := d.addRouteCertWithTx(tx, r); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (d *pgDataStore) updateTCP(r *router.Route) error {
+	return d.scanRoute(r, d.pgx.QueryRow(
+		fmt.Sprintf(sqlUpdateRouteTCP, d.columnNames()),
+		r.ParentRef,
+		r.Service,
+		r.Leader,
+		r.ID,
+		r.Port,
+	))
 }
 
 const sqlRemoveRoute = `UPDATE %s SET deleted_at = now() WHERE id = $1`
@@ -172,14 +387,26 @@ func (d *pgDataStore) Remove(id string) error {
 	return err
 }
 
-const sqlGetRoute = `SELECT %s FROM %s WHERE id = $1 AND deleted_at IS NULL`
+const sqlGetHTTPRoute = `
+SELECT %s FROM %s AS r
+	LEFT OUTER JOIN %s AS rc ON r.id = rc.http_route_id
+	LEFT OUTER JOIN %s AS c ON c.id = rc.certificate_id
+	WHERE r.id = $1 AND r.deleted_at IS NULL`
+
+const sqlGetTCPRoute = `SELECT %s FROM %s WHERE id = $1 AND deleted_at IS NULL`
 
 func (d *pgDataStore) Get(id string) (*router.Route, error) {
 	if id == "" {
 		return nil, ErrNotFound
 	}
 
-	query := fmt.Sprintf(sqlGetRoute, d.columnNames(), d.tableName)
+	var query string
+	switch d.tableName {
+	case tableNameHTTP:
+		query = fmt.Sprintf(sqlGetHTTPRoute, d.columnNames(), d.tableName, tableNameRoutesCertificate, tableNameCertificates)
+	case tableNameTCP:
+		query = fmt.Sprintf(sqlGetTCPRoute, d.columnNames(), d.tableName)
+	}
 	row := d.pgx.QueryRow(query, id)
 
 	r := &router.Route{}
@@ -194,10 +421,22 @@ func (d *pgDataStore) Get(id string) (*router.Route, error) {
 	return r, nil
 }
 
-const sqlListRoutes = `SELECT %s FROM %s WHERE deleted_at IS NULL`
+const sqlListHTTPRoutes = `
+SELECT %s FROM %s AS r
+	LEFT OUTER JOIN %s AS rc ON r.id = rc.http_route_id
+	LEFT OUTER JOIN %s AS c ON c.id = rc.certificate_id
+	WHERE r.deleted_at IS NULL`
+
+const sqlListTCPRoutes = `SELECT %s FROM %s WHERE deleted_at IS NULL`
 
 func (d *pgDataStore) List() ([]*router.Route, error) {
-	query := fmt.Sprintf(sqlListRoutes, d.columnNames(), d.tableName)
+	var query string
+	switch d.tableName {
+	case tableNameHTTP:
+		query = fmt.Sprintf(sqlListHTTPRoutes, d.columnNames(), d.tableName, tableNameRoutesCertificate, tableNameCertificates)
+	case tableNameTCP:
+		query = fmt.Sprintf(sqlListTCPRoutes, d.columnNames(), d.tableName)
+	}
 	rows, err := d.pgx.Query(query)
 	if err != nil {
 		return nil, err
@@ -325,14 +564,15 @@ func (d *pgDataStore) startListener(ctx context.Context) (<-chan string, <-chan 
 }
 
 const (
-	selectColumnsHTTP = "id, parent_ref, service, leader, domain, sticky, tls_cert, tls_key, path, created_at, updated_at"
-	selectColumnsTCP  = "id, parent_ref, service, leader, port, created_at, updated_at"
+	selectColumnsHTTP     = "r.id, r.parent_ref, r.service, r.leader, r.domain, r.sticky, r.path, r.created_at, r.updated_at"
+	selectColumnsHTTPCert = "c.id, c.cert, c.key, c.created_at, c.updated_at"
+	selectColumnsTCP      = "id, parent_ref, service, leader, port, created_at, updated_at"
 )
 
 func (d *pgDataStore) columnNames() string {
 	switch d.routeType {
 	case routeTypeHTTP:
-		return selectColumnsHTTP
+		return selectColumnsHTTP + ", " + selectColumnsHTTPCert
 	case routeTypeTCP:
 		return selectColumnsTCP
 	default:
@@ -344,7 +584,7 @@ type scannable interface {
 	Scan(dest ...interface{}) (err error)
 }
 
-func (d *pgDataStore) scanRoute(route *router.Route, s scannable) error {
+func (d *pgDataStore) scanRouteWithoutCert(route *router.Route, s scannable) error {
 	route.Type = d.routeType
 	switch d.tableName {
 	case tableNameHTTP:
@@ -355,12 +595,58 @@ func (d *pgDataStore) scanRoute(route *router.Route, s scannable) error {
 			&route.Leader,
 			&route.Domain,
 			&route.Sticky,
-			&route.TLSCert,
-			&route.TLSKey,
 			&route.Path,
 			&route.CreatedAt,
 			&route.UpdatedAt,
 		)
+	case tableNameTCP:
+		return s.Scan(
+			&route.ID,
+			&route.ParentRef,
+			&route.Service,
+			&route.Leader,
+			&route.Port,
+			&route.CreatedAt,
+			&route.UpdatedAt,
+		)
+	}
+	panic("unknown tableName: " + d.tableName)
+}
+
+func (d *pgDataStore) scanRoute(route *router.Route, s scannable) error {
+	route.Type = d.routeType
+	switch d.tableName {
+	case tableNameHTTP:
+		var certID, certCert, certKey *string
+		var certCreatedAt, certUpdatedAt *time.Time
+		if err := s.Scan(
+			&route.ID,
+			&route.ParentRef,
+			&route.Service,
+			&route.Leader,
+			&route.Domain,
+			&route.Sticky,
+			&route.Path,
+			&route.CreatedAt,
+			&route.UpdatedAt,
+			&certID,
+			&certCert,
+			&certKey,
+			&certCreatedAt,
+			&certUpdatedAt,
+		); err != nil {
+			return err
+		}
+		if certID != nil {
+			route.Certificate = &router.Certificate{
+				ID:        *certID,
+				Cert:      *certCert,
+				Key:       *certKey,
+				CreatedAt: *certCreatedAt,
+				UpdatedAt: *certUpdatedAt,
+			}
+		}
+		return nil
 	case tableNameTCP:
 		return s.Scan(
 			&route.ID,

commit 560aa870173062506bae561b711d07dec3e21a8e

controller,test: Fix domain migration to correctly set route certs
Bug introduced in 6ec4de6cb5eb8c6b9f098d0402fe79f824719ab1 (#2890)

Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

controller/worker/domain_migration/domain_migration.go
@@ -375,7 +375,7 @@ func (m *migration) appMaybeCreateRoute(appID string, oldRoute *router.Route, ro
 		Sticky:  oldRoute.Sticky,
 		Service: oldRoute.Service,
 	}
-	if oldRoute.Certificate != nil && oldRoute.Certificate.Cert == m.dm.OldTLSCert.Cert {
+	if oldRoute.Certificate != nil && oldRoute.Certificate.Cert == strings.TrimSpace(m.dm.OldTLSCert.Cert) {
 		route.Certificate = &router.Certificate{
 			Cert: m.dm.TLSCert.Cert,
 			Key:  m.dm.TLSCert.PrivateKey,

test/test_z_domain_migration.go
@@ -9,6 +9,7 @@ import (
 
 	ct "github.com/flynn/flynn/controller/types"
 	"github.com/flynn/flynn/pkg/dialer"
+	"github.com/flynn/flynn/router/types"
 	c "github.com/flynn/go-check"
 )
 
@@ -20,7 +21,7 @@ type ZDomainMigrationSuite struct {
 
 var _ = c.Suite(&ZDomainMigrationSuite{})
 
-func (s *ZDomainMigrationSuite) migrateDomain(t *c.C, dm *ct.DomainMigration) {
+func (s *ZDomainMigrationSuite) migrateDomain(t *c.C, dm *ct.DomainMigration) *ct.DomainMigration {
 	debugf(t, "migrating domain from %s to %s", dm.OldDomain, dm.Domain)
 	client := s.controllerClient(t)
 
@@ -62,7 +63,6 @@ func (s *ZDomainMigrationSuite) migrateDomain(t *c.C, dm *ct.DomainMigration) {
 	t.Assert(event.DomainMigration.ID, c.Equals, dm.ID)
 	t.Assert(event.DomainMigration.OldDomain, c.Equals, dm.OldDomain)
 	t.Assert(event.DomainMigration.Domain, c.Equals, dm.Domain)
-	t.Assert(event.DomainMigration.TLSCert, c.IsNil)
 	t.Assert(event.DomainMigration.OldTLSCert, c.NotNil)
 	t.Assert(event.DomainMigration.CreatedAt, c.NotNil)
 	t.Assert(event.DomainMigration.CreatedAt.Equal(*dm.CreatedAt), c.Equals, true)
@@ -101,6 +101,20 @@ func (s *ZDomainMigrationSuite) migrateDomain(t *c.C, dm *ct.DomainMigration) {
 	t.Assert(dashboardRelease.Env["URL"], c.Equals, fmt.Sprintf("https://dashboard.%s", dm.Domain))
 	t.Assert(dashboardRelease.Env["CA_CERT"], c.Equals, cert.CACert)
 
+	routes, err := client.RouteList("controller")
+	t.Assert(err, c.IsNil)
+	t.Assert(len(routes), c.Equals, 2) // one for both new and old domain
+	var route *router.Route
+	for _, r := range routes {
+		if strings.HasSuffix(r.Domain, dm.Domain) {
+			route = r
+			break
+		}
+	}
+	t.Assert(route, c.Not(c.IsNil))
+	t.Assert(route.Domain, c.Equals, fmt.Sprintf("controller.%s", dm.Domain))
+	t.Assert(route.Certificate.Cert, c.Equals, strings.TrimSuffix(cert.Cert, "\n"))
+
 	var doPing func(string, int)
 	doPing = func(component string, retriesRemaining int) {
 		url := fmt.Sprintf("http://%s.%s/ping", component, dm.Domain)
@@ -116,6 +130,8 @@ func (s *ZDomainMigrationSuite) migrateDomain(t *c.C, dm *ct.DomainMigration) {
 	}
 	doPing("controller", 3)
 	doPing("dashboard", 3)
+
+	return event.DomainMigration
 }
 
 func (s *ZDomainMigrationSuite) TestDomainMigration(t *c.C) {
@@ -135,7 +151,7 @@ func (s *ZDomainMigrationSuite) TestDomainMigration(t *c.C) {
 		OldDomain: oldDomain,
 		Domain:    fmt.Sprintf("%s.xip.io", routerIP),
 	}
-	s.migrateDomain(t, dm)
+	dm = s.migrateDomain(t, dm)
 
 	// make sure a new route was created for the app
 	appRoutes, err = cc.RouteList(app.ID)
@@ -147,6 +163,7 @@ func (s *ZDomainMigrationSuite) TestDomainMigration(t *c.C) {
 	s.migrateDomain(t, &ct.DomainMigration{
 		OldDomain: dm.Domain,
 		Domain:    dm.OldDomain,
+		TLSCert:   dm.OldTLSCert,
 	})
 
 	// app should still only have the two routes