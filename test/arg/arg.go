1f9eb6df12c071d43fc2be060910d34910102f83
Lewis Marshall <lewis@lmars.net>
2016-07-02 03:06:17 +0100

    test: Use the default flynn-host backend
    This allows us to test changing the default backend (for example to
    libcontainer) without needing to modify the test runner.
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/arg/arg.go
+++ test/arg/arg.go
@@ -37,7 +37,6 @@ func Parse() *Args {
 	flag.StringVar(&args.BootConfig.Kernel, "kernel", "rootfs/vmlinuz", "path to the Linux binary")
 	flag.StringVar(&args.BootConfig.Network, "network", "10.52.0.1/24", "the network to use for vms")
 	flag.StringVar(&args.BootConfig.NatIface, "nat", "eth0", "the interface to provide NAT to vms")
-	flag.StringVar(&args.BootConfig.Backend, "backend", "libvirt-lxc", "the host backend to use")
 	flag.StringVar(&args.RootFS, "rootfs", "rootfs/rootfs.img", "filesystem image to use with QEMU")
 	flag.StringVar(&args.CLI, "cli", "flynn", "path to flynn-cli binary")
 	flag.StringVar(&args.FlynnHost, "flynn-host", "../host/bin/flynn-host", "path to flynn-host binary")
