7967db5e1fe541d4eb9f0d48ca515ee84db57421
Lewis Marshall <lewis@lmars.net>
2016-09-13 19:57:56 +0100

    vendor: Add singleflight, uritemplates, sparse
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

---
+++ vendor/github.com/rancher/sparse-tools/sparse/failpoint.go
@@ -0,0 +1,29 @@
+package sparse
+
+import (
+	"sync"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+var failFileHashMatch = false
+var mutex sync.Mutex
+
+// SetFailPointFileHashMatch simulates file hash match failure
+func SetFailPointFileHashMatch(fail bool) {
+	mutex.Lock()
+	failFileHashMatch = fail
+	mutex.Unlock()
+}
+
+// FailPointFileHashMatch returns true if this failpoint is set, clears the failpoint
+func FailPointFileHashMatch() bool {
+	mutex.Lock()
+	val := failFileHashMatch
+	if val {
+		log.Warn("FailPointFileHashMatch!")
+		failFileHashMatch = false
+	}
+	mutex.Unlock()
+	return val
+}
