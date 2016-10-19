042367cf1bed33446c07705de45a6e2f8779d0fc
Simon Soriano <simon0191@gmail.com>
2016-10-19 17:40:56 -0500

    cli: Let `flynn kill` receive a list of jobs
    Signed-off-by: Simon Soriano <simon0191@gmail.com>

--- cli/kill.go
+++ cli/kill.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 
 	"github.com/flynn/flynn/controller/client"
@@ -9,16 +10,23 @@ import (
 
 func init() {
 	register("kill", runKill, `
-usage: flynn kill <job>
+usage: flynn kill <job>...
 
-Kill a job.`)
+Kill running jobs.`)
 }
 
 func runKill(args *docopt.Args, client controller.Client) error {
-	job := args.String["<job>"]
-	if err := client.DeleteJob(mustApp(), job); err != nil {
-		return err
+	success := true
+	for _, job := range args.All["<job>"].([]string) {
+		if err := client.DeleteJob(mustApp(), job); err != nil {
+			success = false
+			log.Printf("ERROR: could not kill job %s: %s\n", job, err)
+			continue
+		}
+		log.Printf("Job %s killed.", job)
 	}
-	log.Printf("Job %s killed.", job)
+	if !success {
+		return errors.New("Could not kill all jobs.")
+	}
 	return nil
 }
