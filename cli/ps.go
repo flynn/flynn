d161f8f424c29dba5f17c2acc45285d569811059
Simon Soriano <simon0191@gmail.com>
2016-10-19 21:09:47 -0500

    cli: Add --type flag to `flynn ps`
    Signed-off-by: Simon Soriano <simon0191@gmail.com>

--- cli/ps.go
+++ cli/ps.go
@@ -14,14 +14,15 @@ import (
 
 func init() {
 	register("ps", runPs, `
-usage: flynn ps [-a] [-c] [-q]
+usage: flynn ps [-a] [-c] [-q] [-t <type>]
 
 List flynn jobs.
 
 Options:
-  -a, --all      Show all jobs (default is running and pending)
-  -c, --command  Show command
-	-q, --quiet    Only display IDs
+  -a, --all           Show all jobs (default is running and pending)
+  -c, --command       Show command
+  -q, --quiet         Only display IDs
+  -t, --type=<type>   Show jobs of type <type>
 
 Example:
 
@@ -38,11 +39,15 @@ Example:
        host0-0f34548b-72fa-41fe-a425-abc4ac6a3857  web   up     About a minute ago  cf39a906-38d1-4393-a6b1-8ad2befe842	  /runner/init start web
        host0-129b821f-3195-4b3b-b04b-669196cfbb03  run   down   5 seconds ago       cf39a906-38d1-4393-a6b1-8ad2befe842	  /runner/init /bin/bash
 
-			 $ flynn ps --all --quiet
+       $ flynn ps --all --quiet
        host0-52aedfbf-e613-40f2-941a-d832d10fc400
        host0-205595d8-206a-46a2-be30-2e98f53df272
        host0-0f34548b-72fa-41fe-a425-abc4ac6a3857
        host0-129b821f-3195-4b3b-b04b-669196cfbb03
+
+       $ flynn ps --all --type=run
+       ID                                          TYPE  STATE  CREATED             RELEASE
+       host0-129b821f-3195-4b3b-b04b-669196cfbb03  run   down   5 seconds ago       cf39a906-38d1-4393-a6b1-8ad2befe842
 `)
 }
 
@@ -51,27 +56,25 @@ func runPs(args *docopt.Args, client controller.Client) error {
 	if err != nil {
 		return err
 	}
-	all := args.Bool["--all"] || args.Bool["-a"]
-	if args.Bool["--quiet"] || args.Bool["-q"] {
-		printJobsQuiet(jobs, all)
+
+	jobs = prepareAndFilterJobs(jobs, args.Bool["--all"], args.String["<type>"])
+	sort.Sort(sortJobs(jobs))
+
+	if args.Bool["--quiet"] {
+		printJobsQuiet(jobs)
 	} else {
-		printJobs(jobs, all, args.Bool["--command"] || args.Bool["-c"])
+		printJobs(jobs, args.Bool["--command"])
 	}
 	return nil
 }
 
-func printJobsQuiet(jobs []*ct.Job, all bool) {
-	sort.Sort(sortJobs(jobs))
+func printJobsQuiet(jobs []*ct.Job) {
 	for _, j := range jobs {
-		if !all && j.State != ct.JobStateUp && j.State != ct.JobStatePending {
-			continue
-		}
 		fmt.Println(j.ID)
 	}
 }
 
-func printJobs(jobs []*ct.Job, all bool, commandFlagOn bool) {
-	sort.Sort(sortJobs(jobs))
+func printJobs(jobs []*ct.Job, commandFlagOn bool) {
 	w := tabWriter()
 	defer w.Flush()
 
@@ -81,26 +84,36 @@ func printJobs(jobs []*ct.Job, all bool, commandFlagOn bool) {
 	}
 	listRec(w, headers...)
 	for _, j := range jobs {
+		var created string
+		if j.CreatedAt != nil {
+			created = units.HumanDuration(time.Now().UTC().Sub(*j.CreatedAt)) + " ago"
+		}
+		fields := []interface{}{j.ID, j.Type, j.State, created, j.ReleaseID}
+		if commandFlagOn {
+			fields = append(fields, strings.Join(j.Args, " "))
+		}
+		listRec(w, fields...)
+	}
+}
+
+func prepareAndFilterJobs(jobs []*ct.Job, all bool, jobType string) []*ct.Job {
+	filteredJobs := []*ct.Job{}
+	for _, j := range jobs {
 		if !all && j.State != ct.JobStateUp && j.State != ct.JobStatePending {
 			continue
 		}
 		if j.Type == "" {
 			j.Type = "run"
 		}
-		id := j.ID
-		if id == "" {
-			id = j.UUID
+		if jobType != "" && j.Type != jobType {
+			continue
 		}
-		var created string
-		if j.CreatedAt != nil {
-			created = units.HumanDuration(time.Now().UTC().Sub(*j.CreatedAt)) + " ago"
+		if j.ID == "" {
+			j.ID = j.UUID
 		}
-		fields := []interface{}{id, j.Type, j.State, created, j.ReleaseID}
-		if commandFlagOn {
-			fields = append(fields, strings.Join(j.Args, " "))
-		}
-		listRec(w, fields...)
+		filteredJobs = append(filteredJobs, j)
 	}
+	return filteredJobs
 }
 
 // sortJobs sorts Jobs in chronological order based on their CreatedAt time
