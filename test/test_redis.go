6fb096e4f2e30dc0727c2857e606936dbec0f7fb
Lewis Marshall <lewis@lmars.net>
2017-03-27 12:31:06 +0100

    appliance/redis: Fix REDIS_HOST and REDIS_URL
    Fixes #4014.
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/test_redis.go
+++ test/test_redis.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 
@@ -12,6 +13,55 @@ type RedisSuite struct {
 }
 
 var _ = c.ConcurrentSuite(&RedisSuite{})
+
+func (s *RedisSuite) TestRedisEnv(t *c.C) {
+	a := s.newCliTestApp(t)
+
+	// create a redis resource
+	t.Assert(a.flynn("resource", "add", "redis"), Succeeds)
+
+	// get the new release
+	client := s.controllerClient(t)
+	release, err := client.GetAppRelease(a.id)
+	t.Assert(err, c.IsNil)
+
+	// check that FLYNN_REDIS points to a Redis app
+	service, ok := release.Env["FLYNN_REDIS"]
+	if !ok {
+		t.Fatal("missing FLYNN_REDIS")
+	}
+	redisApp, err := client.GetApp(service)
+	t.Assert(err, c.IsNil)
+	redisRelease, err := client.GetAppRelease(redisApp.ID)
+	t.Assert(err, c.IsNil)
+	t.Assert(redisRelease.Processes, c.HasLen, 1)
+	redisProc, ok := redisRelease.Processes["redis"]
+	if !ok {
+		t.Fatal("missing redis process")
+	}
+	t.Assert(redisProc.Service, c.Equals, service)
+	password, ok := redisRelease.Env["REDIS_PASSWORD"]
+	if !ok {
+		t.Fatal("missing REDIS_PASSWORD")
+	}
+
+	// check that the release has valid redis env vars
+	expected := map[string]string{
+		"REDIS_HOST":     fmt.Sprintf("leader.%s.discoverd", service),
+		"REDIS_PORT":     "6379",
+		"REDIS_PASSWORD": password,
+		"REDIS_URL":      fmt.Sprintf("redis://:%s@leader.%s.discoverd:6379", password, service),
+	}
+	for key, val := range expected {
+		actual, ok := release.Env[key]
+		if !ok {
+			t.Fatalf("env missing key %q", key)
+		}
+		if actual != val {
+			t.Fatalf("expected %s to be %s, got %s", key, val, actual)
+		}
+	}
+}
 
 func (s *RedisSuite) TestDumpRestore(t *c.C) {
 	a := s.newCliTestApp(t)
