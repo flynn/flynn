104d82716b3dfd0612c08fe9da2ee3d97bc031df
Jesse Stuart <jesse@jessestuart.ca>
2017-05-11 13:00:55 -0400

    builder,test: Move protoc installation logic into builder (#4104)
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- builder/img/go.sh
+++ builder/img/go.sh
@@ -7,7 +7,7 @@ shasum="47fda42e46b4c3ec93fa5d4d4cc6a748aa3f9411a2a2b7e08e3a6d80d753ec8b"
 dir="/usr/local"
 
 apt-get update
-apt-get install --yes git build-essential
+apt-get install --yes git build-essential unzip
 apt-get clean
 
 curl -fsSLo /tmp/go.tar.gz "https://storage.googleapis.com/golang/go${version}.linux-amd64.tar.gz"
@@ -18,6 +18,14 @@ rm /tmp/go.tar.gz
 export GOROOT="/usr/local/go"
 export GOPATH="/go"
 export PATH="${GOROOT}/bin:${PATH}"
+
+# install protobuf compiler
+tmpdir=$(mktemp --directory)
+trap "rm -rf ${tmpdir}" EXIT
+curl -sL https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip > "${tmpdir}/protoc.zip"
+unzip -d "${tmpdir}/protoc" "${tmpdir}/protoc.zip"
+mv "${tmpdir}/protoc" /opt
+ln -s /opt/protoc/bin/protoc /usr/local/bin/protoc
 
 goinstall() {
   local repo=$1
