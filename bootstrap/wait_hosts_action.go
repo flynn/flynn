6d5e5f8810a5315793b2437575dc45f520a63f93
Jonathan Rudenberg <jonathan@titanous.com>
2017-07-06 16:28:46 -0400

    bootstrap: Wait for discoverd registration in wait-hosts
    This avoids a race with the build system which only boots a minimal
    cluster and relies on the discoverd records being in place after
    wait-hosts finishes. Without this patch, builds can sometimes fail
    with:
    
        wait-hosts wait-hosts
        host.go:166: object_not_found: service not found
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- bootstrap/wait_hosts_action.go
+++ bootstrap/wait_hosts_action.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	discoverd "github.com/flynn/flynn/discoverd/client"
 	"github.com/flynn/flynn/pkg/cluster"
 )
 
@@ -25,22 +26,41 @@ func (a *WaitHostsAction) Run(s *State) error {
 	up := 0
 outer:
 	for {
+		var instances []*discoverd.Instance
+		disc, err := s.DiscoverdClient()
+		if err != nil {
+			goto wait
+		}
+		instances, err = disc.Service("flynn-host").Instances()
+		if err != nil {
+			goto wait
+		}
+
 		for h := range hosts {
 			status, err := h.GetStatus()
 			if err != nil {
 				continue
 			}
 			if status.Network != nil && status.Network.Subnet != "" && status.Discoverd != nil && status.Discoverd.URL != "" {
-				delete(hosts, h)
-				up++
+				for _, inst := range instances {
+					if inst.Addr == h.Addr() {
+						delete(hosts, h)
+						up++
+						break
+					}
+				}
 			}
 		}
 		if up >= s.MinHosts {
 			break outer
 		}
 
+	wait:
 		select {
 		case <-timeout:
+			if err != nil {
+				return err
+			}
 			msg := "bootstrap: timed out waiting for hosts to come up\n\nThe following hosts were unreachable:\n"
 			for host := range hosts {
 				msg += "\t" + host.Addr() + "\n"
