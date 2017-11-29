4c0ff616a49c1e548a7584125203fae5a09295c6
Lewis Marshall <lewis@lmars.net>
2017-11-29 16:57:28 +0000

    util/assetbuilder: Upgrade to Node.js 4.x and node-sass@3.3.3
    This is a minimal updgrade to fix the build which was failing with:
    
    ```
    /app/node_modules/node-sass/node_modules/request/node_modules/hawk/node_modules/boom/lib/index.js:5
    const Hoek = require('hoek');
    ^^^^^
    SyntaxError: Use of const in strict mode.
    ```
    
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- util/assetbuilder/build.sh
+++ util/assetbuilder/build.sh
@@ -5,12 +5,12 @@ cp ${TARGET}/app/Gemfile      /app/Gemfile
 cp ${TARGET}/app/Gemfile.lock /app/Gemfile.lock
 
 # Install NodeSource repo and nodejs, install then run bundler and download npm deps
-curl --fail --silent --location https://deb.nodesource.com/setup | bash -
+curl --fail --silent --location https://deb.nodesource.com/setup_4.x | bash -
 apt-get update
 apt-get install -y nodejs build-essential ruby ruby-dev
 gem install bundler --no-rdoc --no-ri
 cd /app
 bundle install --deployment
 mkdir --parents node_modules
-npm install recast@0.10.30 es6-promise@3.0.2 node-sass@3.2.0 react-tools@0.13
+npm install recast@0.10.30 es6-promise@3.0.2 node-sass@3.3.3 react-tools@0.13
 chmod -R o+rw /app
