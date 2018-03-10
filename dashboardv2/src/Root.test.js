5a809c03fa657977a74b75d15be6c609cba71e84
Jesse Stuart <email@jessestuart.ca>
2018-03-10 14:40:41 -0500

    dashboardv2: Rename App.test.js to Root.test.js
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboardv2/src/Root.test.js
+++ dashboardv2/src/Root.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Root from './Root'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Root />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
