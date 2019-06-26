28a283257e6de262b5cf12d1ec0cbeef366d0158
Jonathan Rudenberg <jonathan@titanous.com>
2019-06-26 19:45:32 +0000

    pkg/tlsconfig: Update TLS cipher/version config
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- pkg/tlsconfig/tlsconfig.go
+++ pkg/tlsconfig/tlsconfig.go
@@ -11,25 +11,16 @@ func SecureCiphers(c *tls.Config) *tls.Config {
 		c = &tls.Config{}
 	}
 
-	c.MinVersion = tls.VersionTLS10 // disable SSLv3
-	c.MaxVersion = tls.VersionTLS12 // enable TLS_FALLBACK_SCSV: https://go-review.googlesource.com/#/c/1776/
+	c.MinVersion = tls.VersionTLS12
 
-	// Use all available ciphersuites minus RC4 and 3DES in a sane order.
-	// This configuration has forward secrecy for supported browsers, and gets
-	// an ‘A’ grade on SSL Labs.
 	c.CipherSuites = []uint16{
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 	}
-	c.PreferServerCipherSuites = true
 
 	return c
 }
