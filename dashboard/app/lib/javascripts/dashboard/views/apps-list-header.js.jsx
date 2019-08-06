22f22a7d06ab7665c74633149ecfca3d1fc667e2
Jesse Stuart <email@jessestuart.ca>
2019-08-06 12:13:58 -0400

    dashboard: Link to v2 dashboard
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboard/app/lib/javascripts/dashboard/views/apps-list-header.js.jsx
+++ dashboard/app/lib/javascripts/dashboard/views/apps-list-header.js.jsx
@@ -1,30 +1,35 @@
 import RouteLink from './route-link';
+import ExternalLink from './external-link';
+import Config from 'dashboard/config';
 
 var AppsListHeader = React.createClass({
-	displayName: "Views.AppsListHeader",
+	displayName: 'Views.AppsListHeader',
 
-	render: function () {
+	render: function() {
 		return (
-			<section className="clearfix">
-				<RouteLink
-					className="btn-green float-right"
-					path="/github">
+			<section>
+				<section className="clearfix">
+					<RouteLink className="btn-green float-right" path="/github">
 						{this.props.githubAuthed ? (
-							"Create application"
+							'Create application'
 						) : (
 							<span className="connect-with-github">
 								<i className="icn-github-mark" />
 								Connect with Github
 							</span>
 						)}
-				</RouteLink>
+					</RouteLink>
 
-				<RouteLink
-					className="btn-green float-right"
-					path="/providers"
-					style={{ marginRight: '1rem' }}>
+					<RouteLink className="btn-green float-right" path="/providers" style={{ marginRight: '1rem' }}>
 						Provision database
-				</RouteLink>
+					</RouteLink>
+				</section>
+
+				<section className="clearfix" style={{ marginTop: '1rem' }}>
+					<ExternalLink className="btn-green float-right" href={'https://dashboardv2.' + Config.default_route_domain}>
+						Dashboard v2
+					</ExternalLink>
+				</section>
 			</section>
 		);
 	}
