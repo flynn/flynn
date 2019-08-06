e33517bfd595f92016dafb55d2650371e42846b3
Jesse Stuart <email@jessestuart.ca>
2019-08-06 12:14:10 -0400

    dashboard: Fix logout btn in Chrome
    <a> was missing an href attribute which broke the button in Chrome
    
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboard/app/lib/javascripts/dashboard/views/nav.js.jsx
+++ dashboard/app/lib/javascripts/dashboard/views/nav.js.jsx
@@ -3,45 +3,54 @@ import RouteLink from './route-link';
 import Config from '../config';
 
 var Nav = React.createClass({
-	displayName: "Views.Nav",
+	displayName: 'Views.Nav',
 
-	render: function () {
+	render: function() {
 		var isAuthenticated = this.props.authenticated;
 		var activeItem = this.__getActiveItem();
 		var navItems = this.state.items;
 
 		return (
 			<ul>
-				{navItems.map(function (item) {
-					return (
-						<li key={item.path} title={item.title} className={item === activeItem ? "active" : ""}>
-							<RouteLink path={item.path}>
-								<i className={item.icon} />
-							</RouteLink>
-						</li>
-					);
-				}.bind(this))}
-				<li title={isAuthenticated ? "Log out" : "Log in"} onClick={NavActions.handleAuthBtnClick}>
-					<a onClick={function(e){e.preventDefault();}}><i className="icn-auth" /></a>
+				{navItems.map(
+					function(item) {
+						return (
+							<li key={item.path} title={item.title} className={item === activeItem ? 'active' : ''}>
+								<RouteLink path={item.path}>
+									<i className={item.icon} />
+								</RouteLink>
+							</li>
+						);
+					}.bind(this)
+				)}
+				<li title={isAuthenticated ? 'Log out' : 'Log in'} onClick={NavActions.handleAuthBtnClick}>
+					<a
+						onClick={function(e) {
+							e.preventDefault();
+						}}
+						href="/"
+					>
+						<i className="icn-auth" />
+					</a>
 				</li>
 			</ul>
 		);
 	},
 
-	getInitialState: function () {
+	getInitialState: function() {
 		return {
 			items: [
-				{ title: "Dashboard", icon: "icn-dashboard", path: "/" },
-				{ title: "Providers", icon: "icn-databases", path: "/providers" },
-				{ title: "Cluster backup", icon: "icn-download", path: "/backup" },
-				{ title: "Cluster status", icon: "icn-system-status", path: "/status" }
+				{ title: 'Dashboard', icon: 'icn-dashboard', path: '/' },
+				{ title: 'Providers', icon: 'icn-databases', path: '/providers' },
+				{ title: 'Cluster backup', icon: 'icn-download', path: '/backup' },
+				{ title: 'Cluster status', icon: 'icn-system-status', path: '/status' }
 			]
 		};
 	},
 
-	__getActiveItem: function () {
-		var currentPath = "/"+ Config.history.getPath();
-		var sortedItems = this.state.items.slice(0).sort(function (a, b) {
+	__getActiveItem: function() {
+		var currentPath = '/' + Config.history.getPath();
+		var sortedItems = this.state.items.slice(0).sort(function(a, b) {
 			return b.path.localeCompare(a.path);
 		});
 		for (var i = 0, len = sortedItems.length, path; i < len; i++) {
