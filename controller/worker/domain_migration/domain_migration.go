5a1ebc187fd8fbb0f5068fa88cb75250e36cbbd5
Jesse Stuart <email@jessestuart.ca>
2019-08-27 13:52:12 -0400

    controller: Add dashboardv2 to domain migration worker
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- controller/worker/domain_migration/domain_migration.go
+++ controller/worker/domain_migration/domain_migration.go
@@ -167,6 +167,15 @@ func (m *migration) Run() error {
 				"URL":                  fmt.Sprintf("https://dashboard.%s", m.dm.Domain),
 			},
 		},
+		{
+			AppName: "dashboardv2",
+			NewEnv: map[string]string{
+				"DEFAULT_ROUTE_DOMAIN": m.dm.Domain,
+				"CONTROLLER_DOMAIN":    fmt.Sprintf("controller-grpc.%s", m.dm.Domain),
+				"CONTROLLER_HOST":      fmt.Sprintf("https://controller-grpc.%s", m.dm.Domain),
+				"INTERFACE_URL":        fmt.Sprintf("https://dashboardv2.%s", m.dm.Domain),
+			},
+		},
 	}
 
 	for _, am := range appMigrations {
