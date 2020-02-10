9fe3570b2a37c881802c7f08b8eef4dda59cc48b
Jesse Stuart <email@jessestuart.ca>
2020-02-10 10:57:36 -0500

    dashboard: add github compare link in addition to commit link
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboard/src/Release.tsx
+++ dashboard/src/Release.tsx
@@ -19,21 +19,33 @@ function ReleaseComponent({ release, prevRelease: prev }: ReleaseProps) {
 	const labels = release.getLabelsMap();
 	const appMeta = app ? app.getLabelsMap() : new jspb.Map([]);
 
-	const gitCommit =
-		labels.get('git.commit') ||
-		(() => {
-			const rev = labels.get('rev');
-			if (labels.get('git') === 'true' && rev) {
-				return rev;
-			}
-			return null;
-		})();
+	const gitCommit = (release: Release) => {
+		const labels = release.getLabelsMap();
+		return (
+			labels.get('git.commit') ||
+			(() => {
+				const rev = labels.get('rev');
+				if (labels.get('git') === 'true' && rev) {
+					return rev;
+				}
+				return null;
+			})()
+		);
+	};
 
-	let githubURL = (appMeta.get('github.url') || null) as string | null;
-	if (githubURL) {
-		githubURL = `${githubURL.replace(/\/$/, '')}/commit/${gitCommit}`;
+	let baseGithubURL = (appMeta.get('github.url') || null) as string | null;
+	let githubCompareURL = null as string | null;
+	let githubURL = null as string | null;
+	if (baseGithubURL) {
+		baseGithubURL = baseGithubURL.replace(/\/?$/, '');
 	} else if (labels.get('github') === 'true') {
-		githubURL = `https://github.com/${labels.get('github_user')}/${labels.get('github_repo')}/commit/${gitCommit}`;
+		baseGithubURL = `https://github.com/${labels.get('github_user')}/${labels.get('github_repo')}`;
+	}
+	if (baseGithubURL) {
+		githubURL = `${baseGithubURL}/commit/${gitCommit(release)}`;
+		if (prev) {
+			githubCompareURL = `${baseGithubURL}/compare/${gitCommit(prev)}...${gitCommit(release)}`;
+		}
 	}
 
 	const releaseID = release
@@ -48,10 +60,16 @@ function ReleaseComponent({ release, prevRelease: prev }: ReleaseProps) {
 					<br />
 				</>
 			) : null}
-			{gitCommit ? (
+			{gitCommit(release) ? (
 				<>
 					<span>
-						git.commit {githubURL ? <ExternalAnchor href={githubURL}>{gitCommit}</ExternalAnchor> : gitCommit}
+						git.commit {githubURL ? <ExternalAnchor href={githubURL}>{gitCommit(release)}</ExternalAnchor> : gitCommit}
+						{githubCompareURL ? (
+							<>
+								&nbsp;
+								<ExternalAnchor href={githubCompareURL}>[compare]</ExternalAnchor>
+							</>
+						) : null}
 					</span>
 					<br />
 				</>
