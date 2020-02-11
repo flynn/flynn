3068556bccc579b76afc1b3fa8ff989fedafc3b3
Jesse Stuart <email@jessestuart.ca>
2020-02-11 16:41:41 -0500

    dashboard: workaround grommet bug so css color vars available in overlay
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboard/src/Dashboard.tsx
+++ dashboard/src/Dashboard.tsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Grommet, Box, Heading } from 'grommet';
-import { aruba } from 'grommet-theme-aruba';
-import tinycolor from 'tinycolor2';
 import styled from 'styled-components';
 
+import theme from './theme';
 import Config from './config';
 import { useLocation } from 'react-router-dom';
 import Split from './Split';
@@ -79,70 +78,9 @@ function DashboardInner() {
 	);
 }
 
-const colors = {
-	green: '#1bb45e',
-	blue: '#08a1f4',
-	orangeLight: '#ff7700',
-	gray: '#727272',
-	black: '#000000',
-	white: '#ffffff'
-};
-const modifiedAruba = Object.assign({}, aruba, {
-	global: Object.assign({}, (aruba as any).global, {
-		font: {
-			family: null // inherit from ./index.css
-		},
-		colors: Object.assign({}, (aruba as any).global.colors, {
-			// color used on active hover state
-			active:
-				'#' +
-				tinycolor(colors.white)
-					.darken(10)
-					.toHex(),
-			// the main brand color
-			brand: colors.green,
-			// the color to be used when element is in focus
-			focus: colors.blue,
-			// the text color of the input placeholder
-			placeholder: colors.gray,
-			// shade of white
-			white: colors.white,
-			// shade of black
-			black: colors.black,
-			'accent-1':
-				'#' +
-				tinycolor(colors.gray)
-					.darken(20)
-					.toHex(),
-			'status-warning': colors.orangeLight,
-			textInput: {
-				backgroundColor: colors.white
-			},
-			border: {
-				// default border color for light mode
-				light: colors.gray,
-				// default border color for dark mode
-				dark: colors.white
-			},
-			control: {
-				// default control color for light mode
-				light: colors.green,
-				// default control color for dark mode
-				dark: colors.green
-			},
-			text: {
-				// the default application text color for light mode
-				light: colors.gray,
-				// the default application text color for dark mode
-				dark: colors.white
-			}
-		})
-	})
-});
-
 export default function Dashboard() {
 	return (
-		<Grommet full theme={modifiedAruba} cssVars>
+		<Grommet full theme={theme} cssVars>
 			<Router>
 				<React.StrictMode>
 					<DashboardInner />
