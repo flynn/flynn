b343fa5d3bdc9227ee0b73153be51c8fcfb51608
Jesse Stuart <email@jessestuart.ca>
2020-03-10 21:04:52 -0400

    dashboard: WIP OAuth
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboard/src/oauth.ts
+++ dashboard/src/oauth.ts
@@ -1,4 +1,189 @@
-export async function base64(input: ArrayBuffer): Promise<string> {
+import Config from './config';
+
+enum StoreKeys {
+	SERVER_META = 'OAUTH_META',
+	CODE_VERIFIER = 'OAUTH_CODE_VERIFIER',
+	CODE_CHALLENGE = 'OAUTH_CODE_CHALLENGE',
+	STATE = 'OAUTH_STATE',
+	REDIRECT_URI = 'OAUTH_REDIRECT_URI',
+	TOKEN = 'OAUTH_TOKEN'
+}
+
+const Store = {
+	setItem: async function setItem(key: StoreKeys[keyof StoreKeys], value: string): Promise<boolean> {
+		localStorage.setItem(key.toString(), value);
+		return true;
+	},
+
+	getItem: async function setItem(key: StoreKeys[keyof StoreKeys]): Promise<string> {
+		return localStorage.getItem(key.toString()) || '';
+	},
+
+	removeItem: async function removeItem(key: StoreKeys[keyof StoreKeys]): Promise<boolean> {
+		localStorage.removeItem(key.toString());
+		return true;
+	},
+
+	clear: async function clear(): Promise<boolean> {
+		await Object.values(StoreKeys).forEach(async (key: string) => {
+			await Store.removeItem(key.toString());
+		});
+		return true;
+	},
+
+	setToken: async function setToken(token: Token | null): Promise<boolean> {
+		if (token === null) {
+			return Store.removeItem(StoreKeys.TOKEN);
+		}
+		return Store.setItem(StoreKeys.TOKEN, JSON.stringify(token));
+	},
+
+	getToken: async function getToken(): Promise<Token | null> {
+		let token: Token | null;
+		try {
+			token = JSON.parse(await Store.getItem(StoreKeys.TOKEN));
+		} catch (e) {
+			token = null;
+		}
+		return token;
+	}
+};
+
+export const OAUTH_CALLBACK_PATH = '/oauth/callback';
+
+export async function generateAuthorizationURL(): Promise<string> {
+	const meta = await getServerMeta();
+	const params = new URLSearchParams('');
+	params.set('code_challenge', await generateCodeChallenge());
+	params.set('code_challenge_method', 'S256');
+	params.set('state', await generateState());
+	params.set('client_id', Config.OAUTH_CLIENT_ID);
+	params.set('response_type', 'code');
+	const redirectURI = resolveURLPath(OAUTH_CALLBACK_PATH);
+	await Store.setItem(StoreKeys.REDIRECT_URI, redirectURI);
+	params.set('redirect_uri', redirectURI);
+	return `${meta.authorization_endpoint}?${params.toString()}`;
+}
+
+export interface Token {
+	access_token: string;
+	token_type: string;
+	expires_in: number;
+	refresh_token: string;
+
+	issued_time: number;
+}
+
+type TokenCallbackFn = (token: Token | null, error: Error | null) => void;
+
+export async function getToken(responseParams: string, callback: TokenCallbackFn, abortSignal: AbortSignal) {
+	const params = new URLSearchParams(responseParams);
+	if (!(await verifyState(params.get('state') || ''))) {
+		throw new Error(`Error verifying state param`);
+	}
+	const meta = await getServerMeta();
+	const res = await fetch(meta.token_endpoint, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			grant_type: 'authorization_code',
+			code: params.get('code') || '',
+			code_verifier: await Store.getItem(StoreKeys.CODE_VERIFIER),
+			redirect_uri: await Store.getItem(StoreKeys.REDIRECT_URI),
+			client_id: Config.OAUTH_CLIENT_ID
+		}),
+		signal: abortSignal
+	});
+	const token = await res.json();
+	if (abortSignal.aborted) return;
+	if (token.error === '') {
+		token.issued_time = Date.now();
+		setTimeout(() => {
+			if (abortSignal.aborted) return;
+			refreshToken(token.refresh_token || '', callback, abortSignal);
+		}, token.expires_in - 30);
+		await Store.setToken(token);
+		callback(token as Token, null);
+	} else {
+		await Store.setToken(token);
+		callback(null, new Error(`Error getting auth token: ${token.error_description || token.error}`));
+	}
+}
+
+async function refreshToken(code: string, callback: TokenCallbackFn, abortSignal: AbortSignal) {
+	const meta = await getServerMeta();
+	const params = new URLSearchParams('');
+	params.set('grant_type', 'refresh_token');
+	params.set('code', code);
+	const res = await fetch(meta.token_endpoint, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: params.toString(),
+		signal: abortSignal
+	});
+	const token = await res.json();
+	if (abortSignal.aborted) return;
+	if (token.error === '') {
+		setTimeout(() => {
+			if (abortSignal.aborted) return;
+			refreshToken(token.refresh_token || '', callback, abortSignal);
+		}, token.expires_in - 30);
+		await Store.setToken(token);
+		callback(token as Token, null);
+	} else {
+		await Store.setToken(null);
+		callback(null, new Error(`Error refreshing auth token: ${token.error_description || token.error}`));
+	}
+}
+
+function resolveURLPath(path: string): string {
+	const a = document.createElement('a');
+	a.href = path;
+	return a.href;
+}
+
+interface ServerMetadata {
+	issuer: string;
+	authorization_endpoint: string;
+	token_endpoint: string;
+	token_endpoint_auth_methods_supported: string;
+	token_endpoint_auth_signing_alg_values_supported: string;
+	userinfo_endpoint: string;
+}
+
+async function getServerMeta(): Promise<ServerMetadata> {
+	const localMetaJSON = await Store.getItem(StoreKeys.SERVER_META);
+	if (localMetaJSON !== '') {
+		const localMeta = JSON.parse(localMetaJSON);
+		if (codePointCompare(Config.OAUTH_ISSUER, localMeta.issuer)) {
+			return localMeta;
+		}
+	}
+
+	const url = `${Config.OAUTH_ISSUER}/.well-known/oauth-authorization-server`;
+	const res = await fetch(url);
+	const meta = await res.json();
+	if (!codePointCompare(Config.OAUTH_ISSUER, meta.issuer)) {
+		throw new Error(
+			`Error verifying OAuth Server Metadata: Issuer mismatch: "${Config.OAUTH_ISSUER}" != ${meta.issuer}`
+		);
+	}
+	return meta;
+}
+
+function codePointCompare(a: string, b: string): boolean {
+	if (a.length !== b.length) return false;
+	for (let len = a.length, i = 0; i < len; i++) {
+		if (a.codePointAt(i) !== b.codePointAt(i)) return false;
+	}
+	return true;
+}
+
+async function base64(input: ArrayBuffer): Promise<string> {
 	let resolve: (str: string) => void;
 	let reject: (error: Error) => void;
 	const p = new Promise<string>((rs, rj) => {
@@ -15,7 +200,8 @@ export async function base64(input: ArrayBuffer): Promise<string> {
 	reader.addEventListener('load', (e) => {
 		const res = (reader.result || '') as string;
 		// trim data:*/*;base64, prefix
-		resolve(res.slice(res.indexOf('base64,') + 7));
+		const prefix = 'base64,';
+		resolve(res.slice(res.indexOf(prefix) + prefix.length));
 	});
 
 	reader.readAsDataURL(blob);
@@ -23,18 +209,18 @@ export async function base64(input: ArrayBuffer): Promise<string> {
 	return p;
 }
 
-export async function sha256(input: string): Promise<ArrayBuffer> {
+async function sha256(input: string): Promise<ArrayBuffer> {
 	const encoder = new TextEncoder();
 	const data = encoder.encode(input);
 	return crypto.subtle.digest('SHA-256', data);
 }
 
-export function randomString(length: number): string {
+function randomString(length: number): string {
 	const randomValues = random(length * 2);
 	return hex(randomValues).slice(0, length);
 }
 
-export function random(length: number): ArrayBuffer {
+function random(length: number): ArrayBuffer {
 	const buffer = new ArrayBuffer(length);
 	const array = new Uint32Array(buffer);
 	crypto.getRandomValues(array);
@@ -46,4 +232,22 @@ function hex(input: ArrayBuffer): string {
 	return Array.from(view, function(b) {
 		return ('0' + (b & 0xff).toString(16)).slice(-2);
 	}).join('');
+}
+
+async function generateCodeChallenge(): Promise<string> {
+	const codeVerifier = randomString(64);
+	await Store.setItem(StoreKeys.CODE_VERIFIER, codeVerifier);
+	const codeChallenge = await base64(await sha256(codeVerifier));
+	await Store.setItem(StoreKeys.CODE_CHALLENGE, codeChallenge);
+	return codeChallenge;
+}
+
+async function generateState(): Promise<string> {
+	const state = randomString(16);
+	await Store.setItem(StoreKeys.STATE, state);
+	return state;
+}
+
+async function verifyState(state: string): Promise<boolean> {
+	return codePointCompare(await Store.getItem(StoreKeys.STATE), state);
 }
